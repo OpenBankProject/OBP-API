OpenBankProject/OBP-API

commit c92ff16c91deb7efcf59f2f4c42dc0ba57ead25d

Removed 'Opan Bank' from the footer, altered the data mediation.

MavLift/src/main/scala/code/model/OBPTransaction.scala
@@ -232,7 +232,12 @@ class OBPAccount private() extends BsonRecord[OBPAccount]{
 
   
   def accountAliases : Map[String,String] = {
-    Map("Neils Hapke" -> "The Chicken", "Yoav Aner" -> "Software Developer 1", "Jan Slabiak" -> "Alex")
+    Map("Aida Ghiuntar" -> "Cleaning Services",
+        "Martin Gordon" -> "Project Management 1",
+        "Ismail Chaib" -> "Project Management 2",
+        "Eveline Mäthner" -> "Accounts",
+        "Everett Sochowski" -> "Developer",
+        "James Edward Morris" -> "Developer")
   }
   
   //TODO: Access levels are currently the same across all transactions
@@ -429,7 +434,11 @@ class OBPBalance private() extends BsonRecord[OBPBalance]{
   //TODO: Access levels are currently the same across all transactions
   def mediated_currency(user: String) : Box[String] = {
     user match{
-      case _ => Full(currency.get.toString)
+      case "our-network" => Full(amount.get.toString)
+      case "team" => Full(amount.get.toString)
+      case "board" => Full(amount.get.toString)
+      case "authorities" => Full(amount.get.toString)
+      case _ => Empty
     }
   }
   //TODO: Access levels are currently the same across all transactions
@@ -438,7 +447,13 @@ class OBPBalance private() extends BsonRecord[OBPBalance]{
    */
   def mediated_amount(user: String) : Box[String] = {
     user match{
-      case _ => Full(amount.get.toString)
+      case "our-network" => Full(amount.get.toString)
+      case "team" => Full(amount.get.toString)
+      case "board" => Full(amount.get.toString)
+      case "authorities" => Full(amount.get.toString)
+      case _ => {
+        if(amount.get.toString.startsWith("-")) Full("-") else Full("+")
+      }
     }
   }
   

MavLift/src/main/webapp/templates-hidden/default.html
@@ -110,8 +110,8 @@ h1.logo {
       <hr />
       <div class="column span-23 last" style="text-align: center">
         <h4 class="alt">
-          <a href="http://www.openbankproject.com"><i>Open Bank Project</i></a> and <a href="http://www.opanbank.com"><i>OpanBank</i></a>
-	  is (c)2011 TESOBE and distributed under the Apache 2.0 License.</h4>
+          <a href="http://www.openbankproject.com"><i>Open Bank Project</i></a>
+	  is ©2011 TESOBE and distributed under the Apache 2.0 License.</h4>
       </div>
     </div>
   </body>

commit f5a39f75cabc826d19a718f0a6fb4d5423fb5c97

Got rid of a bug introduced in previous commit c92ff16c91deb7efcf59f2f4c42dc0ba57ead25d that was displaying account balance in place of account currency.

MavLift/src/main/scala/code/model/OBPTransaction.scala
@@ -434,10 +434,10 @@ class OBPBalance private() extends BsonRecord[OBPBalance]{
   //TODO: Access levels are currently the same across all transactions
   def mediated_currency(user: String) : Box[String] = {
     user match{
-      case "our-network" => Full(amount.get.toString)
-      case "team" => Full(amount.get.toString)
-      case "board" => Full(amount.get.toString)
-      case "authorities" => Full(amount.get.toString)
+      case "our-network" => Full(currency.get)
+      case "team" => Full(currency.get)
+      case "board" => Full(currency.get)
+      case "authorities" => Full(currency.get)
       case _ => Empty
     }
   }