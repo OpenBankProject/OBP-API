1ed5e8528a4147361191062db4285b705963dae8
Everett Sochowski <everett@tesobe.com>
2012-05-07 11:23:52 +0200

    Transaction dates are now grouped by day, rather than by exact time

--- MavLift/src/main/scala/code/snippet/OBPTransactionSnippet.scala
+++ MavLift/src/main/scala/code/snippet/OBPTransactionSnippet.scala
@@ -241,7 +241,17 @@ class OBPTransactionSnippet {
     val t1 = e1.obp_transaction.get
     val t2 = e2.obp_transaction.get
 
-    t1.details.get.completed.get.equals(t2.details.get.completed.get)
+    val date1 = t1.details.get.completed.get
+    val date2 = t2.details.get.completed.get
+    
+    val cal1 = Calendar.getInstance();
+    val cal2 = Calendar.getInstance();
+    cal1.setTime(date1);
+    cal2.setTime(date2);
+    
+    //True if the two dates fall on the same day of the same year
+    cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) &&
+                  cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR);
   }
 
   /**
