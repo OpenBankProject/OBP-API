2e8cd46d23336d7394665b16eb650cfa3ab60f4e
Ayoub BENALI <ayoub@tesobe.com>
2012-07-26 12:23:06 +0200

    added an oauth mechanism to the API Get request

--- MavLift/src/main/scala/code/snippet/OBPREST.scala
+++ MavLift/src/main/scala/code/snippet/OBPREST.scala
@@ -20,9 +20,9 @@ Open Bank Project (http://www.openbankproject.com)
 
       This product includes software developed at
       TESOBE (http://www.tesobe.com/)
-		by 
-		Simon Redfern : simon AT tesobe DOT com
-		Everett Sochowski: everett AT tesobe DOT com
+    by 
+    Simon Redfern : simon AT tesobe DOT com
+    Everett Sochowski: everett AT tesobe DOT com
 
  */
 package com.tesobe.utils {
@@ -70,9 +70,11 @@ import code.model._
 // See http://www.assembla.com/spaces/liftweb/wiki/REST_Web_Services
 
 
-object OBPRest extends RestHelper with Loggable {
-  println("here we are in OBPRest")
-    serve {
+object OBPRest extends RestHelper with Loggable 
+{
+    println("here we are in OBPRest")
+    serve 
+    {
 
       
       
@@ -103,13 +105,13 @@ object OBPRest extends RestHelper with Loggable {
                "type_en":"Transfer",
                "type_de":"Überweisung",
                "posted":{
-		  		  "$dt":"2012-01-04T18:06:22.000Z"
-				},
+            "$dt":"2012-01-04T18:06:22.000Z"
+        },
                "completed":{
-		  		  "$dt":"2012-09-04T18:52:13.000Z"
-				},
+            "$dt":"2012-09-04T18:52:13.000Z"
+        },
                "new_balance":{
-               	  "currency":"EUR",
+                  "currency":"EUR",
                   "amount":"4323.45"
                },
                "value":{
@@ -119,7 +121,7 @@ object OBPRest extends RestHelper with Loggable {
                "other_data":"9"
             }
          }
- }  ' http://localhost:8080/api/transactions  
+   }  ' http://localhost:8080/api/transactions  
      */
     case "api" :: "transactions" :: Nil JsonPost json => {
       
@@ -160,50 +162,106 @@ object OBPRest extends RestHelper with Loggable {
         case _ => InternalServerErrorResponse()
       }
     } 
-    
-    /**
-     * Currently only anonymous level access is supported until authentication 
-     * is put into place.
-     */
-    // curl -H "Accept: application/json" -H "Context-Type: application/json" -X 
-    // GET "http://localhost:8080/api/accounts/tesobe/anonymous"
-    // This is for demo purposes only, as it's showing every single transaction 
-    // rather than everything tesobe specific. This will need
-    // to be completely reworked.
-    // case "api" :: "accounts" :: "tesobe" :: accessLevel :: Nil JsonGet _ => {
-
-    //   println(accessLevel)
-      
-    //   val allEnvelopes = OBPEnvelope.findAll(QueryBuilder.start().get)
-      
-    //   val envelopeJson = allEnvelopes.map(envelope => 
-    //     envelope.asMediatedJValue(accessLevel))
-      
-    //   JsonResponse(envelopeJson)
-    // }
-    
-    /**
-     * Currently only anonymous level access is supported until authentication 
-     * is put into place.
-     */
-    // curl -H "Accept: application/json" -H "Context-Type: application/json" -X 
-    // GET "http://localhost:8080/api/accounts/tesobe/anonymous"
-    // This is for demo purposes only, as it's showing every single transaction 
-    // rather than everything tesobe specific. This will need
-    // to be completely reworked.
-    case "api" :: "accounts" :: "tesobe" :: "anonymous" :: Nil JsonGet _ => {
-      
+    /*
+      returns the anonymous view automatically.
+      For the moment the is only one account so there is no check to do 
+      before returning the Json
+    */
+    case Req("api" :: "accounts" :: "tesobe":: "anonymous" :: Nil,_ ,GetRequest) => 
+    {
       val allEnvelopes = OBPEnvelope.findAll(QueryBuilder.start().get)
-      
-      val envelopeJson = allEnvelopes.map(envelope => envelope.asMediatedJValue
-        ("anonymous",envelope.id.toString()))
-      
-      JsonResponse(envelopeJson)
+      val envelopeJson = allEnvelopes.map(envelope => 
+      envelope.asMediatedJValue("accessLevel",envelope.id.toString))
+      Full(JsonResponse(envelopeJson))  
+    }    
+    /*
+      return a JSon of all the transactions.
+      requires an OAuth token as authentication mechanism. 
+    */
+    case Req("api" :: "accounts" :: "tesobe":: accessLevel :: Nil,_ ,GetRequest) => 
+    {
+        //check if the accessLeve required already exists 
+        def doesTheViewExists(accessLevel : String) : Boolean = 
+        {
+          /* 
+            for the moment the number of views is limited this why 
+            they are hard coded. The work on 'dataAbstraction' branch 
+            will avoid this kind of verification. 
+          */   
+          val currentExistingViews = List("anonymous", "our-network", "team", 
+            "board", "my-view")
+          currentExistingViews.contains(accessLevel)
+        }
+        //check of the token authorize the application to have access 
+        //to accessLevel
+        def doesTheTokenHasAccess(accessLevel : String, tokenID : String) : Boolean = 
+        {
+            //check if the privileges of the user authorize if to access
+            // to the access level
+            def doesTheUserHasAccess(accessLevel : String, user : User) : Boolean =
+            {
+                import code.model.{Account, Privilege} 
+                Account.find(("holder", "Music Pictures Limited")) match {
+                case Full(account) => if(accessLevel=="anonymous")
+                                        account.anonAccess.is
+                                      else
+                                        Privilege.find(By(Privilege.accountID,account.id.toString), By(Privilege.user, user)) match {
+                                          case Full(privilege) => privilegeCheck(accessLevel,privilege)
+                                          case _ => false 
+                                        }
+                case _ => false  
+                }
+            }
+
+            //due to the actual privilege mechanism : the access level (API) 
+            //and the privilege of the account are stored differently 
+            //So the function do the match 
+            def privilegeCheck(accessLevel : String, privilege : Privilege) : Boolean = 
+              if(accessLevel == "my-view")
+                privilege.ownerPermission.is
+              else if(accessLevel == "our-network")
+                privilege.ourNetworkPermission.is
+              else if(accessLevel ==  "team")
+                privilege.teamPermission.is  
+              else if(accessLevel == "board")
+                privilege.boardPermission.is
+              else 
+                false 
+
+            import code.model.Token._     
+            Token.find(By(Token.key,tokenID)) match {
+              case Full(token) => User.find(By(User.id,token.userId)) match {
+                                    case Full(user) => 
+                                            doesTheUserHasAccess(accessLevel, user) 
+                                    case _ => false 
+                                  }
+              case _ => false  
+            }
+        }
+        
+        import code.snippet.OAuthHandshake._
+        val headers = ("Content-type" -> "application/x-www-form-urlencoded") :: Nil
+        //Extract the OAuth parameters from the header and test if the request is valid
+        var (httpCode, data, oAuthParameters) = validator("protectedResource", "GET")
+        //Test if the view exists and the OAuth request is valid
+        if(doesTheViewExists(accessLevel) && httpCode==200)
+        {
+            //check that the token gives access to the required view
+            if(doesTheTokenHasAccess(accessLevel, oAuthParameters.get("oauth_token").get))
+            {
+              val allEnvelopes = OBPEnvelope.findAll(QueryBuilder.start().get)
+              val envelopeJson = allEnvelopes.map(envelope => 
+              envelope.asMediatedJValue(accessLevel,envelope.id.toString))
+              Full(JsonResponse(envelopeJson))  
+            }
+            else
+              Full(InMemoryResponse(data,headers,Nil,401)) 
+        }
+        else if(!doesTheViewExists(accessLevel) && httpCode==200)
+          Full(InMemoryResponse(data,headers,Nil,404))
+        else 
+          Full(InMemoryResponse(data,headers,Nil,httpCode))
     }
-    
-      
-    }
+  }
 }
-
-
-} // end of package
+} 
