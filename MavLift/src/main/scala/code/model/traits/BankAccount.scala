9f3b40056dcdacc85cc0b564820ce0ef52c76c62
Everett Sochowski <everett@tesobe.com>
2012-11-06 16:21:04 +0100

    Partway through refactoring

--- MavLift/src/main/scala/code/model/traits/BankAccount.scala
+++ MavLift/src/main/scala/code/model/traits/BankAccount.scala
@@ -1,6 +1,11 @@
 package code.model.traits
 import scala.math.BigDecimal
 import java.util.Date
+import net.liftweb.common.Box
+import code.model.dataAccess.LocalStorage
+import net.liftweb.common.{Full, Empty}
+import code.model.dataAccess.Account
+import code.model.dataAccess.OBPEnvelope.OBPQueryParam
 
 trait BankAccount {
 
@@ -30,13 +35,22 @@ trait BankAccount {
   
   def iban : Option[String]
   
-  def transactions : Set[Transaction]
-  
-  def transactions(from: Date, to: Date) : Set[Transaction]
-  
   def transaction(id: String) : Option[Transaction]
   
   //Is an anonymous view available for this bank account
   def allowAnnoymousAccess : Boolean
   
+  def getModeratedTransactions(moderate: Transaction => ModeratedTransaction): List[ModeratedTransaction]
+  
+  def getModeratedTransactions(queryParams: OBPQueryParam*)(moderate: Transaction => ModeratedTransaction): List[ModeratedTransaction]
+  
+}
+
+object BankAccount {
+  def apply(bankpermalink: String, bankAccountPermalink: String) : Box[BankAccount] = {
+    LocalStorage.getAccount(bankpermalink, bankAccountPermalink) match {
+      case Full(account) => Full(Account.toBankAccount(account))
+      case _ => Empty
+    }
+  }
 }
