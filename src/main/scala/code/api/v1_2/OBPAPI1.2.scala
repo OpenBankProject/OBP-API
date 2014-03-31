1f70101a97a9e68d7a72d6e87c62bd1887ee03d0
Everett Sochowski <everett@tesobe.com>
2014-03-31 17:38:29 +0200

    Add logging when users create, update, or delete views

--- src/main/scala/code/api/v1_2/OBPAPI1.2.scala
+++ src/main/scala/code/api/v1_2/OBPAPI1.2.scala
@@ -221,8 +221,7 @@ object OBPAPI1_2 extends OBPRestHelper with Loggable {
           json <- tryo{json.extract[ViewCreationJSON]} ?~ "wrong JSON format"
           u <- user ?~ "user not found"
           account <- BankAccount(bankId, accountId)
-          canAddViews <- booleanToBox(u.ownerAccess(account), {"user: " + u.idGivenByProvider + " at provider " + u.provider + " does not have owner access"})
-          view <- account createView json
+          view <- account createView (u, json)
         } yield {
             val viewJSON = JSONFactory.createViewJSON(view)
             successJsonResponse(Extraction.decompose(viewJSON), 201)
@@ -237,9 +236,8 @@ object OBPAPI1_2 extends OBPRestHelper with Loggable {
         for {
           account <- BankAccount(bankId, accountId)
           u <- user ?~ "user not found"
-          canAddViews <- booleanToBox(u.ownerAccess(account), {"user: " + u.idGivenByProvider + " at provider " + u.provider + " does not have owner access"})
           updateJson <- tryo{json.extract[ViewUpdateData]} ?~ "wrong JSON format"
-          updatedView <- account.updateView(viewId, updateJson)
+          updatedView <- account.updateView(u, viewId, updateJson)
         } yield {
           val viewJSON = JSONFactory.createViewJSON(updatedView)
           successJsonResponse(Extraction.decompose(viewJSON), 200)
@@ -254,8 +252,7 @@ object OBPAPI1_2 extends OBPRestHelper with Loggable {
         for {
           u <- user ?~ "user not found"
           account <- BankAccount(bankId, accountId)
-          canRemoveViews <- booleanToBox(u.ownerAccess(account), {"user: " + u.idGivenByProvider + " at provider " + u.provider + " does not have owner access"})
-          view <- account removeView viewId
+          view <- account removeView (u, viewId)
         } yield noContentJsonResponse
     }
   })
