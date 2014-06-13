787c9c6c3b805fcfca715e878c045f96dc1f9b98
Everett Sochowski <everett@tesobe.com>
2014-06-13 14:22:24 +0200

    Create separate place for transaction images

---
+++ src/main/scala/code/metadata/transactionimages/MongoTransactionImages.scala
@@ -0,0 +1,26 @@
+package code.metadata.transactionimages
+
+import code.model.TransactionImage
+import net.liftweb.common.Box
+import java.net.URL
+import java.util.Date
+
+class MongoTransactionImages extends TransactionImages {
+
+  def getImagesForTransaction(bankId : String, accountId : String, transactionIdGivenByBank: String) : Iterable[TransactionImage] = {
+    //TODO
+    null
+  }
+  
+  def addTransactionImage(bankId : String, accountId : String, transactionId: String)
+  (userId: String, viewId : Long, description : String, datePosted : Date, url: URL) : Box[TransactionImage] = {
+    //TODO
+    null
+  }
+  
+  def deleteTransactionImage(transactionId : String) : Box[Unit] = {
+    //TODO
+    null
+  }
+  
+}
