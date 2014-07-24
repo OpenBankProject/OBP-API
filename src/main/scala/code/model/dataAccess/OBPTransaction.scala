a1c4f6943b96e1e2fa786571de591afde770d749
Everett Sochowski <everett@tesobe.com>
2014-07-24 15:02:27 +0200

    Move OBPGeoTag into MongoCounterparties

--- src/main/scala/code/model/dataAccess/OBPTransaction.scala
+++ src/main/scala/code/model/dataAccess/OBPTransaction.scala
@@ -410,27 +410,3 @@ class OBPValue private() extends BsonRecord[OBPValue]{
 }
 
 object OBPValue extends OBPValue with BsonMetaRecord[OBPValue]
-
-class OBPGeoTag private() extends BsonRecord[OBPGeoTag] with GeoTag {
-  def meta = OBPGeoTag
-
-  //These fields are used to link this to its transaction
-  object transactionId extends StringField(this, 255)
-  object accountId extends StringField(this, 255)
-  object bankId extends StringField(this, 255)
-
-  object userId extends StringField(this,255)
-  object viewID extends LongField(this)
-  object date extends DateField(this)
-
-  object geoLongitude extends DoubleField(this,0)
-  object geoLatitude extends DoubleField(this,0)
-
-  def datePosted = date.get
-  def postedBy = User.findByApiId(userId.get)
-  def viewId = viewID.get
-  def longitude = geoLongitude.get
-  def latitude = geoLatitude.get
-
-}
-object OBPGeoTag extends OBPGeoTag with BsonMetaRecord[OBPGeoTag]
