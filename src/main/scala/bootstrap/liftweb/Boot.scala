eec195783551da1724973956d0d5ea49498d4491
Everett Sochowski <everett@tesobe.com>
2014-08-08 11:43:37 +0200

    Add v1.3.0 to the dispatch table

--- src/main/scala/bootstrap/liftweb/Boot.scala
+++ src/main/scala/bootstrap/liftweb/Boot.scala
@@ -173,6 +173,7 @@ class Boot extends Loggable{
     LiftRules.statelessDispatchTable.append(v1_1.OBPAPI1_1)
     LiftRules.statelessDispatchTable.append(v1_2.OBPAPI1_2)
     LiftRules.statelessDispatchTable.append(v1_2_1.OBPAPI1_2_1)
+    LiftRules.statelessDispatchTable.append(v1_3_0.OBPAPI1_3_0)
     LiftRules.statelessDispatchTable.append(BankMockAPI)
     // LiftRules.statelessDispatchTable.append(Metrics) TODO: see metric menu entry bellow
 
