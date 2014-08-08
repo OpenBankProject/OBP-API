2a48159f59d66117fc22600679563688bc55bf81
Everett Sochowski <everett@tesobe.com>
2014-08-08 11:36:57 +0200

    Implement some cards api call tests

--- src/test/scala/code/api/v1_3_1/PhysicalCardsTest.scala
+++ src/test/scala/code/api/v1_3_1/PhysicalCardsTest.scala
@@ -14,6 +14,8 @@ import code.util.APIUtil.OAuth.Consumer
 import code.model.PhysicalCard
 import code.model.{Consumer => OBPConsumer, Token => OBPToken}
 import java.util.Date
+import code.util.APIUtil.OAuth._
+import code.api.v1_3_0.PhysicalCardsJSON
 
 class PhysicalCardsTest extends ServerSetup {
 
@@ -73,24 +75,35 @@ class PhysicalCardsTest extends ServerSetup {
   val user1 = Some((consumer, token))
   val user2 = Some((consumer, token2))
 
-  val user1CardAtBank1 = new PhysicalCard(
-  val bankCardNumber : String,
-  val nameOnCard = "",
-  val issueNumber = "",
-  val serialNumber = "",
-  val validFrom : Date,
-  val expires : Date,
-  val enabled = true,
-  val cancelled = false,
-  val technology = "",
-  val networks = Set.empty,
-  val allows = Set.empty,
-  val account : BankAccount,
-  val replacement = None,
-  val pinResets = Nil,
-  val collected = None,
-  val posted = None
+  def createCard(number : String) = new PhysicalCard(
+    bankCardNumber = number,
+    nameOnCard = "",
+    issueNumber = "",
+    serialNumber = "",
+    validFrom = new Date(),
+    expires = new Date(),
+    enabled = true,
+    cancelled = false,
+    technology = "",
+    networks = Set.empty,
+    allows = Set.empty,
+    account = None,
+    replacement = None,
+    pinResets = Nil,
+    collected = None,
+    posted = None
   )
+
+  val user1CardAtBank1 = createCard("1")
+  val user1CardAtBank2 = createCard("2")
+  val user2CardAtBank1 = createCard("a")
+  val user2CardAtBank2 = createCard("b")
+
+  val user1AllCards = Set(user1CardAtBank1, user1CardAtBank2)
+  val user2AllCards = Set(user2CardAtBank1, user2CardAtBank2)
+
+  val user1CardsForOneBank = Set(user1CardAtBank1)
+  val user2CardsForOneBank = Set(user2CardAtBank1)
 
   object MockedCardConnector extends Connector {
     //these methods aren't required by our test
@@ -130,16 +143,51 @@ class PhysicalCardsTest extends ServerSetup {
         Set.empty
       }
     }
+
+    def getAccountHolders(bankID: String, accountID: String) : Set[User] = Set.empty
   }
 
   feature("Getting details of physical cards") {
 
     scenario("A user wants to get details of all their cards across all banks") {
 
+      When("A user requests their cards")
+
+      val request = (v1_3Request / "cards").GET <@(user1)
+      val response = makeGetRequest(request)
+
+      Then("We should get a 200")
+      response.code should equal(200)
+
+      //dummy connector above tells us we should get back user1AllCards
+      //we are just testing that the api calls the connector properly
+      And("We should get the correct cards")
+      val expectedCardNumbers = user1AllCards.map(_.bankCardNumber)
+      val json = response.body.extract[PhysicalCardsJSON]
+      val returnedCardNumbers = json.cards.map(_.bank_card_number).toSet
+
+      returnedCardNumbers should equal(expectedCardNumbers)
     }
 
     scenario("A user wants to get details of all their cards issued by a single bank") {
+      When("A user requests their cards")
 
+      //our dummy connector doesn't care about the value of the bank id, so we can just use "somebank"
+      val request = (v1_3Request / "somebank" / "cards").GET <@(user1)
+      val response = makeGetRequest(request)
+
+      Then("We should get a 200")
+      response.code should equal(200)
+
+      //dummy connector above tells us we should get back user1CardsForOneBank
+      //we are just testing that the api calls the connector properly
+      And("We should get the correct cards")
+
+      val expectedCardNumbers = user1CardsForOneBank.map(_.bankCardNumber)
+      val json = response.body.extract[PhysicalCardsJSON]
+      val returnedCardNumbers = json.cards.map(_.bank_card_number).toSet
+
+      returnedCardNumbers should equal(expectedCardNumbers)
     }
   }
 
