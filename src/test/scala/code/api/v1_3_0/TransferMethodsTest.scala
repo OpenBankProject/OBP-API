497aa18245d6579dc136bcd286b266958c7c1428
Everett Sochowski <everett@tesobe.com>
2014-09-08 12:45:23 +0200

    Add placeholder tests for getting transfer methods

---
+++ src/test/scala/code/api/v1_3_0/TransferMethodsTest.scala
@@ -0,0 +1,22 @@
+package code.api.v1_3_0
+
+import code.api.test.ServerSetup
+
+class TransferMethodsTest extends ServerSetup {
+
+  feature("Methods of making bank transfers") {
+
+    scenario("Retrieval of all transfer methods for an account") {
+      //TODO
+      1 should equal(2)
+    }
+
+
+    scenario("Retrieval of all transfer methods for an account that does not exist") {
+      //TODO
+      1 should equal(2)
+    }
+
+  }
+
+}
