518b284e2a1250ccc7117b85f2b99285499617eb
Everett Sochowski <everett@tesobe.com>
2014-11-11 17:50:27 +0100

    Avoid upserts for where tags for now

--- src/main/scala/code/metadata/wheretags/MongoTransactionWhereTags.scala
+++ src/main/scala/code/metadata/wheretags/MongoTransactionWhereTags.scala
@@ -2,28 +2,34 @@ package code.metadata.wheretags
 
 import java.util.Date
 import code.model._
-import net.liftweb.common.Loggable
+import net.liftweb.common.{Full, Loggable}
 
 private object MongoTransactionWhereTags extends WhereTags with Loggable {
 
   def addWhereTag(bankId : BankId, accountId : AccountId, transactionId: TransactionId)
                  (userId: UserId, viewId : ViewId, datePosted : Date, longitude : Double, latitude : Double) : Boolean = {
 
-
-    val newTag = OBPWhereTag.createRecord.
-      bankId(bankId.value).
-      accountId(accountId.value).
-      transactionId(transactionId.value).
-      userId(userId.value).
-      forView(viewId.value).
-      date(datePosted).
-      geoLongitude(longitude).
-      geoLatitude(latitude)
-
-    //use an upsert to avoid concurrency issues
-    // find query takes into account viewId as we only allow one geotag per view
-    OBPWhereTag.upsert(OBPWhereTag.getFindQuery(bankId, accountId, transactionId, viewId), newTag.asDBObject)
-
+    //avoiding upsert for now as it seemed to behave a little strangely
+    val findQuery = OBPWhereTag.getFindQuery(bankId, accountId, transactionId, viewId)
+    val found = OBPWhereTag.find(findQuery)
+    found match {
+      case Full(f) => {
+        f.date(datePosted).
+          geoLongitude(longitude).
+          geoLatitude(latitude).save(true)
+      }
+      case _ => {
+        OBPWhereTag.createRecord.
+          bankId(bankId.value).
+          accountId(accountId.value).
+          transactionId(transactionId.value).
+          userId(userId.value).
+          forView(viewId.value).
+          date(datePosted).
+          geoLongitude(longitude).
+          geoLatitude(latitude).save(true)
+      }
+    }
     //we don't have any useful information here so just return true
     true
   }
