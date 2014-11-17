83725f491355e9c7a2fa4d4611f43db51da6daff
Everett Sochowski <everett@tesobe.com>
2014-11-17 15:58:01 +0100

    Add todo

--- src/main/scala/code/metadata/comments/Comments.scala
+++ src/main/scala/code/metadata/comments/Comments.scala
@@ -17,6 +17,7 @@ trait Comments {
   
   def getComments(bankId : BankId, accountId : AccountId, transactionId : TransactionId)(viewId : ViewId) : List[Comment]
   def addComment(bankId : BankId, accountId : AccountId, transactionId: TransactionId)(userId: UserId, viewId : ViewId, text : String, datePosted : Date) : Box[Comment]
+  //TODO: should commentId be unique among all comments, removing the need for the other parameters?
   def deleteComment(bankId : BankId, accountId : AccountId, transactionId: TransactionId)(commentId : String) : Box[Unit]
   
 }
