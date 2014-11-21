701c937c06b99c80a351c96565a699d80af56084
Everett Sochowski <everett@tesobe.com>
2014-11-21 11:33:32 +0100

    Add a relation db backed model for bank accounts

---
+++ src/main/scala/code/model/dataAccess/MappedBankAccount.scala
@@ -0,0 +1,60 @@
+package code.model.dataAccess
+
+import java.math.MathContext
+
+import code.model._
+import net.liftweb.mapper._
+
+class MappedBankAccount extends BankAccount with LongKeyedMapper[MappedBankAccount] with IdPK with CreatedUpdated {
+
+  override def getSingleton = MappedBankAccount
+
+  object bank extends MappedString(this, 255)
+  object theAccountId extends MappedString(this, 255)
+  object accountIban extends MappedString(this, 50)
+  object accountCurrency extends MappedString(this, 10)
+  object accountSwiftBic extends MappedString(this, 50)
+  object accountNumber extends MappedString(this, 50)
+
+  @deprecated
+  object holder extends MappedString(this, 100)
+
+  //this is the smallest unit of currency! e.g. cents, yen, pence, øre, etc.
+  object accountBalance extends MappedLong(this)
+
+  object accountName extends MappedString(this, 255)
+  object kind extends MappedString(this, 40)
+  object accountLabel extends MappedString(this, 255)
+
+  override def accountId: AccountId = AccountId(theAccountId.get)
+  override def iban: Option[String] = {
+    val i = accountIban.get
+    if(i.isEmpty) None else Some(i)
+  }
+  override def bankId: BankId = BankId(bank.get)
+  override def currency: String = accountCurrency.get
+  override def swift_bic: Option[String] = {
+    val sb = accountSwiftBic.get
+    if(sb.isEmpty) None else Some(sb)
+  }
+  override def number: String = accountNumber.get
+  override def balance: BigDecimal = {
+    //this data was sourced from Wikipedia, so it might not all be correct,
+    //and some banking systems may still retain different units (e.g. CZK?)
+    val decimalPlaces = currency match {
+      //TODO: handle MRO and MGA, which are non-decimal
+      case "CZK" | "JPY" | "KRW" => 0
+      case "KWD" | "OMR" => 3
+      case _ => 2
+    }
+    BigDecimal(accountBalance, decimalPlaces)
+  }
+  override def name: String = accountName.get
+  override def accountType: String = kind.get
+  override def label: String = accountLabel.get
+  override def accountHolder: String = holder.get
+}
+
+object MappedBankAccount extends MappedBankAccount with LongKeyedMetaMapper[MappedBankAccount] {
+  override def dbIndexes = UniqueIndex(bank, theAccountId) :: super.dbIndexes
+}
