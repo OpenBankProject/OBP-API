2cfcd3d0916ed10940d3c89f827a45664e6272d6
Everett Sochowski <everett@tesobe.com>
2014-12-01 17:09:56 +0100

    Work on refactoring data import to work better with multiple account/transaction/bank etc. implementations

--- src/main/scala/code/sandbox/LocalConnectorDataImport.scala
+++ src/main/scala/code/sandbox/LocalConnectorDataImport.scala
@@ -1,7 +1,7 @@
 package code.sandbox
 
 import code.metadata.counterparties.{MongoCounterparties, Metadata}
-import code.model.{ViewUID, ViewId, AccountId, BankId}
+import code.model._
 import code.model.dataAccess._
 import code.util.Helper
 import code.views.Views
@@ -13,7 +13,270 @@ import net.liftweb.util.Helpers._
 /**
  * Imports data into the format used by LocalConnector (e.g. HostedBank)
  */
-object LocalConnectorDataImport extends OBPDataImport with Loggable {
+object LocalConnectorDataImport extends OBPDataImport {
+
+  type BankType = HostedBank
+  type AccountType = Account
+  type TransactionType = OBPEnvelope
+  type MetadataType = Metadata
+
+  //TODO: this only works after createdUsers have been saved (and thus an APIUser has been created
+  protected def tmpSetAccountOwner(owner : AccountOwnerEmail, account: AccountType, createdUsers: List[OBPUser]) : Unit = {
+    val apiUserOwner = createdUsers.find(obpUser => owner == obpUser.email.get).flatMap(_.user.obj)
+
+    apiUserOwner match {
+      case Some(o) => {
+        MappedAccountHolder.create
+          .user(o)
+          .accountBankPermalink(account.bankId.value)
+          .accountPermalink(account.accountId.value).save
+      }
+      case None => {
+        //This shouldn't happen as OBPUser should generate the APIUsers when saved
+        logger.error(s"api user(s) with email $owner not found.")
+        logger.error("Data import completed with errors.")
+      }
+    }
+  }
+
+  protected def createSaveableBanks(data : List[SandboxBankImport]) : Box[List[Saveable[BankType]]] = {
+    val hostedBanks = data.map(b => {
+      HostedBank.createRecord
+        .permalink(b.id)
+        .name(b.full_name)
+        .alias(b.short_name)
+        .website(b.website)
+        .logoURL(b.logo)
+        .national_identifier(b.id) //this needs to match up with what goes in the OBPEnvelopes
+    })
+
+    val validationErrors = hostedBanks.flatMap(_.validate)
+
+    if(!validationErrors.isEmpty) {
+      Failure(s"Errors: ${validationErrors.map(_.msg)}")
+    } else {
+
+      def asSaveable(b : HostedBank) = {
+        new Saveable[HostedBank] {
+          val value = b
+          def save() : Unit = {
+            value.save()
+          }
+        }
+      }
+
+      Full(hostedBanks.map(asSaveable))
+    }
+  }
+
+  protected def createSaveableAccountResults(data : SandboxDataImport, banks : List[BankType], users : List[OBPUser]) : Box[List[(Saveable[Account], List[Saveable[ViewImpl]], List[AccountOwnerEmail])]] = {
+    def getHostedBank(acc : SandboxAccountImport) = Box(banks.find(b => b.permalink.get == acc.bank))
+
+    //TODO: refactor Saveable
+    def asSaveableAccount(acc : Account, bank : HostedBank) = new Saveable[Account] {
+      val value = acc
+      def save() = {
+        //TODO: need to ensure HostedBank got saved first
+        value.bankID(bank.id.get)
+        value.save(true)
+      }
+    }
+
+    //TODO: refactor Saveable
+    def asSaveableViewImpl(viewImpl : ViewImpl) = new Saveable[ViewImpl] {
+      val value = viewImpl
+      def save() = value.save
+    }
+
+    val results = data.accounts.map(acc => {
+      for {
+        hBank <- getHostedBank(acc) ?~ {
+          logger.warn("hosted bank not found")
+          "Server error"
+        }
+        balance <- tryo{BigDecimal(acc.balance.amount)} ?~ s"Invalid balance: ${acc.balance.amount}"
+        ownersNonEmpty <- Helper.booleanToBox(acc.owners.nonEmpty) ?~
+          s"Accounts must have at least one owner. Violation: (bank id ${acc.bank}, account id ${acc.id})"
+        ownersDefinedInDataImport <- Helper.booleanToBox(acc.owners.forall(ownerEmail => data.users.exists(u => u.email == ownerEmail))) ?~ {
+          val violations = acc.owners.filter(ownerEmail => !data.users.exists(u => u.email == ownerEmail))
+          s"Accounts must have owner(s) defined in data import. Violation: ${violations.mkString(",")}"
+        }
+        accId = AccountId(acc.id)
+        bankId = BankId(acc.bank)
+        ownerViewDoesNotExist <- Helper.booleanToBox(Views.views.vend.view(ViewUID(ViewId("owner"), bankId, accId)).isEmpty) ?~ {
+          s"owner view for account ${acc.id} at bank ${acc.bank} already exists"
+        }
+        publicViewDoesNotExist <- Helper.booleanToBox(Views.views.vend.view(ViewUID(ViewId("public"), bankId, accId)).isEmpty) ?~ {
+          s"public view for account ${acc.id} at bank ${acc.bank} already exists"
+        }
+      } yield {
+        val account = Account.createRecord
+          .permalink(acc.id)
+          .bankID(hBank.id.get)
+          .accountLabel(acc.label)
+          .accountCurrency(acc.balance.currency)
+          .accountBalance(balance)
+          .accountNumber(acc.number)
+          .kind(acc.`type`)
+          .accountIban(acc.IBAN)
+
+        val bankId = BankId(acc.bank)
+        val accountId = AccountId(acc.id)
+
+        val ownerView = ViewImpl.unsavedOwnerView(bankId, accountId, "Owner View")
+
+        val publicView =
+          if(acc.generate_public_view) Some(ViewImpl.unsavedDefaultPublicView(bankId, accountId, "Public View"))
+          else None
+
+        val views = List(Some(ownerView), publicView).flatten
+
+        (asSaveableAccount(account, hBank), views.map(asSaveableViewImpl), acc.owners)
+      }
+    })
+
+    dataOrFirstFailure(results)
+  }
+
+  protected def createSaveableTransactionsAndMetas(transactions : List[SandboxTransactionImport], createdBanks : List[BankType], createdAccounts : List[AccountType]):
+    Box[(List[Saveable[TransactionType]], List[Saveable[MetadataType]])] = {
+
+    def saveableEnv(e : OBPEnvelope) = new Saveable[OBPEnvelope] {
+      val value = e
+      def save() = value.save(true)
+    }
+
+    def saveableMetadata(m : Metadata) = new Saveable[Metadata] {
+      val value = m
+      def save() = m.save(true)
+    }
+
+    // a bit ugly to have this as a var
+    var metadatasToSave : List[Metadata] = Nil
+
+    val envs : List[Box[OBPEnvelope]] = transactions.map(t => {
+
+      type Counterparty = String
+
+      def createMeta(holder : String, publicAlias : String, accountNumber : String) = {
+        Metadata.createRecord
+          .holder(holder)
+          .accountNumber(accountNumber)
+          .originalPartyAccountId(t.this_account.id)
+          .originalPartyBankId(t.this_account.bank)
+          .publicAlias(publicAlias)
+      }
+
+      def findExistingMetadata(counter : SandboxTransactionCounterparty) = {
+        //find by name and account number
+        counter.name match {
+          case Some(name) =>
+            metadatasToSave.find(m => {
+              m.holder.get == name &&
+                m.accountNumber.get == counter.account_number.getOrElse("")
+            })
+          case None => {
+            counter.account_number match {
+              case Some(accNum) =>
+                metadatasToSave.find(m => {
+                  m.accountNumber.get == accNum
+                })
+              case None => None
+            }
+          }
+        }
+      }
+
+      def generateNewMetadata(accountNumber : Option[String]) : Metadata = {
+        val holder = "unknown_" + UUID.randomUUID.toString
+        val publicAlias = MongoCounterparties.newPublicAliasName(BankId(t.this_account.bank), AccountId(t.this_account.id))
+        createMeta(holder, publicAlias, accountNumber.getOrElse(""))
+      }
+
+      def newMetadataFromCounterparty(counter : SandboxTransactionCounterparty) : Metadata = {
+        val counterAccNumber = counter.account_number.getOrElse("")
+        counter.name match {
+          case Some(n) if n.nonEmpty => {
+            val publicAlias = MongoCounterparties.newPublicAliasName(BankId(t.this_account.bank), AccountId(t.this_account.id))
+            createMeta(n, publicAlias, counterAccNumber)
+          }
+          case _ => generateNewMetadata(Some(counterAccNumber))
+        }
+      }
+
+      val metadata = t.counterparty match {
+        case Some(counter) => {
+          val existingMeta = findExistingMetadata(counter)
+          existingMeta.getOrElse(newMetadataFromCounterparty(counter))
+        }
+        case None => generateNewMetadata(None)
+      }
+
+      for {
+        createdBank <- Box(createdBanks.find(b => b.permalink.get == t.this_account.bank)) ?~
+          s"Transaction this_account bank must be specified in import banks. Unspecified bank: ${t.this_account.bank}"
+        //have to compare a.bankID to createdBank.id instead of just checking a.bankId against t.this_account.bank as createdBank hasn't been
+        //saved so the a.bankId method (which involves a db lookup) will not work
+        createdAcc <- Box(createdAccounts.find(a => a.bankID.toString == createdBank.id.get.toString && a.accountId == AccountId(t.this_account.id))) ?~
+          s"Transaction this_account account must be specified in import accounts. Unspecified account id: ${t.this_account.id} at bank: ${t.this_account.bank}"
+        newBalanceValue <- tryo{BigDecimal(t.details.new_balance)} ?~ s"Invalid new balance: ${t.details.new_balance}"
+        tValue <- tryo{BigDecimal(t.details.value)} ?~ s"Invalid transaction value: ${t.details.value}"
+        postedDate <- tryo{dateFormat.parse(t.details.posted)} ?~ s"Invalid date format: ${t.details.posted}. Expected pattern $datePattern"
+        completedDate <-tryo{dateFormat.parse(t.details.completed)} ?~ s"Invalid date format: ${t.details.completed}. Expected pattern $datePattern"
+      } yield {
+
+        //bankNationalIdentifier not available from  createdAcc.bankNationalIdentifier as it hasn't been saved so we get it from createdBank
+        val obpThisAccountBank = OBPBank.createRecord
+          .national_identifier(createdBank.national_identifier.get)
+
+        val obpThisAccount = OBPAccount.createRecord
+          .holder(createdAcc.holder.get)
+          .number(createdAcc.accountNumber.get)
+          .kind(createdAcc.kind.get)
+          .bank(obpThisAccountBank)
+
+        val counterpartyAccountNumber = t.counterparty.flatMap(_.account_number)
+
+        val obpOtherAccount = OBPAccount.createRecord
+          .holder(metadata.holder.get)
+          .number(counterpartyAccountNumber.getOrElse(""))
+
+        val newBalance = OBPBalance.createRecord
+          .amount(newBalanceValue)
+          .currency(createdAcc.accountCurrency.get)
+
+        val transactionValue = OBPValue.createRecord
+          .amount(tValue)
+          .currency(createdAcc.accountCurrency.get)
+
+        val obpDetails = OBPDetails.createRecord
+          .completed(completedDate)
+          .posted(postedDate)
+          .kind(t.details.`type`)
+          .label(t.details.description)
+          .new_balance(newBalance)
+          .value(transactionValue)
+
+
+        val obpTransaction = OBPTransaction.createRecord
+          .details(obpDetails)
+          .this_account(obpThisAccount)
+          .other_account(obpOtherAccount)
+
+        val env = OBPEnvelope.createRecord
+          .transactionId(t.id)
+          .obp_transaction(obpTransaction)
+
+        //add the metadatas to the list of them to save
+        metadatasToSave = metadata :: metadatasToSave
+        env
+      }
+
+    })
+
+    val envelopes = dataOrFirstFailure(envs)
+    envelopes.map(es => (es.map(saveableEnv), metadatasToSave.map(saveableMetadata)))
+  }
 
   /**
    * @param data
@@ -189,20 +452,6 @@ object LocalConnectorDataImport extends OBPDataImport with Loggable {
 
     }
 
-    /**
-     * Takes a list of boxes and returns a list of the content of the boxes if all boxes are Full, or returns
-     * the first failure
-     *
-     * TODO: handle Empty boxes
-     */
-    def dataOrFirstFailure[T](boxes : List[Box[T]]) = {
-      val firstFailure = boxes.collectFirst{case f: Failure => f}
-      firstFailure match {
-        case Some(f) => f
-        case None => Full(boxes.flatten) //no failures, so we can return the results
-      }
-    }
-
     // a bit ugly to have this as a var
     var metadatasToSave : List[Metadata] = Nil
 
@@ -214,12 +463,7 @@ object LocalConnectorDataImport extends OBPDataImport with Loggable {
           acc.accountId == AccountId(transaction.this_account.id) &&
             acc.bankId == BankId(transaction.this_account.bank))
 
-      val transactionsWithNoAccountSpecifiedInImport = data.transactions.flatMap(t => {
-        val createdAcc = createdAccount(t)
-        if(createdAcc.isDefined) Some(t)
-        else None
-      })
-
+      val transactionsWithNoAccountSpecifiedInImport = data.transactions.filter(createdAccount(_).isDefined)
       val transactionsWithEmptyIds = data.transactions.filter(_.id.isEmpty)
 
       case class TransactionIdentifier(id : String, account : String, bank : String)
--- src/main/scala/code/sandbox/OBPDataImport.scala
+++ src/main/scala/code/sandbox/OBPDataImport.scala
@@ -1,7 +1,12 @@
 package code.sandbox
 
 import java.text.SimpleDateFormat
-import net.liftweb.common.Box
+import code.bankconnectors.Connector
+import code.model.dataAccess.{MappedAccountHolder, ViewImpl, OBPUser}
+import code.model._
+import code.views.Views
+import net.liftweb.common.{Loggable, Full, Failure, Box}
+import net.liftweb.mapper.By
 import net.liftweb.util.SimpleInjector
 
 object OBPDataImport extends SimpleInjector {
@@ -12,16 +17,219 @@ object OBPDataImport extends SimpleInjector {
 
 }
 
-trait OBPDataImport {
+//TODO: refactor Saveable
+trait Saveable[T] {
+  val value : T
+  def save() : Unit
+}
+
+
+trait OBPDataImport extends Loggable {
   val datePattern = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
   val dateFormat = new SimpleDateFormat(datePattern)
+
+  type BankType <: Bank
+  type AccountType <: BankAccount
+  type MetadataType <: OtherBankAccountMetadata
+  type TransactionType
+  type AccountOwnerEmail = String
+
+  /**
+   * Takes a list of boxes and returns a list of the content of the boxes if all boxes are Full, or returns
+   * the first failure
+   *
+   * TODO: handle Empty boxes
+   */
+  protected def dataOrFirstFailure[T](boxes : List[Box[T]]) = {
+    val firstFailure = boxes.collectFirst{case f: Failure => f}
+    firstFailure match {
+      case Some(f) => f
+      case None => Full(boxes.flatten) //no failures, so we can return the results
+    }
+  }
+
+  protected def tmpCreateBanks(data : SandboxDataImport) = {
+    val existing = data.banks.flatMap(b => Connector.connector.vend.getBanks)
+
+    val allIds = data.banks.map(_.id)
+    val emptyIds = allIds.filter(_.isEmpty)
+    val uniqueIds = data.banks.map(_.id).distinct
+    val duplicateIds = allIds diff uniqueIds
+
+    if(!existing.isEmpty) {
+      val existingIds = existing.map(_.bankId.value)
+      Failure(s"Bank(s) with id(s) $existingIds already exist (and may have different non-id [e.g. short_name] values).")
+    } else if (!emptyIds.isEmpty){
+      Failure(s"Bank(s) with empty ids are not allowed")
+    } else if(!duplicateIds.isEmpty) {
+      Failure(s"Banks must have unique ids. Duplicates found: $duplicateIds")
+    } else {
+      createSaveableBanks(data.banks)
+    }
+  }
+
+  protected def createSaveableBanks(data : List[SandboxBankImport]) : Box[List[Saveable[BankType]]]
+
+  //TODO: remove dependency on OBPUser
+  protected def tmpCreateUsers(data : SandboxDataImport) : Box[List[Saveable[OBPUser]]] = {
+    val existing = data.users.flatMap(u => OBPUser.find(By(OBPUser.email, u.email)))
+    val allEmails = data.users.map(_.email)
+    val duplicateEmails = allEmails diff allEmails.distinct
+
+    if(!existing.isEmpty) {
+      val existingEmails = existing.map(_.email.get)
+      Failure(s"User(s) with email(s) $existingEmails already exist (and may be different (e.g. different display_name)")
+    } else if(!duplicateEmails.isEmpty) {
+      Failure(s"Users must have unique emails: Duplicates found: $duplicateEmails")
+    }else {
+
+      val obpUsers = data.users.map(u => {
+        OBPUser.create
+          .email(u.email)
+          .lastName(u.display_name)
+          .password(u.password)
+          .validated(true)
+      })
+
+      val validationErrors = obpUsers.flatMap(_.validate)
+
+      if(!validationErrors.isEmpty) {
+        Failure(s"Errors: ${validationErrors.map(_.msg)}")
+      } else {
+
+        def asSaveable(u : OBPUser) = new Saveable[OBPUser] {
+          val value = u
+          def save() = u.save()
+        }
+
+        Full(obpUsers.map(asSaveable))
+      }
+    }
+  }
+
+  protected def tmpCreateAccounts(data : SandboxDataImport, banks : List[BankType], users : List[OBPUser]) : Box[List[(Saveable[AccountType], List[Saveable[ViewImpl]], List[AccountOwnerEmail])]] = {
+
+    val banksNotSpecifiedInImport = data.accounts.flatMap(acc => {
+      if(data.banks.exists(b => b.id == acc.bank)) None
+      else Some(acc.bank)
+    })
+
+    val emptyAccountIds = data.accounts.filter(acc => acc.id.isEmpty)
+
+    case class AccountIdentifier(id : String, bank : String)
+    case class AccountNumberForBank(number : String, bank : String)
+    val ids = data.accounts.map(acc => AccountIdentifier(acc.id, acc.bank))
+    val duplicateIds = ids diff ids.distinct
+
+    val numbers = data.accounts.map(acc =>AccountNumberForBank(acc.number, acc.bank))
+    val duplicateNumbers = numbers diff numbers.distinct
+
+    val existing = data.accounts.flatMap(acc => {
+      Connector.connector.vend.getBankAccount(BankId(acc.bank), AccountId(acc.id))
+    })
+
+    if(!banksNotSpecifiedInImport.isEmpty) {
+      Failure(s"Error: one or more accounts specified are for" +
+        s" banks not specified in the import data. Unspecified banks: $banksNotSpecifiedInImport)")
+    } else if (emptyAccountIds.nonEmpty){
+      Failure(s"Error: one or more accounts has an empty id")
+    } else if (duplicateIds.nonEmpty){
+      val duplicateMsg = duplicateIds.map(d => s"(bank id ${d.bank}, account id: ${d.id})").mkString(",")
+      Failure(s"Error: accounts at the same bank may not share an id: $duplicateMsg")
+    } else if(duplicateNumbers.nonEmpty){
+      val duplicateMsg = duplicateNumbers.map(d => s"(bank id ${d.bank}, account number: ${d.number})").mkString(",")
+      Failure(s"Error: accounts at the same bank may not share account numbers: $duplicateMsg")
+    } else if(existing.nonEmpty) {
+      val existingAccountAndBankIds = existing.map(e => (s"(account id: ${e.accountId.value} bank id: ${e.bankId.value})").mkString(","))
+      Failure(s"Account(s) to be imported already exist: $existingAccountAndBankIds")
+    } else {
+      createSaveableAccountResults(data, banks, users)
+    }
+  }
+
+  protected def createSaveableAccountResults(data : SandboxDataImport, banks : List[BankType], users : List[OBPUser]) : Box[List[(Saveable[AccountType], List[Saveable[ViewImpl]], List[AccountOwnerEmail])]]
+
+  protected def createSaveableTransactionsAndMetas(transactions : List[SandboxTransactionImport], createdBanks : List[BankType], createdAccounts : List[AccountType]):
+    Box[(List[Saveable[TransactionType]], List[Saveable[MetadataType]])]
+
+  protected def tmpCreateTransactionsAndMetas(data : SandboxDataImport, createdBanks : List[BankType], createdAccounts : List[AccountType]) : Box[(List[Saveable[TransactionType]], List[Saveable[MetadataType]])] = {
+    def createdAccount(transaction : SandboxTransactionImport) =
+      createdAccounts.find(acc =>
+        acc.accountId == AccountId(transaction.this_account.id) &&
+          acc.bankId == BankId(transaction.this_account.bank))
+
+    val transactionsWithNoAccountSpecifiedInImport = data.transactions.filter(createdAccount(_).isDefined)
+    val transactionsWithEmptyIds = data.transactions.filter(_.id.isEmpty)
+
+    case class TransactionIdentifier(id : String, account : String, bank : String)
+
+    val identifiers = data.transactions.map(t => TransactionIdentifier(t.id, t.this_account.id, t.this_account.bank))
+    val duplicateIdentifiers = identifiers diff identifiers.distinct
+
+    val existing = data.transactions.filter(t => {
+      Connector.connector.vend.getTransaction(BankId(t.this_account.bank), AccountId(t.this_account.id), TransactionId(t.id)).isDefined
+    })
+
+    if(transactionsWithNoAccountSpecifiedInImport.nonEmpty) {
+      val identifiers = transactionsWithNoAccountSpecifiedInImport.map(
+        t => s"(transaction id ${t.id}, account id ${t.this_account.id}, bank id ${t.this_account.bank})")
+      Failure(s"Transaction(s) exist with accounts/banks not specified in import data: $identifiers")
+    } else if (transactionsWithEmptyIds.nonEmpty) {
+      Failure(s"Transaction(s) exist with empty ids")
+    } else if(duplicateIdentifiers.nonEmpty) {
+      val duplicatesMsg = duplicateIdentifiers.map(i => s"(transaction id : ${i.id}, account id: ${i.account}, bank id: ${i.bank})").mkString(",")
+      Failure(s"Transactions for an account must have unique ids. Violations: ${duplicatesMsg} ")
+    } else if(existing.nonEmpty) {
+      val existingIdentifiers = existing.map { t =>
+        s"(transaction id: ${t.id} account id : ${t.this_account.id} bank id : ${t.this_account.bank})"
+      }
+      Failure(s"Some transactions already exist: ${existingIdentifiers.mkString("[", ",", "]")}")
+    } else {
+      //TODO validate numbers and dates in one place
+      createSaveableTransactionsAndMetas(data.transactions, createdBanks, createdAccounts)
+    }
+  }
+
+  //TODO: have this return a saveable something?
+  protected def tmpSetAccountOwner(owner : AccountOwnerEmail, account: AccountType, createdUsers: List[OBPUser]) : Unit
+
+  def tmpImportData(data: SandboxDataImport) : Box[Unit] = {
+    for {
+      banks <- tmpCreateBanks(data)
+      users <- tmpCreateUsers(data)
+      accountResults <- tmpCreateAccounts(data, banks.map(_.value), users.map(_.value))
+      (transactions, metadatas) <- tmpCreateTransactionsAndMetas(data, banks.map(_.value), accountResults.map(_._1.value))
+    } yield {
+      banks.foreach(_.save())
+
+      users.foreach(_.save())
+
+      accountResults.foreach {
+        case (account, views, accOwnerEmails) =>
+          account.save()
+          views.foreach(_.save())
+
+          views.map(_.value).filterNot(_.isPublic).foreach(v => {
+            //grant the owner access to non-public views
+            //this should always find the owners as that gets verified at an earlier stage, but it's not perfect this way
+            val accOwners = users.map(_.value).filter(u => accOwnerEmails.exists(email => u.email.get == email)).flatMap(_.user.obj)
+            accOwners.foreach(Views.views.vend.addPermission(v.uid, _))
+          })
+
+          accOwnerEmails.foreach(tmpSetAccountOwner(_, account.value, users.map(_.value)))
+      }
+
+      transactions.foreach(_.save())
+      metadatas.foreach(_.save())
+    }
+  }
 
   /**
    * @param data
    * @return A full box if the import worked, or else a failure describing what went wrong
    */
   //TODO: might be nice to use something like scalaz's validations here
-  def importData(data : SandboxDataImport) : Box[Unit]
+  //def importData(data : SandboxDataImport) : Box[Unit]
 }
 
 
