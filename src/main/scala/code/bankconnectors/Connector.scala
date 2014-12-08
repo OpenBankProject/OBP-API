f85ff704f3363a1308c361e82aa19bd0f7bcdc1a
Everett Sochowski <everett@tesobe.com>
2014-12-08 16:58:16 +0100

    Simplify payments by integrating them into the Connetor trait

--- src/main/scala/code/bankconnectors/Connector.scala
+++ src/main/scala/code/bankconnectors/Connector.scala
@@ -1,22 +1,19 @@
 package code.bankconnectors
 
-import code.payments.PaymentProcessor
+import code.util.Helper._
 import net.liftweb.common.Box
 import code.model._
 import net.liftweb.util.SimpleInjector
 import code.model.User
-import code.model.ModeratedOtherBankAccount
 import code.model.OtherBankAccount
-import code.model.ModeratedTransaction
 import code.model.Transaction
 import java.util.Date
-import scala.Some
 
 object Connector  extends SimpleInjector {
 
   val connector = new Inject(buildOne _) {}
 
-  def buildOne: Connector with PaymentProcessor = LocalConnector
+  def buildOne: Connector = LocalConnector
 
 }
 
@@ -66,5 +63,33 @@ trait Connector {
   
   //gets the users who are the legal owners/holders of the account
   def getAccountHolders(bankId: BankId, accountID: AccountId) : Set[User]
-  //...
+
+
+  //Payments api: just return Failure("not supported") from makePaymentImpl if you don't want to implement it
+  /**
+   * \
+   *
+   * @param initiator The user attempting to make the payment
+   * @param fromAccountUID The unique identifier of the account sending money
+   * @param toAccountUID The unique identifier of the account receiving money
+   * @param amt The amount of money to send ( > 0 )
+   * @return The id of the sender's new transaction,
+   */
+  def makePayment(initiator : User, fromAccountUID : BankAccountUID, toAccountUID : BankAccountUID, amt : BigDecimal) : Box[TransactionId] = {
+    for{
+      fromAccount <- getBankAccountType(fromAccountUID.bankId, fromAccountUID.accountId) ?~
+        s"account ${fromAccountUID.accountId} not found at bank ${fromAccountUID.bankId}"
+      isOwner <- booleanToBox(initiator.ownerAccess(fromAccount), "user does not have access to owner view")
+      toAccount <- getBankAccountType(toAccountUID.bankId, toAccountUID.accountId) ?~
+        s"account ${toAccountUID.accountId} not found at bank ${toAccountUID.bankId}"
+      sameCurrency <- booleanToBox(fromAccount.currency == toAccount.currency, {
+        s"Cannot send payment to account with different currency (From ${fromAccount.currency} to ${toAccount.currency}"
+      })
+      isPositiveAmtToSend <- booleanToBox(amt > BigDecimal("0"), s"Can't send a payment with a value of 0 or less. ($amt)")
+      //TODO: verify the amount fits with the currency -> e.g. 12.543 EUR not allowed, 10.00 JPY not allowed, 12.53 EUR allowed
+      transactionId <- makePaymentImpl(fromAccount, toAccount, amt)
+    } yield transactionId
+  }
+
+  protected def makePaymentImpl(fromAccount : AccountType, toAccount : AccountType, amt : BigDecimal) : Box[TransactionId]
 }
--- src/main/scala/code/bankconnectors/LocalConnector.scala
+++ src/main/scala/code/bankconnectors/LocalConnector.scala
@@ -1,29 +1,32 @@
 package code.bankconnectors
 
+import java.text.SimpleDateFormat
+import java.util.TimeZone
+
 import code.payments.{SandboxLocalConnectorPayments, PaymentProcessor}
-import net.liftweb.common.Box
+import net.liftweb.common.{Failure, Box, Loggable, Full}
+import net.liftweb.json.JsonAST.JValue
 import scala.concurrent.ops.spawn
 import code.model._
 import code.model.dataAccess._
 import net.liftweb.mapper.By
-import net.liftweb.common.Loggable
+import net.liftweb.mongodb.BsonDSL._
 import org.bson.types.ObjectId
 import net.liftweb.util.Helpers._
 import net.liftweb.util.Props
 import com.mongodb.QueryBuilder
 import code.metadata.counterparties.{Counterparties, MongoCounterparties, Metadata}
-import net.liftweb.common.Full
 import com.tesobe.model.UpdateBankAccount
 
-private object LocalConnector extends Connector with Loggable with SandboxLocalConnectorPayments {
+private object LocalConnector extends Connector with Loggable {
 
   type AccountType = Account
 
-  def getBank(bankId : BankId): Box[Bank] =
+  override def getBank(bankId : BankId): Box[Bank] =
     getHostedBank(bankId)
 
   //gets banks handled by this connector
-  def getBanks : List[Bank] =
+  override def getBanks : List[Bank] =
     HostedBank.findAll
 
   override def getBankAccountType(bankId : BankId, accountId : AccountId) : Box[Account] = {
@@ -34,7 +37,7 @@ private object LocalConnector extends Connector with Loggable with SandboxLocalC
   }
 
 
-  def getOtherBankAccount(bankId: BankId, accountId : AccountId, otherAccountID : String): Box[OtherBankAccount] = {
+  override def getOtherBankAccount(bankId: BankId, accountId : AccountId, otherAccountID : String): Box[OtherBankAccount] = {
 
     /**
      * In this implementation (for legacy reasons), the "otherAccountID" is actually the mongodb id of the
@@ -67,7 +70,7 @@ private object LocalConnector extends Connector with Loggable with SandboxLocalC
       }
   }
 
-  def getOtherBankAccounts(bankId: BankId, accountId : AccountId): List[OtherBankAccount] = {
+  override def getOtherBankAccounts(bankId: BankId, accountId : AccountId): List[OtherBankAccount] = {
 
     /**
      * In this implementation (for legacy reasons), the "otherAccountID" is actually the mongodb id of the
@@ -94,7 +97,7 @@ private object LocalConnector extends Connector with Loggable with SandboxLocalC
     })
   }
 
-  def getTransactions(bankId: BankId, accountId: AccountId, queryParams: OBPQueryParam*): Box[List[Transaction]] = {
+  override def getTransactions(bankId: BankId, accountId: AccountId, queryParams: OBPQueryParam*): Box[List[Transaction]] = {
     logger.debug("getTransactions for " + bankId + "/" + accountId)
     for{
       bank <- getHostedBank(bankId)
@@ -105,7 +108,7 @@ private object LocalConnector extends Connector with Loggable with SandboxLocalC
     }
   }
 
-  def getTransaction(bankId: BankId, accountId : AccountId, transactionId : TransactionId): Box[Transaction] = {
+  override def getTransaction(bankId: BankId, accountId : AccountId, transactionId : TransactionId): Box[Transaction] = {
     for{
       bank <- getHostedBank(bankId) ?~! s"Transaction not found: bank $bankId not found"
       account  <- bank.getAccount(accountId) ?~! s"Transaction not found: account $accountId not found"
@@ -116,21 +119,36 @@ private object LocalConnector extends Connector with Loggable with SandboxLocalC
     }
   }
 
-  def getPhysicalCards(user : User) : Set[PhysicalCard] = {
+  override def getPhysicalCards(user : User) : Set[PhysicalCard] = {
     Set.empty
   }
 
-  def getPhysicalCardsForBank(bankId: BankId, user : User) : Set[PhysicalCard] = {
+  override def getPhysicalCardsForBank(bankId: BankId, user : User) : Set[PhysicalCard] = {
     Set.empty
   }
 
-  def getAccountHolders(bankId: BankId, accountID: AccountId) : Set[User] = {
+  override def getAccountHolders(bankId: BankId, accountID: AccountId) : Set[User] = {
     MappedAccountHolder.findAll(
       By(MappedAccountHolder.accountBankPermalink, bankId.value),
       By(MappedAccountHolder.accountPermalink, accountID.value)).map(accHolder => accHolder.user.obj).flatten.toSet
   }
 
-    private def createTransaction(env: OBPEnvelope, theAccount: Account): Transaction = {
+  override protected def makePaymentImpl(fromAccount : Account, toAccount : Account, amt : BigDecimal) : Box[TransactionId] = {
+    val fromTransAmt = -amt //from account balance should decrease
+    val toTransAmt = amt //to account balance should increase
+
+    //this is the transaction that gets attached to the account of the person making the payment
+    val createdFromTrans = saveNewTransaction(fromAccount, toAccount, fromTransAmt)
+
+    // this creates the transaction that gets attached to the account of the person receiving the payment
+    saveNewTransaction(toAccount, fromAccount, toTransAmt)
+
+    //assumes OBPEnvelope id is what gets used as the Transaction id in the API. If that gets changed, this needs to
+    //be updated (the tests should fail if it doesn't)
+    createdFromTrans.map(t => TransactionId(t.transactionId.get))
+  }
+
+  private def createTransaction(env: OBPEnvelope, theAccount: Account): Transaction = {
     val transaction: OBPTransaction = env.obp_transaction.get
     val otherAccount_ = transaction.other_account.get
 
@@ -190,6 +208,74 @@ private object LocalConnector extends Connector with Loggable with SandboxLocalC
       startDate,
       finishDate,
       balance)
+  }
+
+  private def saveNewTransaction(account : Account, otherAccount : Account, amount : BigDecimal) : Box[OBPEnvelope] = {
+
+    val oldBalance = account.balance
+
+    def saveAndUpdateAccountBalance(transactionJS : JValue, thisAccount : Account) : Box[OBPEnvelope] = {
+
+      val envelope: Box[OBPEnvelope] = OBPEnvelope.envlopesFromJvalue(transactionJS)
+
+      if(envelope.isDefined) {
+        val e : OBPEnvelope = envelope.get
+        logger.debug(s"Updating current balance for ${thisAccount.bankName} / ${thisAccount.accountNumber} / ${thisAccount.accountType}")
+        thisAccount.accountBalance(e.obp_transaction.get.details.get.new_balance.get.amount.get).save
+        logger.debug("Saving new transaction")
+        Full(e.save)
+      } else {
+        Failure("couldn't save transaction")
+      }
+    }
+
+    for {
+      otherBank <- Connector.connector.vend.getBank(otherAccount.bankId) ?~! "no other bank found"
+      transTime = now
+      //mongodb/the lift mongo thing wants a literal Z in the timestamp, apparently
+      envJsonDateFormat = {
+        val simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'")
+        simpleDateFormat.setTimeZone(TimeZone.getTimeZone("UTC"))
+        simpleDateFormat
+      }
+
+      envJson =
+      ("obp_transaction" ->
+        ("this_account" ->
+          ("holder" -> account.owners.headOption.map(_.name).getOrElse("")) ~ //TODO: this is rather fragile...
+            ("number" -> account.number) ~
+            ("kind" -> account.accountType) ~
+            ("bank" ->
+              ("IBAN" -> account.iban.getOrElse("")) ~
+                ("national_identifier" -> account.nationalIdentifier) ~
+                ("name" -> account.bankId.value))) ~
+          ("other_account" ->
+            ("holder" -> otherAccount.accountHolder) ~
+              ("number" -> otherAccount.number) ~
+              ("kind" -> otherAccount.accountType) ~
+              ("bank" ->
+                ("IBAN" -> "") ~
+                  ("national_identifier" -> otherBank.nationalIdentifier) ~
+                  ("name" -> otherBank.fullName))) ~
+          ("details" ->
+            ("type_en" -> "") ~
+              ("type_de" -> "") ~
+              ("posted" ->
+                ("$dt" -> envJsonDateFormat.format(transTime))
+                ) ~
+              ("completed" ->
+                ("$dt" -> envJsonDateFormat.format(transTime))
+                ) ~
+              ("new_balance" ->
+                ("currency" -> account.currency) ~
+                  ("amount" -> (oldBalance + amount).toString)) ~
+              ("value" ->
+                ("currency" -> account.currency) ~
+                  ("amount" -> amount.toString))))
+      saved <- saveAndUpdateAccountBalance(envJson, account)
+    } yield {
+      saved
+    }
   }
 
   /**
