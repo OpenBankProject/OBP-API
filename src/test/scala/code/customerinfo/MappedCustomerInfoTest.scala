2dda3a35c34f9a3d7df251457d3806d93a546f90
Everett Sochowski <everett@tesobe.com>
2015-01-31 20:15:07 +0100

    Ensure test MappedCustomerInfo exists before tests are run

--- src/test/scala/code/customerinfo/MappedCustomerInfoTest.scala
+++ src/test/scala/code/customerinfo/MappedCustomerInfoTest.scala
@@ -11,11 +11,10 @@ class MappedCustomerInfoProviderTest extends ServerSetup with DefaultUsers {
 
   val testBankId = BankId("bank")
 
-  lazy val customerInfo1 = MappedCustomerInfo.create
+  val customerInfo1 = MappedCustomerInfo.create
     .mBank(testBankId.value).mEmail("bob@example.com").mFaceImageTime(new Date(12340000))
     .mFaceImageUrl("http://example.com/image.jpg").mLegalName("John Johnson")
     .mMobileNumber("12343434").mNumber("343").mUser(obpuser1).saveMe
-
 
   feature("Getting customer info") {
 
@@ -32,7 +31,7 @@ class MappedCustomerInfoProviderTest extends ServerSetup with DefaultUsers {
 
     scenario("Customer info exists and we try to get it") {
       Given("MappedCustomerInfo exists for a user")
-      MappedCustomerInfo.find(By(MappedCustomerInfo.mUser, obpuser1)).isDefined should equal(true)
+      MappedCustomerInfo.find(By(MappedCustomerInfo.mUser, obpuser1.apiId.value)).isDefined should equal(true)
 
       When("We try to get it")
       val foundOpt = MappedCustomerInfoProvider.getInfo(testBankId, obpuser1)
@@ -48,8 +47,7 @@ class MappedCustomerInfoProviderTest extends ServerSetup with DefaultUsers {
 
 
   override def beforeAll() = {
-    super.beforeAll
-    MappedCustomerInfo.bulkDelete_!!()
+    super.beforeAll()
   }
 
   override def afterAll() = {
