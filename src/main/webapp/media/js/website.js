914675c9307955875fe8baa12fa5b13e1965ec73
Stefan Bethge <stefan+github@lanpartei.de>
2015-05-16 18:48:45 +0200

    take over some authorize form changes from ulster branch

--- src/main/webapp/media/js/website.js
+++ src/main/webapp/media/js/website.js
@@ -1,3 +1,29 @@
 $(document).ready(function() {
+    //fallback for html5 placeholder
+    if ( !("placeholder" in document.createElement("input")) ) {
+        $("input[placeholder], textarea[placeholder]").each(function() {
+            var val = $(this).attr("placeholder");
+            if ( this.value == "" ) {
+                this.value = val;
+            }
+            $(this).focus(function() {
+                if ( this.value == val ) {
+                    this.value = "";
+                }
+            }).blur(function() {
+                if ( $.trim(this.value) == "" ) {
+                    this.value = val;
+                }
+            })
+        });
 
+        // Clear default placeholder values on form submit
+        $('form').submit(function() {
+            $(this).find("input[placeholder], textarea[placeholder]").each(function() {
+                if ( this.value == $(this).attr("placeholder") ) {
+                    this.value = "";
+                }
+            });
+        });
+    }
 });
