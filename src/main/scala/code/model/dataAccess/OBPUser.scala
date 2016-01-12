7b31953af3e573d5c8fc49d2333554e393ef3835
Petar Bozin <petar.bozin@md-softwaredesign.de>
2016-01-12 20:16:25 +0100

    Code cleanup

--- src/main/scala/code/model/dataAccess/OBPUser.scala
+++ src/main/scala/code/model/dataAccess/OBPUser.scala
@@ -340,7 +340,7 @@ import net.liftweb.util.Helpers._
                     val newUser = OBPUser.create
                       .firstName(extDisplayName)
                       .email(extEmail)
-                      .password(dummyPassword)
+                      .password(extPassword) //dummyPassword)
                     // Assume that user's email is always validated if coming from Kafka
                     newUser.validated(true)
                     // Using email as display name
