c8c82f3123f8a061e96df175b2793ab8349b7f41
Petar Bozin <petar.bozin@md-softwaredesign.de>
2016-01-29 09:27:10 +0100

    Code cleanup - experimental code at the moment

--- src/main/scala/code/api/jwtauth.scala
+++ src/main/scala/code/api/jwtauth.scala
@@ -27,19 +27,16 @@ limitations under the License.
  */
 package code.api
 
-import java.net.{URLDecoder, URLEncoder}
 import java.util.Date
-import javax.crypto.Mac
-import javax.crypto.spec.SecretKeySpec
 
-import authentikat.jwt._
+import authentikat.jwt.{JwtHeader, JsonWebToken, JwtClaimsSet}
 import code.model.{Consumer, Token, TokenType, User}
 import net.liftweb.common.{Box, Empty, Full, Loggable, ParamFailure}
 import net.liftweb.http._
 import net.liftweb.http.rest.RestHelper
 import net.liftweb.mapper.By
-import net.liftweb.util.Helpers.{tryo, _}
-import net.liftweb.util.{Helpers, Props}
+import net.liftweb.util.Helpers
+import net.liftweb.util.Helpers._
 
 import scala.compat.Platform
 
@@ -59,11 +56,8 @@ object JWTAuth extends RestHelper with Loggable {
       //Test if the request is valid
       if(httpCode==200)
       {
-        //Generate the token and secret
-        val secret = Helpers.randomString(40)
-        val header = JwtHeader("HS256")
-        val claimsSet = JwtClaimsSet(Map("app" -> "key"))
-        val token: String = JsonWebToken(header, claimsSet, secret)
+        val claims = Map("app" -> "key")
+        val (token,secret) = generateTokenAndSecret(claims)
 
         //Save the token that we have generated
         if(saveRequestToken(jwtAuthParameters,token, secret))
@@ -87,7 +81,8 @@ object JWTAuth extends RestHelper with Loggable {
     if(httpCode==200)
     {
       //Generate the token and secret/token
-      val (token,secret) = generateTokenAndSecret()
+      val claims = Map("app" -> "key")
+      val (token,secret) = generateTokenAndSecret(claims)
       //Save the token that we have generated
       if(saveAuthorizationToken(jwtAuthParameters,token, secret))
       //remove the request token so the application could not exchange it
@@ -110,6 +105,7 @@ object JWTAuth extends RestHelper with Loggable {
     def getAllParameters : Map[String,String]= {
 
       def toMapFromBasicAuth(paramsEncoded : String) = {
+        println("basic=" + paramsEncoded )
         val params = new String(base64Decode(paramsEncoded.replaceAll("Basic ", "")))
         params.toString.split(":") match {
           case Array(str1, str2) => Map("username" -> str1, "password" -> str2)
@@ -118,63 +114,31 @@ object JWTAuth extends RestHelper with Loggable {
       }
 
       //Convert the list of jwtauth parameters to a Map
-      def toMap(parametersList : Map[String, List[String]]) = {
-        println("parameters=" + parametersList)
+      def toMapFromReq(parametersList : Req ) = {
+        val jwtauthPossibleParameters =
+          List(
+            "username",
+            "password",
+            "token"
+          )
 
-          val jwtauthPossibleParameters =
-            List(
-              "username",
-              "password",
-              "token"
-            )
-          for ( p <- parametersList if jwtauthPossibleParameters.contains(p._1))
-            yield (p._1 -> p._2.head)
+        if (parametersList.json_?) {
+          val parameters = parametersList.json.map( _ .values ).getOrElse(Map[String,String] _)
+          parameters.asInstanceOf[Map[String,String]]
+        } else if (parametersList.xml_?) {
+          val parameters = parametersList.xml.map( _ .text ).getOrElse(Map[String,String] _)
+          parameters.asInstanceOf[Map[String,String]]
+        } else {
+          Map("error" -> "parameters incorrect")
+        }
       }
 
       S.request match {
           case Full(a) =>  a.header("Authorization") match {
             case Full(parameters) => toMapFromBasicAuth(parameters)
-            case _ => toMap(a.params)
+            case _ => toMapFromReq(a)
           }
-          case _ => toMap(S.request.openTheBox.params)
-      }
-    }
-
-    def supportedjwtauthVersion(jwtauthVersion : Option[String]) : Boolean = {
-      //auth_version is OPTIONAL.  If present, MUST be set to "1.0".
-      jwtauthVersion match
-      {
-        case Some(a) =>  a=="1" || a=="1.0"
-        case _ => true
-      }
-    }
-
-    def wrongTimestamp(requestTimestamp : Option[String]) : Option[String] = {
-      requestTimestamp match {
-        case Some(timestamp) => {
-          tryo{
-            timestamp.toLong
-          } match {
-            case Full(l) =>
-              tryo{
-                new Date(l)
-              } match {
-                case Full(d) => {
-                  val currentTime = Platform.currentTime / 1000
-                  val timeRange : Long = Helpers.minutes(3)
-                  //check if the timestamp is positive and in the time range
-                  if(d.getTime < 0 || d.before(new Date(currentTime - timeRange)) ||  d.after(new Date(currentTime + timeRange)))
-                    Some("timestamp value: " + timestamp +" is in and invalid time range")
-                  else
-                    None
-                }
-                case _ => Some("timestamp value: " + timestamp +" is an invalid date")
-              }
-            case _ => Some("timestamp value: " + timestamp +" is invalid")
-          }
-
-        }
-        case _ => Some("the following parameter is missing: timestamp")
+          case _ => Map("error" -> "request incorrect")
       }
     }
 
@@ -185,73 +149,15 @@ object JWTAuth extends RestHelper with Loggable {
       }
     }
 
-    def correctSignature(jwtauthparameters : Map[String, String], httpMethod : String) = {
-      //Normalize an encode the request parameters as explained in Section 3.4.1.3.2
-      //of jwtauth 1.0 specification (http://tools.ietf.org/html/rfc5849)
-      def generatejwtauthParametersString(jwtauthparameters : Map[String, String]) : String = {
-        def sortParam( keyAndValue1 : (String, String), keyAndValue2 : (String, String))= keyAndValue1._1.compareTo(keyAndValue2._1) < 0
-        var parameters =""
-
-        //sort the parameters by name
-        jwtauthparameters.toList.sortWith(sortParam _).foreach(
-          t =>
-            if(t._1 != "signature")
-              parameters += URLEncoder.encode(t._1,"UTF-8")+"%3D"+ URLEncoder.encode(t._2,"UTF-8")+"%26"
-
-        )
-        parameters = parameters.dropRight(3) //remove the "&" encoded sign
-        parameters
-      }
-
-      //prepare the base string
-      var baseString = httpMethod+"&"+URLEncoder.encode(Props.get("hostname").openOr(S.hostAndPath)  + S.uri,"UTF-8")+"&"
-      baseString+= generatejwtauthParametersString(jwtauthparameters)
-
-      val encodeBaseString = URLEncoder.encode(baseString,"UTF-8")
-      //get the key to sign
-      val comsumer = Consumer.find(
-          By(Consumer.key,jwtauthparameters.get("consumer_key").get)
-        ).get
-      var secret= comsumer.secret.toString
-
-      jwtauthparameters.get("token") match {
-        case Some(tokenKey) => Token.find(By(Token.key,tokenKey)) match {
-            case Full(token) => secret+= "&" +token.secret.toString()
-            case _ => secret+= "&"
-          }
-        case _ => secret+= "&"
-      }
-      logger.info("base string: " + baseString)
-      //signing process
-      val signingAlgorithm : String = if(jwtauthparameters.get("signature_method").get.toLowerCase == "hmac-sha256")
-        "HmacSHA256"
-      else
-        "HmacSHA1"
-
-      logger.info("signing method: " + signingAlgorithm)
-      logger.info("signing key: " + secret)
-      logger.info("signing key in bytes: " + secret.getBytes("UTF-8"))
-
-      var m = Mac.getInstance(signingAlgorithm)
-      m.init(new SecretKeySpec(secret.getBytes("UTF-8"),signingAlgorithm))
-      val calculatedSignature = Helpers.base64Encode(m.doFinal(baseString.getBytes))
-
-      logger.info("calculatedSignature: " + calculatedSignature)
-      //logger.info("received signature:" + jwtauthparameters.get("signature").get)
-      logger.info("received signature after decoding: " + URLDecoder.decode(jwtauthparameters.get("signature").get))
-
-      calculatedSignature== URLDecoder.decode(jwtauthparameters.get("signature").get,"UTF-8")
-    }
-
     //check if the token exists and is still valid
-    def validToken(tokenKey : String) ={
+    def validRequestToken(tokenKey : String) ={
       Token.find(By(Token.key, tokenKey),By(Token.tokenType,TokenType.Request)) match {
         case Full(token) => token.isValid
         case _ => false
       }
     }
 
-    def validToken2(tokenKey : String) = {
+    def validAccessToken(tokenKey : String) = {
       Token.find(By(Token.key, tokenKey),By(Token.tokenType,TokenType.Access)) match {
         case Full(token) => token.isValid
           case _ => false
@@ -260,27 +166,15 @@ object JWTAuth extends RestHelper with Loggable {
 
     //@return the missing parameters depending of the request type
     def missingjwtauthParameters(parameters : Map[String, String], requestType : String) : Set[String] = {
-      val parametersBase =
-        List()
-      //    "username",
-      //    "password",
-      //    "token"
-      //  )
       println(parameters.toString)
       if(requestType == "requestToken")
-        ("username" :: "password" :: parametersBase).toSet diff parameters.keySet
+        ("username" :: "password" :: List()).toSet diff parameters.keySet
       else if(requestType=="authorizationToken")
-        ("token" :: parametersBase).toSet diff parameters.keySet
+        ("token" :: List()).toSet diff parameters.keySet
       else if(requestType=="protectedResource")
-        ("token" :: parametersBase).toSet diff parameters.keySet
+        ("token" :: List()).toSet diff parameters.keySet
       else
         parameters.keySet
-    }
-
-    def supportedSignatureMethod(jwtauthSignatureMethod : String ) : Boolean =
-    {
-      jwtauthSignatureMethod.toLowerCase == "hmac-sha256" ||
-      jwtauthSignatureMethod.toLowerCase == "hs256"
     }
 
     var message = ""
@@ -296,28 +190,8 @@ object JWTAuth extends RestHelper with Loggable {
       httpCode = 400
     }
 
-    //supported signature method
-    //else if (!supportedSignatureMethod(parameters.get("signature_method").get))
-    //{
-    //  message = "Unsupported signature method, please use hmac-sha1 or hmac-sha256"
-    //  httpCode = 400
-    //}
-    //check if the application is registered and active
-    //else if(! registeredApplication(parameters.get("consumer_key").get))
-    //{
-    //  logger.error("application: " + parameters.get("consumer_key").get + " not found")
-    //  message = "Invalid consumer credentials"
-    //  httpCode = 401
-    //}
-    //valid timestamp
-    //else if(! wrongTimestamp(parameters.get("timestamp")).isEmpty)
-    //{
-    //  message = wrongTimestamp(parameters.get("timestamp")).get
-    //  httpCode = 400
-    //}
-
     //In the case jwtauth authorization token request, check if the token is still valid and the verifier is correct
-    else if(requestType=="authorizationToken" && !validToken(parameters.get("token").get))
+    else if(requestType=="authorizationToken" && !validRequestToken(parameters.get("token").get))
     {
       message = "Invalid or expired request token: " + parameters.get("token").get
       httpCode = 401
@@ -325,7 +199,7 @@ object JWTAuth extends RestHelper with Loggable {
     //In the case protected resource access request, check if the token is still valid
     else if (
         requestType=="protectedResource" &&
-      ! validToken2(parameters.get("token").get)
+      ! validAccessToken(parameters.get("token").get)
     )
     {
       message = "Invalid or expired access token: " + parameters.get("token").get
@@ -345,11 +219,14 @@ object JWTAuth extends RestHelper with Loggable {
     (httpCode, message, parameters)
   }
 
-  private def generateTokenAndSecret() =
+  private def generateTokenAndSecret(claims: Map[String,String]) =
   {
-    // generate some random strings
-    val token_message = Helpers.randomString(40)
+    // generate random string
     val secret_message = Helpers.randomString(40)
+    // jwt header
+    val header = JwtHeader("HS256")
+    // generate jwt token
+    val token_message = JsonWebToken(header, JwtClaimsSet(claims), secret_message)
 
     (token_message, secret_message)
   }
