129643f7371be8360b74d2991f52a19a9f75859c
Petar Bozin <petar.bozin@md-softwaredesign.de>
2016-06-17 11:18:08 +0200

    Refactored UserCustomerLink

--- src/main/scala/code/usercustomerlinks/MappedUserCustomerLink.scala
+++ src/main/scala/code/usercustomerlinks/MappedUserCustomerLink.scala
@@ -8,7 +8,24 @@ import net.liftweb.mapper._
 /**
  * Created by markom on 5/30/16.
  */
-object MappedUserCustomerLinkProvider extends UserCustomerLinkProvider {
+
+class MappedUserCustomerLink extends UserCustomerLink with LongKeyedMapper[MappedUserCustomerLink] with IdPK with CreatedUpdated {
+
+  def getSingleton = MappedUserCustomerLink
+
+  // Name the objects m* so that we can give the overridden methods nice names.
+  // Assume we'll have to override all fields so name them all m*
+  object mUserCustomerLinkId extends MappedUUID(this)
+  object mCustomerId extends DefaultStringField(this)
+  object mUserId extends DefaultStringField(this)
+  object mDateInserted extends MappedDateTime(this)
+  object mIsActive extends MappedBoolean(this)
+
+  override def userCustomerLinkId: String = mUserCustomerLinkId.get
+  override def customerId: String = mCustomerId.get // id.toString
+  override def userId: String = mUserId.get
+  override def dateInserted: Date = mDateInserted.get
+  override def isActive: Boolean = mIsActive
 
   override def createUserCustomerLink(userId: String, customerId: String, dateInserted: Date, isActive: Boolean): Box[UserCustomerLink] = {
 
@@ -34,28 +51,7 @@ object MappedUserCustomerLinkProvider extends UserCustomerLinkProvider {
 
 }
 
-class MappedUserCustomerLink extends UserCustomerLink with LongKeyedMapper[MappedUserCustomerLink] with IdPK with CreatedUpdated {
-
-  def getSingleton = MappedUserCustomerLink
-
-  // Name the objects m* so that we can give the overridden methods nice names.
-  // Assume we'll have to override all fields so name them all m*
-  object mUserCustomerLinkId extends MappedUUID(this)
-  object mCustomerId extends DefaultStringField(this)
-  object mUserId extends DefaultStringField(this)
-  object mDateInserted extends MappedDateTime(this)
-  object mIsActive extends MappedBoolean(this)
-
-  override def userCustomerLinkId: String = mUserCustomerLinkId.get
-  override def customerId: String = mCustomerId.get // id.toString
-  override def userId: String = mUserId.get
-  override def dateInserted: Date = mDateInserted.get
-  override def isActive: Boolean = mIsActive
-
-
-
-}
-
 object MappedUserCustomerLink extends MappedUserCustomerLink with LongKeyedMetaMapper[MappedUserCustomerLink] {
-  override def dbIndexes = UniqueIndex(mUserCustomerLinkId) :: super.dbIndexes
+  override def dbIndexes = UniqueIndex(mUserCustomerLinkId) :: UniqueIndex(mUserId, mCustomerId) :: super.dbIndexes
+
 }
--- src/main/scala/code/usercustomerlinks/UserCustomerLink.scala
+++ src/main/scala/code/usercustomerlinks/UserCustomerLink.scala
@@ -1,9 +1,18 @@
 package code.usercustomerlinks
 
 import java.util.Date
-import code.model.{User, BankId}
+
 import net.liftweb.common.Box
 import net.liftweb.util.SimpleInjector
+
+
+object UserCustomerLink extends SimpleInjector {
+
+  val userCustomerLink = new Inject(buildOne _) {}
+
+  def buildOne: UserCustomerLink = MappedUserCustomerLink
+
+}
 
 trait UserCustomerLink {
   def userCustomerLinkId: String
@@ -11,18 +20,7 @@ trait UserCustomerLink {
   def customerId: String
   def dateInserted: Date
   def isActive: Boolean
-}
 
-
-object UserCustomerLink extends SimpleInjector {
-
-  val userCustomerLinkProvider = new Inject(buildOne _) {}
-
-  def buildOne: UserCustomerLinkProvider = MappedUserCustomerLinkProvider
-
-}
-
-trait UserCustomerLinkProvider {
   def createUserCustomerLink(userId: String, customerId: String, dateInserted: Date, isActive: Boolean): Box[UserCustomerLink]
   def getUserCustomerLink(userId: String, customerId: String): Box[UserCustomerLink]
   def getUserCustomerLinks: Box[List[UserCustomerLink]]
