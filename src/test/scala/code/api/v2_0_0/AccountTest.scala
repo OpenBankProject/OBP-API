15e4c701c60e0f7b75c710cdb6cf5c61d568f276
Marko Milic <marko.milic@md-softwaredesign.de>
2016-07-25 09:11:35 +0200

    write test / check coverage for v2.0.0 allAccountsAllBanks #100

--- src/test/scala/code/api/v2_0_0/AccountTest.scala
+++ src/test/scala/code/api/v2_0_0/AccountTest.scala
@@ -31,7 +31,7 @@ class AccountTest extends V200ServerSetup with DefaultUsers {
 
   feature("Assuring that Get all accounts at all banks works as expected - v2.0.0") {
 
-    scenario("We create an account and get accounts as anonymous and then as authenticated user") {
+    scenario("We create an account and get accounts as anonymous and then as authenticated user - allAccountsAllBanks") {
       Given("The bank")
       val testBank = mockBankId
 
@@ -62,6 +62,54 @@ class AccountTest extends V200ServerSetup with DefaultUsers {
 
       When("We make the authenticated access request")
       val requestGetAll = (v2_0Request / "accounts").GET <@ (user1)
+      val responseGetAll = makeGetRequest(requestGetAll)
+
+      Then("We should get a 200")
+      responseGetAll.code should equal(200)
+
+      val isPublicAll =
+        for {
+          obj@JObject(o) <- responseGetAll.body
+          if (o contains JField("id", JString(newAccountId1)))
+          JBool(isPublic) <- obj \\ "is_public"
+        } yield {
+          isPublic
+        }
+      And("The new created account has to be private")
+      isPublicAll.forall(_ == false) should equal(true)
+    }
+
+    scenario("We create an account and get accounts as anonymous and then as authenticated user - allAccountsAtOneBank") {
+      Given("The bank")
+      val testBank = mockBankId
+
+      Then("We create an private account at the bank")
+      val accountPutJSON = CreateAccountJSON(obpuser1.userId,"CURRENT", AmountOfMoneyJSON121("EUR", "0"))
+      val requestPut = (v2_0Request / "banks" / testBank.value / "accounts" / newAccountId1).PUT <@ (user1)
+      val responsePut = makePutRequest(requestPut, write(accountPutJSON))
+
+      And("We should get a 200")
+      responsePut.code should equal(200)
+
+      When("We make the anonymous access request")
+      val requestGet = (v2_0Request / "banks" / testBank.value / "accounts").GET
+      val responseGet = makeGetRequest(requestGet)
+
+      Then("We should get a 200")
+      responseGet.code should equal(200)
+
+      val isPublic: List[Boolean] =
+        for {
+          JObject(o) <- responseGet.body
+          JField("is_public", JBool(isPublic)) <- o
+        } yield {
+          isPublic
+        }
+      And("All received accounts have to be public")
+      isPublic.forall(_ == true) should equal(true)
+
+      When("We make the authenticated access request")
+      val requestGetAll = (v2_0Request / "banks" / testBank.value / "accounts").GET <@ (user1)
       val responseGetAll = makeGetRequest(requestGetAll)
 
       Then("We should get a 200")
