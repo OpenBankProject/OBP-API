8d4a6f08c6af344fdf48d8d71e75d73e91d77301
Marko Milic <marko.milic@md-softwaredesign.de>
2016-08-02 13:27:04 +0200

    Closes #116 - Kafka connector : If non existing transaction_id is requested we should through 404 instead of 500

--- src/main/scala/code/bankconnectors/KafkaMappedConnector.scala
+++ src/main/scala/code/bankconnectors/KafkaMappedConnector.scala
@@ -223,10 +223,14 @@ object KafkaMappedConnector extends Connector with CreateViewImpls with Loggable
                        "transactionId" -> transactionId.toString )
     // Since result is single account, we need only first list entry
     implicit val formats = net.liftweb.json.DefaultFormats
-    val r = process(reqId, "getTransaction", argList).extract[KafkaInboundTransaction]
-    // Check does the response data match the requested data
-    if (transactionId.value != r.id) throw new Exception(ErrorMessages.InvalidGetTransactionConnectorResponse)
-    createNewTransaction(r)
+    val r = process(reqId, "getTransaction", argList).extractOpt[KafkaInboundTransaction]
+    r match {
+      // Check does the response data match the requested data
+      case Some(x) if transactionId.value != x.id => Failure(ErrorMessages.InvalidGetTransactionConnectorResponse, Empty, Empty)
+      case Some(x) if transactionId.value == x.id => createNewTransaction(x)
+      case _ => Failure(ErrorMessages.ConnectorEmptyResponse, Empty, Empty)
+    }
+
   }
 
   override def getTransactions(bankId: BankId, accountID: AccountId, queryParams: OBPQueryParam*): Box[List[Transaction]] = {
