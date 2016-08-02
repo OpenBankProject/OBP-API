e6761c568c1ef295d95356702eddfd4f38c1829f
sorinmanole <somanole@gmail.com>
2016-08-02 12:56:11 +0200

    remove unnecessary imports

--- src/main/scala/code/api/v2_1_0/APIMethods210.scala
+++ src/main/scala/code/api/v2_1_0/APIMethods210.scala
@@ -1,63 +1,26 @@
 package code.api.v2_1_0
 
 import java.text.SimpleDateFormat
-import java.util.Calendar
-
-import code.TransactionTypes.TransactionType
-import code.api.APIFailure
-import code.api.util.APIUtil._
 import code.api.util.ApiRole._
-import code.api.util.{APIUtil, ApiRole, ErrorMessages}
-import code.api.v1_2_1.OBPAPI1_2_1._
-import code.api.v1_2_1.{APIMethods121, AmountOfMoneyJSON => AmountOfMoneyJSON121, JSONFactory => JSONFactory121}
-import code.api.v1_4_0.JSONFactory1_4_0
-import code.api.v1_4_0.JSONFactory1_4_0.{ChallengeAnswerJSON, CustomerFaceImageJson, TransactionRequestAccountJSON}
-import code.entitlement.Entitlement
-import code.search.{elasticsearchMetrics, elasticsearchWarehouse}
-import net.liftweb.http.CurrentReq
-//import code.api.v2_0_0.{CreateCustomerJson}
-
-import code.model.dataAccess.OBPUser
-import net.liftweb.mapper.By
-
-//import code.api.v1_4_0.JSONFactory1_4_0._
-import code.api.v2_0_0.JSONFactory200._
-import code.bankconnectors.Connector
-import code.fx.fx
-import code.kycchecks.KycChecks
-import code.kycdocuments.KycDocuments
-import code.kycmedias.KycMedias
-import code.kycstatuses.KycStatuses
+import code.api.util.ErrorMessages
 import code.model._
-import code.model.dataAccess.BankAccountCreation
-import code.socialmedia.SocialMediaHandle
-import code.transactionrequests.TransactionRequests
 
-import code.meetings.Meeting
-import code.usercustomerlinks.UserCustomerLink
-
-import net.liftweb.common.{Full, _}
-import net.liftweb.http.rest.RestHelper
-import net.liftweb.http.{JsonResponse, Req}
+import net.liftweb.http.Req
 import net.liftweb.json.JsonAST.JValue
-import net.liftweb.util.Helpers._
 import net.liftweb.util.Props
 
 import scala.collection.immutable.Nil
 import scala.collection.mutable.ArrayBuffer
 // Makes JValue assignment to Nil work
-import code.customer.{MockCustomerFaceImage, Customer}
 import code.util.Helper._
-import net.liftweb.http.js.JE.JsRaw
-import net.liftweb.json.Extraction
 import net.liftweb.json.JsonDSL._
 
-import code.api.{APIFailure, OBPRestHelper}
+import code.api.APIFailure
 import code.api.util.APIUtil._
 import code.sandbox.{OBPDataImport, SandboxDataImport}
 import code.util.Helper
-import net.liftweb.common.{Box, Full, Failure, Loggable}
-import net.liftweb.http.{JsonResponse, ForbiddenResponse, S}
+import net.liftweb.common.Box
+import net.liftweb.http.JsonResponse
 import net.liftweb.http.js.JE.JsRaw
 import net.liftweb.http.rest.RestHelper
 import net.liftweb.util.Helpers._
