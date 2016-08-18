85025f30a05f496bf47c5db0cc965787f498ab66
Petar Bozin <petar.bozin@md-softwaredesign.de>
2016-08-18 17:47:27 +0200

    Adjusted some more for running tests

--- mvn.sh
+++ mvn.sh
@@ -2,6 +2,6 @@
 
 # Deprecated option -XX:MaxPermSize=256m is kept
 # just in case someone still uses java 1.7
-export MAVEN_OPTS="-Xmx1024m -Xms512m -Xss1024k -XX:MaxPermSize=256m"
+export MAVEN_OPTS="-Xmx1024m -Xms1024m -Xss1024k -XX:MaxPermSize=256m"
 
 mvn $1 $2 $3 $4
