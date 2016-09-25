259ab88303b951e4d3ba8331537d21eae80e5bff
Simon Redfern <simon@tesobe.com>
2016-09-25 08:00:41 +0200

    Update UserJSONRecord for PostCounterpartyMetadata

--- src/test/scala/code/sandbox/PostCounterpartyMetadata.scala
+++ src/test/scala/code/sandbox/PostCounterpartyMetadata.scala
@@ -50,7 +50,7 @@ import scala.collection.mutable.ListBuffer
 import scala.io.Source
 
 case class CounterpartyJSONRecord(name: String, category: String, superCategory: String, logoUrl: String, homePageUrl: String, region: String)
-case class UserJSONRecord(email: String, password: String, display_name: String)
+case class UserJSONRecord(email: String, password: String, user_name: String)
 
 
 // Import counterparty metadata
@@ -74,7 +74,7 @@ object PostCounterpartyMetadata extends SendServerRequests {
     implicit val formats = DefaultFormats
 
     //load json for counterpaties
-    val counterpartyDataPath = "/Users/simonredfern/Documents/OpenBankProject/DATA/API_sandbox/OP/load_op_11/OBP_sandbox_counterparties_pretty.json"
+    val counterpartyDataPath = "/Users/simonredfern/Documents/OpenBankProject/DATA/API_sandbox/load_gh_pre_statup_bootcamp_29/OBP_sandbox_counterparties_pretty.json"
 
     // This contains a list of counterparty lists. one list for each region
     val counerpartyListData = JsonParser.parse(Source.fromFile(counterpartyDataPath) mkString)
@@ -110,7 +110,7 @@ object PostCounterpartyMetadata extends SendServerRequests {
 
     //load sandbox users from json
 
-    val mainDataPath = "/Users/simonredfern/Documents/OpenBankProject/DATA/API_sandbox/OP/load_op_11/OBP_sandbox_pretty.json"
+    val mainDataPath = "/Users/simonredfern/Documents/OpenBankProject/DATA/API_sandbox/load_gh_pre_statup_bootcamp_29/OBP_sandbox_pretty.json"
 
     val mainData = JsonParser.parse(Source.fromFile(mainDataPath) mkString)
     val users = (mainData \ "users").children
