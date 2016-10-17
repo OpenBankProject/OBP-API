e43ed60ab0c4f4724ee8ea182874d3a5d04602e8
Marko Milic <marko.milic@md-softwaredesign.de>
2016-10-17 09:21:46 +0200

    Add GET /banks/BANK_ID/users/USER_ID/entitlements #184

--- src/main/scala/code/api/v2_1_0/OBPAPI2_1_0.scala
+++ src/main/scala/code/api/v2_1_0/OBPAPI2_1_0.scala
@@ -235,6 +235,7 @@ object OBPAPI2_1_0 extends OBPRestHelper with APIMethods130 with APIMethods140 w
                          "createTransactionRequest" ::
                          "getTransactionRequests" ::
                          "getRoles" ::
+                         "getEntitlementsByBankAndUser" ::
                          Nil
   //Second step - iterate through all endpoints defined in resource doc
   //       then - omit endpoints of disabled version in props file
