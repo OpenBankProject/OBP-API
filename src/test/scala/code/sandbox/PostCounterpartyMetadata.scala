ea735766f6570e7142eac27816c7ba9b3566e3f5
Simon Redfern <simon@tesobe.com>
2016-11-28 12:50:48 +0900

    removing duplicate allAccountsAllBanks in v2.1.0 + PostCounterpartyMetadata uses username

--- src/test/scala/code/sandbox/PostCounterpartyMetadata.scala
+++ src/test/scala/code/sandbox/PostCounterpartyMetadata.scala
@@ -74,7 +74,7 @@ object PostCounterpartyMetadata extends SendServerRequests {
     implicit val formats = DefaultFormats
 
     //load json for counterpaties
-    val counterpartyDataPath = "/Users/simonredfern/Documents/OpenBankProject/DATA/korea/loaded_03/OBP_sandbox_counterparties_pretty.json"
+    val counterpartyDataPath = "/Users/simonredfern/Documents/OpenBankProject/DATA/korea/loaded_06/OBP_sandbox_counterparties_pretty.json"
 
     // This contains a list of counterparty lists. one list for each region
     val counerpartyListData = JsonParser.parse(Source.fromFile(counterpartyDataPath) mkString)
@@ -110,7 +110,7 @@ object PostCounterpartyMetadata extends SendServerRequests {
 
     //load sandbox users from json
 
-    val mainDataPath = "/Users/simonredfern/Documents/OpenBankProject/DATA/korea/loaded_03/OBP_sandbox_pretty.json"
+    val mainDataPath = "/Users/simonredfern/Documents/OpenBankProject/DATA/korea/loaded_06/OBP_sandbox_pretty.json"
 
     val mainData = JsonParser.parse(Source.fromFile(mainDataPath) mkString)
     val users = (mainData \ "users").children
@@ -121,10 +121,10 @@ object PostCounterpartyMetadata extends SendServerRequests {
       val user = u.extract[UserJSONRecord]
       println(" ")
       print("login as user: ")
-      println (user.email + " - " + user.password)
+      println (user.user_name + " - " + user.password)
 
       if(!OAuthClient.loggedIn) {
-        OAuthClient.authenticateWithOBPCredentials(user.email, user.password)
+        OAuthClient.authenticateWithOBPCredentials(user.user_name, user.password)
         //println(" - ok.")
       }
 
