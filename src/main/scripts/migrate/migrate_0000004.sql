c5060ef016fff8b1106d536b37a8474fe441ad08
Petar Bozin <petar.bozin@md-softwaredesign.de>
2017-02-10 10:29:37 +0100

    Modified SQL script for APIUser -> ResourceUser migration

--- src/main/scripts/migrate/migrate_0000004.sql
+++ src/main/scripts/migrate/migrate_0000004.sql
@@ -1,4 +1,4 @@
 -- Rename apiuser to resourceuser 
 ALTER TABLE apiuser RENAME TO resourceuser;
-ALTER TABLE resourceuser DROP INDEX apiuser_provider__providerid;
+DROP INDEX apiuser_provider__providerid;
 CREATE UNIQUE INDEX resourceuser_provider__providerid ON resourceuser (PROVIDER_,PROVIDERID);
