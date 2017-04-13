64276f5fc48db3ba35064ccfd8c46e344f253c87
Petar Bozin <petar.bozin@md-softwaredesign.de>
2017-04-13 11:09:11 +0200

    Improved local remotedata port assignment

--- src/main/scala/code/remotedata/RemotedataConfig.scala
+++ src/main/scala/code/remotedata/RemotedataConfig.scala
@@ -12,7 +12,7 @@ object RemotedataConfig {
   val remotePort = Props.get("remotedata.port").openOr("2662")
 
   val localHostname = "127.0.0.1" 
-  val localPort = Props.get("remotedata.local.port").openOr("2552")
+  var localPort = 0
 
   val akka_loglevel = Props.get("remotedata.loglevel").openOr("INFO")
 
@@ -94,7 +94,7 @@ object RemotedataConfig {
   ${commonConf} 
   akka {
     remote.netty.tcp.hostname = ${localHostname}
-    remote.netty.tcp.port = ${localPort} 
+    remote.netty.tcp.port = ${localPort}
   }
   """
 
