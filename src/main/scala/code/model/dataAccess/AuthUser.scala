ce2abe7d6d8660c8716fb90a0db46bf5a08749f1
hongwei1 <hhh@tesobe.com>
2017-05-31 14:30:33 +0200

    Documented the AuthUser and ResourceUser

--- src/main/scala/code/model/dataAccess/AuthUser.scala
+++ src/main/scala/code/model/dataAccess/AuthUser.scala
@@ -55,6 +55,25 @@ import net.liftweb.util
   *
   *
   * // TODO Document the difference between this and AuthUser / ResourceUser
+  * 
+  * 1 AuthUser : is used for authentication, only for webpage Login in stuff
+  *   1) It is MegaProtoUser, has lots of methods for validation username, password, email ....
+  *      Such as lost password, reset password ..... 
+  *      Lift have some helper methods to make these things easily. 
+  *   
+  *  
+  * 
+  * 2 ResourceUser: is only a normal LongKeyedMapper 
+  *   1) All the accounts, transactions ,roles, views, accountHolders, customers... should be linked to ResourceUser.userId_ field.
+  *   2) The consumer keys, tokens are also belong ResourceUser
+  *  
+  * 
+  * 3 RelationShips:
+  *   1)When `Sign up` new user --> create AuthUser --> call AuthUser.save() --> create ResourceUser user.
+  *      They share the same username and email.
+  *   2)AuthUser `user` field as the Foreign Key to link to Resource User. 
+  *      one AuthUser <---> one ResourceUser 
+  *  
   *
  */
 class AuthUser extends MegaProtoUser[AuthUser] with Logger {
