cad6d4b2f769ccd532030ca67a719b6c165b60cf
jota <jota@mailbox.tu-berlin.de>
2017-07-17 14:18:12 +0200

    Added payloads

--- src/main/scala/code/bankconnectors/payloads.scala
+++ src/main/scala/code/bankconnectors/payloads.scala
@@ -15,11 +15,15 @@ case class GetUserByUsernamePassword(username: String, password: String) extends
 case class UpdateUserAccountViews(username: String, password: String) extends TopicCaseClass
 case class GetAdapterInfo(date: String) extends TopicCaseClass
 case class GetAccounts(authInfo: AuthInfo) extends TopicCaseClass
-
-
+case class GetAccountbyAccountID(authInfo: AuthInfo, bankId: String, accountId: String)
+case class GetAccountbyAccountNumber(authInfo: AuthInfo, bankId: String, accountNumber: String)
+case class GetTransactions(authInfo: AuthInfo,bankId: String, accountId: String, queryParams: String)
 /**
   * case classes used as payloads
   */
 case class Banks(authInfo: AuthInfo, data: List[InboundBank])
 case class BankWrapper(authInfo: AuthInfo, data: InboundBank)
 case class AdapterInfo(data: InboundAdapterInfo)
+case class BankAccounts(authInfo: AuthInfo, data: List[InboundAccountJune2017])
+case class BankAccount(authInfo: AuthInfo, data: InboundAccountJune2017)
+case class Transcations(authInfo: AuthInfo, data: List[InternalTransaction])
