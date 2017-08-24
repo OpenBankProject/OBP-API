b1fb9df1c95936ecb8cb937aecb9f9072d8c26ac
constantine2nd <marko@tesobe.com>
2017-08-24 15:00:44 +0200

    Added snakify, camelify and camelifyMethod for JValue

--- src/main/scala/code/api/util/APIUtil.scala
+++ src/main/scala/code/api/util/APIUtil.scala
@@ -53,10 +53,10 @@ import net.liftweb.common.{Empty, _}
 import net.liftweb.http._
 import net.liftweb.http.js.JE.JsRaw
 import net.liftweb.http.js.JsExp
-import net.liftweb.json.JsonAST.JValue
+import net.liftweb.json.JsonAST.{JField, JValue}
 import net.liftweb.json.{Extraction, parse}
 import net.liftweb.util.Helpers._
-import net.liftweb.util.Props
+import net.liftweb.util.{Props, StringHelpers}
 
 import scala.collection.JavaConverters._
 import scala.collection.mutable.ArrayBuffer
@@ -1279,5 +1279,42 @@ Returns a string showed to the developer
     }
   }
 
+  /**
+    * Turn a string of format "FooBar" into snake case "foo_bar"
+    *
+    * Note: snakify is not reversible, ie. in general the following will _not_ be true:
+    *
+    * s == camelify(snakify(s))
+    *
+    * @return the underscored JValue
+    */
+  def snakify(json: JValue): JValue = json transform {
+    case JField(name, x) => JField(StringHelpers.snakify(name), x)
+  }
+
+
+  /**
+    * Turns a string of format "foo_bar" into camel case "FooBar"
+    *
+    * Functional code courtesy of Jamie Webb (j@jmawebb.cjb.net) 2006/11/28
+    * @param json the JValue to CamelCase
+    *
+    * @return the CamelCased JValue
+    */
+  def camelify(json: JValue): JValue = json transform {
+    case JField(name, x) => JField(StringHelpers.camelify(name), x)
+  }
+
+  /**
+    * Turn a string of format "foo_bar" into camel case with the first letter in lower case: "fooBar"
+    * This function is especially used to camelCase method names.
+    *
+    * @param json the JValue to CamelCase
+    *
+    * @return the CamelCased JValue
+    */
+  def camelifyMethod(json: JValue): JValue = json transform {
+    case JField(name, x) => JField(StringHelpers.camelifyMethod(name), x)
+  }
 
 }
