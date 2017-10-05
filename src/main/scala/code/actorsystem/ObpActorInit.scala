41f690ff1d593f12f6325bf23bc4c3197379b586
hongwei1 <hhh@tesobe.com>
2017-10-05 17:08:36 +0200

    enhance the future timeout error

--- src/main/scala/code/actorsystem/ObpActorInit.scala
+++ src/main/scala/code/actorsystem/ObpActorInit.scala
@@ -2,14 +2,17 @@ package code.actorsystem
 
 import akka.util.Timeout
 import code.api.APIFailure
+import code.api.util.ErrorMessages._
+import code.util.Helper.MdcLoggable
 import net.liftweb.common._
+import net.liftweb.util.Helpers.tryo
 import net.liftweb.util.Props
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.duration._
 import scala.concurrent.{Await, Future}
 
-trait ObpActorInit {
+trait ObpActorInit extends MdcLoggable{
   // Default is 3 seconds, which should be more than enough for slower systems
   val ACTOR_TIMEOUT: Long = Props.getLong("remotedata.timeout").openOr(3)
 
@@ -37,7 +40,9 @@ trait ObpActorInit {
       case t: T => Full(t)
       case _ => Empty ~> APIFailure(s"future extraction to box failed", 501)
     }
-    Await.result(r, TIMEOUT)
+    
+    tryo{Await.result(r, TIMEOUT).head} ?~! {FutureTimeoutException}
+    
   }
 
   def CreateActorNameFromClassName(c: String): String = {
