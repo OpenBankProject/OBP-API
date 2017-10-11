c2fcf500944f3d2467cfe3c737362f76ea6afd4f
hongwei1 <hhh@tesobe.com>
2017-10-11 11:34:24 +0200

    Update the endpoints create Customer, will update accounts views according to specific connectors
    (cherry picked from commit b1956d6)

--- src/main/scala/code/bankconnectors/Connector.scala
+++ src/main/scala/code/bankconnectors/Connector.scala
@@ -16,6 +16,7 @@ import code.branches.Branches.{Branch, BranchId, BranchT}
 import code.fx.FXRate
 import code.management.ImporterAPI.ImporterTransaction
 import code.metadata.counterparties.{Counterparties, CounterpartyTrait, MappedCounterparty}
+import code.model.dataAccess.{AuthUser, MappedBankAccount, ResourceUser}
 import code.model.dataAccess.ResourceUser
 import code.model.{Transaction, TransactionRequestType, User, _}
 import code.products.Products.{Product, ProductCode}
@@ -1160,4 +1161,17 @@ trait Connector extends MdcLoggable{
     } yield { trt }.toList
     res.headOption
   }
+  
+  /**
+    * This a Helper method, it is only used in some connectors. Not all the connectors need it yet. 
+    * This is in progress.
+    * Here just return some String to make sure the method return sth, and the API level is working well !
+    * 
+    * @param username
+    * @return 
+    */
+  def UpdateUserAccoutViewsByUsername(username: String): Box[Any] = {
+    Full("Only some specific connectors need this method, in process")
+  }
+  
 }
