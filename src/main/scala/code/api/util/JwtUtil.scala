fa7789f288ed1c38890a203f63de51b326388be7
constantine2nd <marko@tesobe.com>
2018-02-28 08:56:39 +0100

    OAuh 2.0 - validating bearer JWT access tokens

--- src/main/scala/code/api/util/JwtUtil.scala
+++ src/main/scala/code/api/util/JwtUtil.scala
@@ -1,9 +1,17 @@
 package code.api.util
 
+import java.net.URL
+
 import com.auth0.jwt.JWT
+import com.nimbusds.jose.JWSAlgorithm
 import com.nimbusds.jose.crypto.{MACVerifier, RSASSAVerifier}
-import com.nimbusds.jwt.SignedJWT
+import com.nimbusds.jose.jwk.source.{JWKSource, RemoteJWKSet}
+import com.nimbusds.jose.proc.{JWSVerificationKeySelector, SecurityContext}
+import com.nimbusds.jwt.{JWTClaimsSet, SignedJWT}
+import com.nimbusds.jwt.proc.{BadJWTException, DefaultJWTProcessor}
+import net.liftweb.common.{Box, Empty, Failure, Full}
 import net.liftweb.util.Props
+import java.text.ParseException
 
 object JwtUtil {
 
@@ -76,6 +84,48 @@ object JwtUtil {
     }
   }
 
+  /**
+    * Helper function which validating bearer JWT access tokens
+    * @param accessToken The access token to validate, typically submitted with a HTTP header like
+    *                    Authorization: Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6InMxIn0.eyJzY3A...
+    * @param remoteJWKSetUrl The URL of OAuth 2.0 server's JWK set, published at a well-known URL
+    * @return The boxed token claims set or Failure
+    */
+
+  def validateAccessToken(accessToken: String, remoteJWKSetUrl: String): Box[JWTClaimsSet] = {
+    // Set up a JWT processor to parse the tokens and then check their signature
+    // and validity time window (bounded by the "iat", "nbf" and "exp" claims)
+    val jwtProcessor = new DefaultJWTProcessor[SecurityContext]
+
+    // The public RSA keys to validate the signatures will be sourced from the
+    // OAuth 2.0 server's JWK set, published at a well-known URL. The RemoteJWKSet
+    // object caches the retrieved keys to speed up subsequent look-ups and can
+    // also gracefully handle key-rollover
+    val keySource: JWKSource[SecurityContext] = new RemoteJWKSet(new URL(("http://localhost:8080/openid-connect-server-webapp/jwk")))
+
+    // The expected JWS algorithm of the access tokens (agreed out-of-band)
+    val expectedJWSAlg = JWSAlgorithm.RS256
+
+    // Configure the JWT processor with a key selector to feed matching public
+    // RSA keys sourced from the JWK set URL
+    val keySelector = new JWSVerificationKeySelector[SecurityContext](expectedJWSAlg, keySource)
+    jwtProcessor.setJWSKeySelector(keySelector)
+
+    try {
+      // Process the token
+      val maybeCtx: Option[SecurityContext] = None
+      val ctx: SecurityContext = maybeCtx.orNull // optional context parameter, not required here
+      val claimsSet = jwtProcessor.process(accessToken, ctx)
+      Full(claimsSet)
+    } catch {
+      case e: BadJWTException => Failure(e.getMessage, Full(e), Empty)
+      case e: ParseException  => Failure(e.getMessage, Full(e), Empty)
+      case e: Exception       => Failure(e.getMessage, Full(e), Empty)
+    }
+  }
+
+
+
 
   def main(args: Array[String]): Unit = {
     // val jwtToken = "eyJraWQiOiJyc2ExIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiJhZG1pbiIsImF6cCI6ImNsaWVudCIsImlzcyI6Imh0dHA6XC9cL2xvY2FsaG9zdDo4MDgwXC9vcGVuaWQtY29ubmVjdC1zZXJ2ZXItd2ViYXBwXC8iLCJleHAiOjE1MTk1MDMxODAsImlhdCI6MTUxOTQ5OTU4MCwianRpIjoiMmFmZjNhNGMtZjY5Zi00ZWM1LWE2MzEtYWUzMGYyYzQ4MjZiIn0.NwlK2EJKutaybB4YyEhuwb231ZNkD-BEwhScadcWWn8PFftjVyjqjD5_BwSiWHHa_QaESNPdZugAnF4I2DxtXmpir_x2fB2ch888AzXw6CgTT482I16m1jpL-2iSlQk1D-ZW6fJ2Qemdi3x2V13Xgt9PBvk5CsUukJ8SSqTPbSNNER9Nq2dlS-qQfg61TzhPkuuXDlmCQ3b8QHgUf6UnCfee1jRaohHQoCvJJJubmUI3dY0Df1ynTodTTZm4J1TV6Wp6ZhsPkQVmdBAUsE5kIFqADaE179lldh86-97bVHGU5a4aTYRRKoTPDltt1NvY5XJrjLCgZH8AEW7mOHz9mw"
@@ -86,6 +136,7 @@ object JwtUtil {
     println("Signature :" + getSignature(jwtToken))
     println("Verify JWT :" + verifyRsaSignedJwt(jwtToken))
   }
+
 
 }
 
