c04cfec4533a7f5be35176e40fdf0775ba05d194
constantine2nd <marko@tesobe.com>
2018-03-30 10:16:12 +0200

    Enhanced extracting result from DB.runQuery

--- src/main/scala/code/metrics/MappedAggregateMetrics.scala
+++ src/main/scala/code/metrics/MappedAggregateMetrics.scala
@@ -7,12 +7,18 @@ object MappedAggregateMetrics extends AggregateMetrics {
   override def getAllAggregateMetrics(startDate: Date, endDate: Date): List[Double] = {
 
     val dbQuery = s"SELECT count(*), avg(duration), min(duration), max(duration) FROM mappedmetric WHERE date_c >= '$startDate' AND date_c <= '$endDate'"
-    val queryResult = DB.runQuery(dbQuery)
+    /**
+      * Example of a Tuple response
+      * (List(count, avg, min, max),List(List(7503, 70.3398640543782487, 0, 9039)))
+      * First value of the Tuple is a List of field names returned by SQL query.
+      * Second value of the Tuple is a List of rows of the result returned by SQL query. Please note it's only one row.
+      */
+    val (_, List(count :: avg :: min :: max :: _)): (List[String], List[List[String]]) = DB.runQuery(dbQuery)
 
-    val totalCount = (queryResult._2(0))(0)
-    val avgResponseTime = "%.2f".format(((queryResult._2(0))(1)).toDouble)
-    val minResponseTime = (queryResult._2(0))(2)
-    val maxResponseTime = (queryResult._2(0))(3)
+    val totalCount = count
+    val avgResponseTime = "%.2f".format(avg.toDouble)
+    val minResponseTime = min
+    val maxResponseTime = max
 
 
     List(totalCount.toDouble, avgResponseTime.toDouble, minResponseTime.toDouble, maxResponseTime.toDouble)
