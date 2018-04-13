3141ae5e61b31c5fceecdbb343f2cdb57fb49c22
Simon Redfern <simon@tesobe.com>
2018-04-13 09:38:24 +0200

    TODO re ApiVersion

--- src/main/scala/code/api/util/ApiVersion.scala
+++ src/main/scala/code/api/util/ApiVersion.scala
@@ -41,10 +41,12 @@ object ApiVersion {
   lazy val bankMockApi = BankMockApi()
   case class BerlinGroupV1()  extends ApiVersion {
     override def toString() = "v1"
+    //override def toString() = "berlin_group_v1" // TODO don't want to confuse with OBP
   }
   lazy val berlinGroupV1 = BerlinGroupV1()
   case class UKOpenBankingV200()  extends ApiVersion {
     override def toString() = "v2.0.0"
+    // override def toString() = "uk_v2.0.0" // TODO don't want to confuse with OBP
   }
   lazy val ukOpenBankingV200 = UKOpenBankingV200()
   case class OpenIdConnect1() extends ApiVersion
