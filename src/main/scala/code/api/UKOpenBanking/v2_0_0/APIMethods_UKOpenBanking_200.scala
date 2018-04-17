d79f7ddcdc4df3ef2f3bd61bba39cc9bb9063f1c
constantine2nd <marko@tesobe.com>
2018-04-17 17:33:50 +0200

    UK Open Banking - Aded /accounts/ACCOUNT_ID endpoint

--- src/main/scala/code/api/UKOpenBanking/v2_0_0/APIMethods_UKOpenBanking_200.scala
+++ src/main/scala/code/api/UKOpenBanking/v2_0_0/APIMethods_UKOpenBanking_200.scala
@@ -4,8 +4,10 @@ import code.api.ResourceDocs1_4_0.SwaggerDefinitionsJSON
 import code.api.util.APIUtil._
 import code.api.util.{ApiVersion, ErrorMessages}
 import code.bankconnectors.Connector
+import code.model.AccountId
 import code.views.Views
 import net.liftweb.http.rest.RestHelper
+
 import scala.collection.mutable.ArrayBuffer
 import scala.concurrent.ExecutionContext.Implicits.global
 
@@ -55,6 +57,46 @@ trait APIMethods_UKOpenBanking_200 {
           }
       }
     }
+
+
+    resourceDocs += ResourceDoc(
+      readAccount,
+      implementedInApiVersion,
+      "readAccount",
+      "GET",
+      "/accounts/ACCOUNT_ID",
+      "Experimental! - UK Open Banking: Read Account",
+      s"""
+         |Reads a bank account, with balances where required.
+         |It is assumed that a consent of the PSU to this access is already given and stored on the ASPSP system.
+         |
+        |${authenticationRequiredMessage(true)}
+         |""",
+      emptyObjectJson,
+      SwaggerDefinitionsJSON.accountsJsonUKOpenBanking_v200,
+      List(ErrorMessages.UserNotLoggedIn,ErrorMessages.UnknownError),
+      Catalogs(notCore, notPSD2, notOBWG),
+      List(apiTagUKOpenBanking, apiTagAccount, apiTagPrivateData))
+
+    apiRelations += ApiRelation(readAccount, readAccount, "self")
+
+    lazy val readAccount : OBPEndpoint = {
+      //get private accounts for all banks
+      case "accounts" :: AccountId(accountId) :: Nil JsonGet _ => {
+        cc =>
+          for {
+            (user, callContext) <- extractCallContext(ErrorMessages.UserNotLoggedIn, cc)
+            u <- unboxFullAndWrapIntoFuture{ user }
+            availablePrivateAccounts <- Views.views.vend.getPrivateBankAccountsFuture(u) map {
+              _.filter(_.accountId.value == accountId.value)
+            }
+            accounts <- {Connector.connector.vend.getBankAccountsFuture(availablePrivateAccounts, callContext)}
+          } yield {
+            (JSONFactory_UKOpenBanking_200.createAccountJSON(accounts.getOrElse(Nil)), callContext)
+          }
+      }
+    }
+
 
   }
 
