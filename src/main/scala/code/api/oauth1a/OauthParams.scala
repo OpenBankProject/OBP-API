b0b36cb3bb86b2606d199127990fb31479d5245c
Marko Milić <marko@tesobe.com>
2018-07-12 08:17:02 +0200

    Refined OAuth 1.0 code - Added Apache License

--- src/main/scala/code/api/oauth1a/OauthParams.scala
+++ src/main/scala/code/api/oauth1a/OauthParams.scala
@@ -1,27 +1,17 @@
 /**
-  * Open Bank Project - API
-  * Copyright (C) 2011-2018, TESOBE Ltd
+  * Copyright 2018 Hunor Kovács: kovacshuni@yahoo.com
   *
-  * This program is free software: you can redistribute it and/or modify
-  * it under the terms of the Apache License, Version 2.0.
-  **
-  * This program is distributed in the hope that it will be useful,
-  * but WITHOUT ANY WARRANTY; without even the implied warranty of
-  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. the Apache License, Version 2.0 for more details.
-  **
-  * You should have received a copy of the Apache License, Version 2.0 License
-  * along with this program. If not, see <http://www.apache.org/licenses/LICENSE-2.0>.
-  **
-  * Email: contact@tesobe.com
-  * TESOBE Ltd
-  * Osloerstrasse 16/17
-  * Berlin 13359, Germany
-  **
-  * This product includes software developed at
-  * https://github.com/kovacshuni/koauth
-  * by
-  * Hunor Kovács: kovacshuni@yahoo.com
+  * Licensed under the Apache License, Version 2.0 (the "License");
+  * you may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at
   *
+  *  http://www.apache.org/licenses/LICENSE-2.0
+  *
+  * Unless required by applicable law or agreed to in writing, software
+  * distributed under the License is distributed on an "AS IS" BASIS,
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
   */
 
 package code.api.oauth1a
