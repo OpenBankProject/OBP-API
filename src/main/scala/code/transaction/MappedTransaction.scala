e238ebe849b10a9e5d955ebdc2b45f9f819b9ebf
Marko Milić <marko@tesobe.com>
2018-10-05 08:14:49 +0200

    Webhooks support #985 - added actor

--- src/main/scala/code/transaction/MappedTransaction.scala
+++ src/main/scala/code/transaction/MappedTransaction.scala
@@ -2,12 +2,15 @@ package code.transaction
 
 import java.util.UUID
 
+import code.actorsystem.ObpLookupSystem
 import code.api.util.{APIUtil, ApiTrigger}
 import code.bankconnectors.Connector
-import code.util._
-import net.liftweb.common.Logger
-import net.liftweb.mapper._
 import code.model._
+import code.util._
+import code.webhook.WebHookActor
+import net.liftweb.common._
+import net.liftweb.common.Box.tryo
+import net.liftweb.mapper._
 
 class MappedTransaction extends LongKeyedMapper[MappedTransaction] with IdPK with CreatedUpdated with TransactionUUID {
 
@@ -215,16 +218,20 @@ class MappedTransaction extends LongKeyedMapper[MappedTransaction] with IdPK wit
 }
 
 object MappedTransaction extends MappedTransaction with LongKeyedMetaMapper[MappedTransaction] {
-  private val logger = Logger(classOf[MappedTransaction])
   override def dbIndexes = UniqueIndex(transactionId, bank, account) :: Index(bank, account) :: super.dbIndexes
   override def afterSave = List(
-    t => {
-      // TODO Make an Actor and send data below to it
-      logger.debug("TRIGGER: " + ApiTrigger.OnBalanceChange.toString())
-      logger.debug("ACCOUNT_ID: " + t.theAccountId)
-      logger.debug("BANK_ID: " + t.theBankId)
-      logger.debug("AMOUNT: " + t.amount.get)
-      logger.debug("BALANCE: " + t.newAccountBalance.get)
+    t =>
+      tryo {
+        val eventId = UUID.randomUUID().toString()
+        val actor = ObpLookupSystem.getWebHookActor()
+        actor ! WebHookActor.Request(
+          ApiTrigger.onBalanceChange,
+          eventId,
+          t.theBankId.value,
+          t.theBankId.value,
+          t.amount.get.toString,
+          t.newAccountBalance.get.toString
+        )
     }
   )
 }
---
+++ src/main/scala/code/webhook/WebHookActor.scala
@@ -0,0 +1,33 @@
+package code.webhook
+
+import akka.actor.{Actor, ActorLogging}
+import code.api.util.ApiTrigger
+import code.util.Helper.MdcLoggable
+import code.webhook.WebHookActor.Request
+
+
+object WebHookActor {
+  case class Request(trigger: ApiTrigger , eventId: String, bankId: String, accountId: String, amount: String, balance: String)
+}
+
+
+/**
+  * This Actor acts in next way:
+  */
+class WebHookActor extends Actor with ActorLogging with MdcLoggable {
+
+  def receive = waitingForRequest
+
+  private def waitingForRequest: Receive = {
+    case Request(trigger, eventId, bankId, accountId, amount, balance) =>
+      implicit val ec = context.dispatcher
+      logger.debug("TRIGGER: " + trigger.toString())
+      logger.debug("EVENT_ID: " + eventId)
+      logger.debug("ACCOUNT_ID: " + bankId)
+      logger.debug("BANK_ID: " + accountId)
+      logger.debug("AMOUNT: " + amount)
+      logger.debug("BALANCE: " + balance)
+  }
+
+}
+
