ca7f9e6bcf7b09706ff8ff1184dd914cd9dc7073
Marko Milić <marko@tesobe.com>
2018-10-09 15:52:55 +0200

    Tweaked Copyright at some tests files

--- src/test/scala/code/api/v3_1_0/WebHooksTest.scala
+++ src/test/scala/code/api/v3_1_0/WebHooksTest.scala
@@ -20,15 +20,9 @@ TESOBE Ltd
 Osloerstrasse 16/17
 Berlin 13359, Germany
 
-  This product includes software developed at
-  TESOBE (http://www.tesobe.com/)
-  by
-  Simon Redfern : simon AT tesobe DOT com
-  Stefan Bethge : stefan AT tesobe DOT com
-  Everett Sochowski : everett AT tesobe DOT com
-  Ayoub Benali: ayoub AT tesobe DOT com
-
-  */
+This product includes software developed at
+TESOBE (http://www.tesobe.com/)
+*/
 package code.api.v3_1_0
 
 import code.api.ErrorMessage
