57bc64cbf9fd6f11f2f4a88f83995e127821e245
Marko Milić <marko@tesobe.com>
2018-10-23 14:45:02 +0200

    Added tests for endpoint getCustomerByCustomerNumber

--- src/test/scala/code/api/v3_1_0/CustomerTest.scala
+++ src/test/scala/code/api/v3_1_0/CustomerTest.scala
@@ -29,10 +29,11 @@ import code.api.ErrorMessage
 import code.api.util.APIUtil.OAuth._
 import code.api.util.ApiRole.{CanGetCustomer, canGetCustomer}
 import code.api.util.ErrorMessages._
-import code.api.util.{ApiVersion}
+import code.api.util.ApiVersion
 import code.api.v3_1_0.OBPAPI3_1_0.Implementations3_1_0
 import code.entitlement.Entitlement
 import com.github.dwickern.macros.NameOf.nameOf
+import net.liftweb.json.Serialization.write
 import org.scalatest.Tag
 
 class CustomerTest extends V310ServerSetup {
@@ -46,6 +47,9 @@ class CustomerTest extends V310ServerSetup {
     */
   object VersionOfApi extends Tag(ApiVersion.v3_1_0.toString)
   object ApiEndpoint extends Tag(nameOf(Implementations3_1_0.getCustomerByCustomerId))
+  object ApiEndpoint2 extends Tag(nameOf(Implementations3_1_0.getCustomerByCustomerNumber))
+
+  val customerNumberJson = PostCustomerNumberJsonV310(customer_number = "123")
 
   feature("Get Customer by CUSTOMER_ID v3.1.0 - Unauthorized access")
   {
@@ -84,6 +88,44 @@ class CustomerTest extends V310ServerSetup {
       response310.code should equal(400)
       And("error should be " + CustomerNotFoundByCustomerId)
       response310.body.extract[ErrorMessage].error should startWith (CustomerNotFoundByCustomerId)
+    }
+  }
+
+
+  feature("Get Customer by customer number v3.1.0 - Unauthorized access") {
+    scenario("We will call the endpoint without user credentials", ApiEndpoint2, VersionOfApi) {
+      val bankId = randomBankId
+      When("We make a request v3.1.0")
+      val request310 = (v3_1_0_Request / "banks" / bankId / "customers" / "customer-number").POST
+      val response310 = makePostRequest(request310, write(customerNumberJson))
+      Then("We should get a 400")
+      response310.code should equal(400)
+      And("error should be " + UserNotLoggedIn)
+      response310.body.extract[ErrorMessage].error should equal (UserNotLoggedIn)
+    }
+  }
+  feature("Get Customer by customer number v3.1.0 - Authorized access") {
+    scenario("We will call the endpoint without the proper Role " + canGetCustomer, ApiEndpoint2, VersionOfApi) {
+      val bankId = randomBankId
+      When("We make a request v3.1.0 without a Role " + canGetCustomer)
+      val request310 = (v3_1_0_Request / "banks" / bankId / "customers" / "customer-number").POST <@(user1)
+      val response310 = makePostRequest(request310, write(customerNumberJson))
+      Then("We should get a 403")
+      response310.code should equal(403)
+      And("error should be " + UserHasMissingRoles + CanGetCustomer)
+      response310.body.extract[ErrorMessage].error should equal (UserHasMissingRoles + CanGetCustomer)
+    }
+
+    scenario("We will call the endpoint with the proper Role " + canGetCustomer, ApiEndpoint2, VersionOfApi) {
+      val bankId = randomBankId
+      Entitlement.entitlement.vend.addEntitlement(bankId, resourceUser1.userId, CanGetCustomer.toString)
+      When("We make a request v3.1.0 with the Role " + canGetCustomer + " but with non existing customer number")
+      val request310 = (v3_1_0_Request / "banks" / bankId / "customers" / "customer-number").POST <@(user1)
+      val response310 = makePostRequest(request310, write(customerNumberJson))
+      Then("We should get a 400")
+      response310.code should equal(400)
+      And("error should be " + CustomerNotFound)
+      response310.body.extract[ErrorMessage].error should startWith (CustomerNotFound)
     }
   }
 
