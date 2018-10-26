11cf22997dde113e8d359ae7dd84c60fec399d87
Marko Milić <marko@tesobe.com>
2018-10-26 12:06:19 +0200

    Add Customer tax_residence resource #1108 - fixed json

--- src/main/scala/code/api/ResourceDocs1_4_0/SwaggerDefinitionsJSON.scala
+++ src/main/scala/code/api/ResourceDocs1_4_0/SwaggerDefinitionsJSON.scala
@@ -1980,7 +1980,7 @@ object SwaggerDefinitionsJSON {
   val postCustomerNumberJsonV310 = PostCustomerNumberJsonV310(customer_number = "123")
 
   val taxResidenceV310 = TaxResidenceV310(domain = "Enter some domain", tax_number = "Enter some number", tax_residence_id = "902ba3bb-dedd-45e7-9319-2fd3f2cd98a1")
-  val postTaxResidenceJsonV310 = taxResidenceV310
+  val postTaxResidenceJsonV310 = PostTaxResidenceJsonV310(domain = "Enter some domain", tax_number = "Enter some number")
   val taxResidenceJsonV310 = TaxResidenceJsonV310(tax_residence = List(taxResidenceV310))
 
 
