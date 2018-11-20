0b4af0077c88ef7aa7672c2a2a9ead332d0fb855
Marko Milić <marko@tesobe.com>
2018-11-20 10:57:38 +0100

    Optimised code for TaxResidence in Akka mode

--- src/main/scala/code/remotedata/RemotedataTaxResidenceActor.scala
+++ src/main/scala/code/remotedata/RemotedataTaxResidenceActor.scala
@@ -4,6 +4,8 @@ import akka.actor.Actor
 import code.actorsystem.ObpActorHelper
 import code.taxresidence.{MappedTaxResidenceProvider, RemotedataTaxResidenceCaseClasses}
 import code.util.Helper.MdcLoggable
+import akka.pattern.pipe
+import scala.concurrent.ExecutionContext.Implicits.global
 
 class RemotedataTaxResidenceActor extends Actor with ObpActorHelper with MdcLoggable {
 
@@ -14,15 +16,15 @@ class RemotedataTaxResidenceActor extends Actor with ObpActorHelper with MdcLogg
 
     case cc.getTaxResidence(customerId: String) =>
       logger.debug("getTaxResidence(" + customerId + ")")
-      sender ! (mapper.getTaxResidenceRemote(customerId))
+      mapper.getTaxResidence(customerId) pipeTo sender
 
     case cc.createTaxResidence(customerId: String, domain: String, taxNumber: String) =>
       logger.debug("addTaxResidence(" + customerId + ", " + domain + ", " + taxNumber + ")")
-      sender ! (mapper.createTaxResidenceRemote(customerId, domain, taxNumber))
+      mapper.createTaxResidence(customerId, domain, taxNumber) pipeTo sender
 
     case cc.deleteTaxResidence(taxResidenceId: String) =>
       logger.debug("deleteTaxResidence(" + taxResidenceId + ")")
-      sender ! (mapper.deleteTaxResidenceRemote(taxResidenceId))
+      mapper.deleteTaxResidence(taxResidenceId) pipeTo sender
 
     case message => logger.warn("[AKKA ACTOR ERROR - REQUEST NOT RECOGNIZED] " + message)
 
