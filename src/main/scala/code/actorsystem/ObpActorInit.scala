385676703b51e6cedd9eedf9b0488c58124adbb8
Marko Milić <marko@tesobe.com>
2018-12-18 13:13:00 +0100

    Avoided starting of unnecessary actors

--- src/main/scala/code/actorsystem/ObpActorInit.scala
+++ src/main/scala/code/actorsystem/ObpActorInit.scala
@@ -15,9 +15,8 @@ trait ObpActorInit extends MdcLoggable{
   // Default is 3 seconds, which should be more than enough for slower systems
   val ACTOR_TIMEOUT: Long = APIUtil.getPropsAsLongValue("remotedata.timeout").openOr(3)
 
-  val actorName = CreateActorNameFromClassName(this.getClass.getName)
-  val actor = ObpLookupSystem.getRemotedataActor(actorName)
-  logger.debug(s"Create this Actor: $actorName: ${actor}")
+  lazy val actorName = CreateActorNameFromClassName(this.getClass.getName)
+  lazy val actor = ObpLookupSystem.getRemotedataActor(actorName)
   val TIMEOUT = (ACTOR_TIMEOUT seconds)
   implicit val timeout = Timeout(ACTOR_TIMEOUT * (1000 milliseconds))
 
