39435b9f9b334d40a2235a1d6f024ee6d59cb356
chrisjsimpson <chris@karmacomputing.co.uk>
2019-03-03 18:10:17 +0000

    correct template path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['ntemplates']
+templates_path = ['templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
