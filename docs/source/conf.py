16544697ccb9e84cf9d56c5e52296f6003714ddd
chrisjsimpson <chris@karmacomputing.co.uk>
2019-03-03 17:49:25 +0000

    explicitly added sphinxcontrib-httpexample.js to html_js_files for sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -143,6 +143,8 @@ html_theme = 'alabaster'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+html_js_files = ['sphinxcontrib-httpexample.js']
+
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
