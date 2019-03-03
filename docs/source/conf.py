21070035f5179ca264367330d605ed6ebe40034c
chrisjsimpson <chris@karmacomputing.co.uk>
2019-03-03 17:58:45 +0000

    attempting to resolve static file paths readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -141,7 +141,7 @@ html_theme = 'alabaster'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = []
 
 html_js_files = ['sphinxcontrib-httpexample.js']
 
