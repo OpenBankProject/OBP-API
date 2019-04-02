7c16b56fd915014bdcb654755e61518794f0c746
hongwei <hongwei@tesobe.com>
2019-04-02 08:36:59 +0200

    code clean

--- obp-api/src/main/scala/code/api/util/APIUtil.scala
+++ obp-api/src/main/scala/code/api/util/APIUtil.scala
@@ -1906,12 +1906,6 @@ Returns a string showed to the developer
       x => (x._1, x._2.map(_.copy(ipAddress = getRemoteIpAddress())))
     } 
     
-    //a props filed from Default.props
-    if ("props.connector"=="kafka") 
-     res map {
-      x => (x._1, x._2.map(_.copy(authInfo = None)))
-    } else
-      res
   }
 
   /**
