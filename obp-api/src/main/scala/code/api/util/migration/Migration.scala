9db24622d17ce7390e2d71fbe91e4081afc4c9c2
Marko Milić <marko@tesobe.com>
2019-04-18 09:36:57 +0200

    Refactored the code of Migration

---
+++ obp-api/src/main/scala/code/api/util/migration/Migration.scala
@@ -0,0 +1,79 @@
+package code.api.util.migratin
+
+import code.api.util.APIUtil
+import code.api.util.APIUtil.getPropsAsBoolValue
+import code.consumer.Consumers
+import code.customer.Customer
+import code.migration.MigrationScriptLogProvider
+import code.util.Helper.MdcLoggable
+import com.github.dwickern.macros.NameOf.nameOf
+
+object Migration extends MdcLoggable {
+  
+  private val execute = getPropsAsBoolValue("migration_scripts.execute", false)
+
+  private def executeScript(blockOfCode: => Boolean): Boolean = {
+    if(execute) blockOfCode else execute
+  }
+  
+  private def runOnce(name: String)(blockOfCode: => Boolean): Boolean = {
+    MigrationScriptLogProvider.migrationScriptLogProvider.vend.isExecuted(name) match {
+      case false => blockOfCode
+      case true => true
+    }
+  }
+  
+  def saveLog(name: String, commitId: String, isSuccessful: Boolean, startDate: Long, endDate: Long, comment: String) = {
+    MigrationScriptLogProvider.migrationScriptLogProvider.vend.saveLog(name, commitId, isSuccessful, startDate, endDate, comment) match {
+      case true =>
+      case false =>
+        logger.warn(s"Migration.database.$name is executed at this instance but the corresponding log is not saved!!!!!!")
+    }
+  }
+  
+  object database {
+    
+    def executeScripts(): Boolean = executeScript {
+      dummyScript()
+      populateTableViewDefinition()
+      populateTableAccountAccess()
+    }
+    
+    private def dummyScript(): Boolean = {
+      val name = "Dummy test script"
+      runOnce(name) {
+        val startDate = System.currentTimeMillis()
+        val commitId: String = APIUtil.gitCommit
+        val comment: String = "dummy comment"
+        val isSuccessful = true
+        val endDate = System.currentTimeMillis()
+        saveLog(name, commitId, isSuccessful, startDate, endDate, comment)
+        isSuccessful
+      }
+    }
+    
+    private def populateTableAccountAccess(): Boolean = {
+      val name = nameOf(populateTableAccountAccess)
+      runOnce(name) {
+        TableAccountAccess.populate(name)
+      }
+    }
+
+    private def populateTableViewDefinition(): Boolean = {
+      val name = nameOf(populateTableViewDefinition)
+      runOnce(name) {
+        TableViewDefinition.populate(name)
+      }
+    }  
+    
+    private def generateAndPopulateMissingCustomerUUIDs(): Boolean = {
+      Customer.customerProvider.vend.populateMissingUUIDs()
+    }
+
+    private def generateAndPopulateMissingConsumersUUIDs(): Boolean = {
+      Consumers.consumers.vend.populateMissingUUIDs()
+    }
+    
+  }
+  
+}
---
+++ obp-api/src/main/scala/code/api/util/migration/MigrationOfAccountAccess.scala
@@ -0,0 +1,54 @@
+package code.api.util.migratin
+
+import code.api.util.APIUtil
+import code.api.util.migratin.Migration.saveLog
+import code.model.dataAccess.{ViewImpl, ViewPrivileges}
+import code.views.system.AccountAccess
+import net.liftweb.mapper.{By, ByList}
+
+object TableAccountAccess {
+  def populate(name: String): Boolean = {
+    val startDate = System.currentTimeMillis()
+    val commitId: String = APIUtil.gitCommit
+    val views = ViewImpl.findAll()
+
+    // Delete all rows at the table
+    AccountAccess.bulkDelete_!!()
+
+    // Insert rows into table "accountaccess" based on data in the tables viewimpl and viewprivileges
+    val insertedRows: List[Boolean] =
+      for {
+        view <- views
+        permission <- ViewPrivileges.findAll(By(ViewPrivileges.view, view.id))
+      } yield {
+        val viewId = ViewImpl.find(By(ViewImpl.id_, permission.view.get)).map(_.permalink_.get).getOrElse("")
+        AccountAccess
+          .create
+          .bank_id(view.bankPermalink.get)
+          .account_id(view.accountPermalink.get)
+          .user_fk(permission.user.get)
+          .view_id(viewId)
+          .save()
+      }
+    val isSuccessful = insertedRows.forall(_ == true)
+    val accountAccess = AccountAccess.findAll()
+    val accountAccessSize = accountAccess.size
+    val viewPrivileges = ViewPrivileges.findAll()
+    val viewPrivilegesSize = viewPrivileges.size
+
+    // We want to find foreign keys "viewprivileges.view_c" which cannot be mapped to "viewimpl.id_"
+    val x1 = ViewPrivileges.findAll(ByList(ViewPrivileges.view, views.map(_.id))).map(_.view.get).distinct.sortWith(_>_)
+    val x2 = viewPrivileges.map(_.view.get).distinct.sortWith(_>_)
+    val deadForeignKeys = x2.diff(x1)
+
+    val endDate = System.currentTimeMillis()
+    val comment: String =
+      s"""Account access size: $accountAccessSize;
+         |View privileges size: $viewPrivilegesSize;
+         |List of dead foreign keys at the field ViewPrivileges.view_c: ${deadForeignKeys.mkString(",")};
+         |Duration: ${endDate - startDate} ms;
+             """.stripMargin
+    saveLog(name, commitId, isSuccessful, startDate, endDate, comment)
+    isSuccessful
+  }
+}
