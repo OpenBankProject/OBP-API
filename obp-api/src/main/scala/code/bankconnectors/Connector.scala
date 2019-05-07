4084eff17e1c07fe9afbb924772b6ee8d34be78e
Marko Milić <marko@tesobe.com>
2019-05-07 16:19:09 +0200

    Fixed build failure

--- obp-api/src/main/scala/code/bankconnectors/Connector.scala
+++ obp-api/src/main/scala/code/bankconnectors/Connector.scala
@@ -1679,5 +1679,5 @@ trait Connector extends MdcLoggable with CustomJsonFormats{
   def getKycStatuses(customerId: String,
                    callContext: Option[CallContext]
                   ): OBPReturnType[Box[List[KycStatus]]] = Future{(Failure(setUnimplementedError), callContext)}
-  )
+  
 }
