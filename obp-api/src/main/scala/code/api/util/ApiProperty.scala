558f38408b058fc49c5bfd43591261f9bf587cff
Marko Milić <marko@tesobe.com>
2019-05-08 11:16:41 +0200

    Added diagram for function getValueByNameOrAliasAsBoolean

--- obp-api/src/main/scala/code/api/util/ApiProperty.scala
+++ obp-api/src/main/scala/code/api/util/ApiProperty.scala
@@ -6,9 +6,32 @@ import net.liftweb.common.Full
 
 object ApiProperty extends MdcLoggable {
   
-  val requireScopesForAllRoles = getValueByNameOrAlias("require_scopes_for_all_roles", "require_scopes")
+  val requireScopesForAllRoles = getValueByNameOrAliasAsBoolean("require_scopes_for_all_roles", "require_scopes")
 
-  private def getValueByNameOrAlias(name: String, alias: String): Boolean = {
+  /**
+    * Workflow of get property with boolean value
+    * 
+                      1st try                              2nd try
+     get         +---------------+                    +---------------+
+     property    |               |  no match          |               |  no match
+    +----------->+     Name      +------------------->+     Alias     +----------+
+                 |               |                    |               |          |
+                 +-------+-------+                    +-------+-------+          |
+                         |                                    |                  |
+         true/false      | match                              | match            |
+    <--------------------+                                    |                  |
+                                                              |                  |
+         true/false                                           |                  |
+    <---------------------------------------------------------+                  |
+                                                                                 |
+         false                                                                   |
+    <----------------------------------------------------------------------------+
+    *
+    * @param name The name of a property
+    * @param alias The alias of a property
+    * @return true/false
+    */
+  private def getValueByNameOrAliasAsBoolean(name: String, alias: String): Boolean = {
     (getPropsValue(name), getPropsValue(alias)) match {
       case (Full(actual), Full(deprecated)) => // Both properties are defined. Use actual one and log warning. {true/false}
         logger.warn(s"The props file has defined actual property name $name as well as deprecated $alias. The deprecated one is ignored!")
