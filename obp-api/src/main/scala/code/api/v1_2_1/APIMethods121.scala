31fd946f85c339f64892c7adffeb33deb1877ff9
Simon Redfern <simon@tesobe.com>
2019-06-19 12:14:33 +0200

    Updating Resource Doc for Revoke access to one View

--- obp-api/src/main/scala/code/api/v1_2_1/APIMethods121.scala
+++ obp-api/src/main/scala/code/api/v1_2_1/APIMethods121.scala
@@ -857,11 +857,24 @@ trait APIMethods121 {
       apiVersion,
       "removePermissionForUserForBankAccountForOneView",
       "DELETE",
-      "/banks/BANK_ID/accounts/ACCOUNT_ID/permissions/PROVIDER_ID/USER_ID/views/VIEW_ID",
+      "/banks/BANK_ID/accounts/ACCOUNT_ID/permissions/PROVIDER/PROVIDER_ID/views/VIEW_ID",
       "Revoke access to one View.",
-      s"""Revokes the user identified by PROVIDER_ID at their provider PROVIDER access to the view VIEW_ID at BANK_ID for account ACCOUNT_ID.
+      s"""Revokes access to a View on an Account for a certain User.
+         |
+         |The User is identified by PROVIDER_ID at their PROVIDER.
+         |
+         |The Account is specified by BANK_ID and ACCOUNT_ID.
+         |
+         |The View is specified by VIEW_ID.
+         |
         |
-        |Revoking a user access to a public view will return an error message.
+        |PROVIDER (may be a URL so) must be URL Encoded.
+        |
+        |PROVIDER_ID is normally equivalent to USERNAME. However, see Get User by ID or GET Current User for Provider information.
+        |
+        |Attempting to revoke access to a public view will return an error message.
+        |
+        |An Account Owner cannot revoke access to an Owner View unless at least one other User has Owner View access.
         |
         |${authenticationRequiredMessage(true)} and the user needs to have access to the owner view.""",
       emptyObjectJson,
