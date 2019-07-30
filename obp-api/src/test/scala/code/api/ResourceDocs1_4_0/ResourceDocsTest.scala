64d235917d8f9b02df807eaaffc215fcf6093c42
hongwei <hongwei@tesobe.com>
2019-07-30 10:42:18 +0200

    added the tests for swagger string

--- obp-api/src/test/scala/code/api/ResourceDocs1_4_0/ResourceDocsTest.scala
+++ obp-api/src/test/scala/code/api/ResourceDocs1_4_0/ResourceDocsTest.scala
@@ -1,10 +1,13 @@
 package code.api.v3_1_0
 
+import net.liftweb.json.JValue
+import org.scalatest.Tag
+import io.swagger.parser.OpenAPIParser
+import java.util
 import code.api.ResourceDocs1_4_0.ResourceDocsV140ServerSetup
 import code.api.util.{ApiVersion, CustomJsonFormats}
 import code.api.v1_4_0.JSONFactory1_4_0.ImplementedByJson
-import net.liftweb.json.JValue
-import org.scalatest.Tag
+import net.liftweb.json
 
 import scala.collection.immutable.List
 class ResourceDocsTest extends ResourceDocsV140ServerSetup with CustomJsonFormats{
@@ -99,6 +102,14 @@ class ResourceDocsTest extends ResourceDocsV140ServerSetup with CustomJsonFormat
       responseGetObp.code should equal(200)
       responseGetObp.body.extract[ResourceDocsJson]
     }
+    
+    scenario(s"We will test ${ApiEndpoint1.name} Api -v1.3.0", ApiEndpoint1, VersionOfApi) {
+      val requestGetObp = (ResourceDocsV4_0Request / "resource-docs" / "v1.3.0" / "obp").GET 
+      val responseGetObp = makeGetRequest(requestGetObp)
+      And("We should get  200 and the response can be extract to case classes")
+      responseGetObp.code should equal(200)
+      responseGetObp.body.extract[ResourceDocsJson]
+    }
 
     scenario(s"We will test ${ApiEndpoint1.name} Api -v1.2.1", ApiEndpoint1, VersionOfApi) {
       val requestGetObp = (ResourceDocsV4_0Request / "resource-docs" / "v1.2.1" / "obp").GET 
@@ -115,7 +126,11 @@ class ResourceDocsTest extends ResourceDocsV140ServerSetup with CustomJsonFormat
       val responseGetObp = makeGetRequest(requestGetObp)
       And("We should get  200 and the response can be extract to case classes")
       responseGetObp.code should equal(200)
-      //TODO, should have more tests to make sure swagger file is validated .
+      val swaggerJsonString = json.compactRender(responseGetObp.body)
+      val validatedSwaggerResult = ValidateSwaggerString(swaggerJsonString)
+      val errors = validatedSwaggerResult._1
+      if (!errors.isEmpty) logger.info(s"Here is the wrong swagger json:    $swaggerJsonString")
+      errors.isEmpty should be (true)
     }
 
     scenario(s"We will test ${ApiEndpoint2.name} Api - v3.1.1", ApiEndpoint2, VersionOfApi) {
@@ -123,8 +138,140 @@ class ResourceDocsTest extends ResourceDocsV140ServerSetup with CustomJsonFormat
       val responseGetObp = makeGetRequest(requestGetObp)
       And("We should get  200 and the response can be extract to case classes")
       responseGetObp.code should equal(200)
-      //TODO, should have more tests to make sure swagger file is validated .
+      val swaggerJsonString = json.compactRender(responseGetObp.body)
+
+      val validatedSwaggerResult = ValidateSwaggerString(swaggerJsonString)
+      val errors = validatedSwaggerResult._1
+      errors.isEmpty should be (true)
+    }
+
+    scenario(s"We will test ${ApiEndpoint2.name} Api - v3.0.0", ApiEndpoint2, VersionOfApi) {
+      val requestGetObp = (ResourceDocsV4_0Request / "resource-docs" / "v3.0.0" / "swagger").GET
+      val responseGetObp = makeGetRequest(requestGetObp)
+      And("We should get  200 and the response can be extract to case classes")
+      responseGetObp.code should equal(200)
+      val swaggerJsonString = json.compactRender(responseGetObp.body)
+      val validatedSwaggerResult = ValidateSwaggerString(swaggerJsonString)
+      val errors = validatedSwaggerResult._1
+      errors.isEmpty should be (true)
+    }
+
+    scenario(s"We will test ${ApiEndpoint2.name} Api - v2.2.0", ApiEndpoint2, VersionOfApi) {
+      val requestGetObp = (ResourceDocsV4_0Request / "resource-docs" / "v2.2.0" / "swagger").GET
+      val responseGetObp = makeGetRequest(requestGetObp)
+      And("We should get  200 and the response can be extract to case classes")
+      responseGetObp.code should equal(200)
+      val swaggerJsonString = json.compactRender(responseGetObp.body)
+
+      val validatedSwaggerResult = ValidateSwaggerString(swaggerJsonString)
+      val errors = validatedSwaggerResult._1
+      errors.isEmpty should be (true)
+    }
+
+    scenario(s"We will test ${ApiEndpoint2.name} Api - v2.1.0", ApiEndpoint2, VersionOfApi) {
+      val requestGetObp = (ResourceDocsV4_0Request / "resource-docs" / "v2.1.0" / "swagger").GET
+      val responseGetObp = makeGetRequest(requestGetObp)
+      And("We should get  200 and the response can be extract to case classes")
+      responseGetObp.code should equal(200)
+      val swaggerJsonString = json.compactRender(responseGetObp.body)
+      val validatedSwaggerResult = ValidateSwaggerString(swaggerJsonString)
+      val errors = validatedSwaggerResult._1
+      errors.isEmpty should be (true)
+    }
+
+    scenario(s"We will test ${ApiEndpoint2.name} Api - v2.0.0", ApiEndpoint2, VersionOfApi) {
+      val requestGetObp = (ResourceDocsV4_0Request / "resource-docs" / "v2.0.0" / "swagger").GET
+      val responseGetObp = makeGetRequest(requestGetObp)
+      And("We should get  200 and the response can be extract to case classes")
+      responseGetObp.code should equal(200)
+      val swaggerJsonString = json.compactRender(responseGetObp.body)
+
+      val validatedSwaggerResult = ValidateSwaggerString(swaggerJsonString)
+      val errors = validatedSwaggerResult._1
+      errors.isEmpty should be (true)
+    }
+
+    scenario(s"We will test ${ApiEndpoint2.name} Api - v1.4.0", ApiEndpoint2, VersionOfApi) {
+      val requestGetObp = (ResourceDocsV4_0Request / "resource-docs" / "v1.4.0" / "swagger").GET
+      val responseGetObp = makeGetRequest(requestGetObp)
+      And("We should get  200 and the response can be extract to case classes")
+      responseGetObp.code should equal(200)
+      val swaggerJsonString = json.compactRender(responseGetObp.body)
+
+      val validatedSwaggerResult = ValidateSwaggerString(swaggerJsonString)
+      val errors = validatedSwaggerResult._1
+      errors.isEmpty should be (true)
+    }
+
+    scenario(s"We will test ${ApiEndpoint2.name} Api - v1.3.0", ApiEndpoint2, VersionOfApi) {
+      val requestGetObp = (ResourceDocsV4_0Request / "resource-docs" / "v1.3.0" / "swagger").GET
+      val responseGetObp = makeGetRequest(requestGetObp)
+      And("We should get  200 and the response can be extract to case classes")
+      responseGetObp.code should equal(200)
+      val swaggerJsonString = json.compactRender(responseGetObp.body)
+
+      val validatedSwaggerResult = ValidateSwaggerString(swaggerJsonString)
+      val errors = validatedSwaggerResult._1
+      errors.isEmpty should be (true)
+    }
+
+    scenario(s"We will test ${ApiEndpoint2.name} Api - v1.2.1", ApiEndpoint2, VersionOfApi) {
+      val requestGetObp = (ResourceDocsV4_0Request / "resource-docs" / "v1.2.1" / "swagger").GET
+      val responseGetObp = makeGetRequest(requestGetObp)
+      And("We should get  200 and the response can be extract to case classes")
+      responseGetObp.code should equal(200)
+      val swaggerJsonString = json.compactRender(responseGetObp.body)
+
+      val validatedSwaggerResult = ValidateSwaggerString(swaggerJsonString)
+      val errors = validatedSwaggerResult._1
+      errors.isEmpty should be (true)
     }
     
+  }
+  
+  
+  
+  
+  //Note: it is tricky to validate the swagger string, I just find this : https://github.com/swagger-api/swagger-parser/issues/718
+  //So follow it to call the `Validate` method:
+  //https://github.com/OpenAPITools/openapi-generator/blob/master/modules/openapi-generator-cli/src/main/java/org/openapitools/codegen/cmd/Validate.java#L46
+  def ValidateSwaggerString (swaggerJsonString: String)= {
+    val result = new OpenAPIParser().readContents(swaggerJsonString, null, null)
+    val messageList: util.List[String] = result.getMessages()
+
+    val errors = new util.HashSet[String](messageList)
+    val warnings = new util.HashSet[String]
+
+    val sb = new StringBuilder
+
+    if (!errors.isEmpty) {
+      sb.append("Errors:").append(System.lineSeparator)
+      errors.forEach((msg: String) => sb.append("\t-").append(msg).append(System.lineSeparator))
+    }
+
+    if (!warnings.isEmpty) {
+      sb.append("Warnings: ").append(System.lineSeparator)
+      warnings.forEach((msg: String) => sb.append("\t-").append(msg).append(System.lineSeparator))
+    }
+
+    if (!errors.isEmpty) {
+      sb.append(System.lineSeparator)
+      sb.append("[error] Spec has ").append(errors.size).append(" errors.")
+      System.err.println(sb.toString)
+      System.exit(1)
+    }
+    else if (!warnings.isEmpty) {
+      sb.append(System.lineSeparator)
+      sb.append("[info] Spec has ").append(warnings.size).append(" recommendation(s).")
+    }
+    else { // we say "issues" here rather than "errors" to account for both errors and issues.
+      sb.append("No validation issues detected.")
+    }
+    val allMessages = sb.toString
+    logger.info(s"validatedSwaggerResult.errors $errors")
+    logger.info(s"validatedSwaggerResult.warnings $warnings")
+    logger.info(s"validatedSwaggerResult.allMessages $allMessages")
+    
+    (errors, warnings, allMessages)
   }
 } 
