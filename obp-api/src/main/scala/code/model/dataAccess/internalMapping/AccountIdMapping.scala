69582fba83148e4ff62ecd41261de22d8c054df4
hongwei <hongwei@tesobe.com>
2019-08-23 14:23:01 +0200

    added the AccountIdMapping

---
+++ obp-api/src/main/scala/code/model/dataAccess/internalMapping/AccountIdMapping.scala
@@ -0,0 +1,22 @@
+package code.model.dataAccess.internalMapping
+
+import code.util.MappedUUID
+import com.openbankproject.commons.model.{BankId, AccountId}
+import net.liftweb.mapper._
+
+class AccountIdMapping extends AccountIdMappingT with LongKeyedMapper[AccountIdMapping] with IdPK with CreatedUpdated {
+
+  def getSingleton = AccountIdMapping
+
+  object mAccountId extends MappedUUID(this)
+  object mAccountReference extends MappedString(this, 255)
+
+  override def accountId = AccountId(mAccountId.get)
+  override def accountPlainTextReference = mAccountReference.get
+  
+}
+
+object AccountIdMapping extends AccountIdMapping with LongKeyedMetaMapper[AccountIdMapping] {
+  //one account info per bank for each api user
+  override def dbIndexes = UniqueIndex(mAccountId) :: UniqueIndex(mAccountId, mAccountReference) :: super.dbIndexes
+}
