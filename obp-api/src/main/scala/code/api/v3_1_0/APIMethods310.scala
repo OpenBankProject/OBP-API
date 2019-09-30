6795cd3c70bc4be5b24ecb454b2f2f34959c2d7a
hongwei <hongwei@tesobe.com>
2019-09-30 14:23:22 +0200

    tweaked the customers response

--- obp-api/src/main/scala/code/api/v3_1_0/APIMethods310.scala
+++ obp-api/src/main/scala/code/api/v3_1_0/APIMethods310.scala
@@ -468,7 +468,7 @@ trait APIMethods310 {
          |
          |""".stripMargin,
       emptyObjectJson,
-      transactionsJsonV300,
+      customerJSONs,
       List(UserNotLoggedIn, FirehoseViewsNotAllowedOnThisInstance, UserHasMissingRoles, UnknownError),
       Catalogs(notCore, notPSD2, notOBWG),
       List(apiTagAccountFirehose, apiTagAccount, apiTagFirehoseData, apiTagNewStyle),
