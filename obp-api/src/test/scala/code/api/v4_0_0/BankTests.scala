8f237d6614310f6d4198e8bf30106914bb26eff2
Marko Milić <marko@tesobe.com>
2019-11-05 11:57:23 +0100

    Added endpoint createBank v4.0.0 - improved tests

--- obp-api/src/test/scala/code/api/v4_0_0/BankTests.scala
+++ obp-api/src/test/scala/code/api/v4_0_0/BankTests.scala
@@ -1,14 +1,14 @@
 package code.api.v4_0_0
 
 import code.api.ErrorMessage
-import code.api.ResourceDocs1_4_0.SwaggerDefinitionsJSON
+import code.api.ResourceDocs1_4_0.SwaggerDefinitionsJSON.bankJSONV220
 import code.api.util.APIUtil.OAuth._
-import code.api.util.ApiRole.{CanCreateBank, CanGetEntitlementsForAnyUserAtAnyBank, canCreateBank}
+import code.api.util.ApiRole.CanCreateBank
 import code.api.util.ErrorMessages.UserHasMissingRoles
-import code.api.util.{ApiRole, ApiVersion, ErrorMessages}
+import code.api.util.{ApiRole, ApiVersion, ErrorMessages, NewStyle}
 import code.api.v4_0_0.APIMethods400.Implementations4_0_0
 import code.entitlement.Entitlement
-import code.setup.DefaultUsers
+import code.setup.{APIResponse, DefaultUsers}
 import com.github.dwickern.macros.NameOf.nameOf
 import net.liftweb.json.Serialization.write
 import org.scalatest.Tag
@@ -38,7 +38,7 @@ class BankTests extends V400ServerSetupAsync with DefaultUsers {
     scenario("We try to consume endpoint createBank - Anonymous access", ApiEndpoint1, VersionOfApi) {
       When("We make the request")
       val requestGet = (v4_0_0_Request / "banks").POST
-      val responseGet = makePostRequestAsync(requestGet, write(SwaggerDefinitionsJSON.bankJSONV220))
+      val responseGet = makePostRequestAsync(requestGet, write(bankJSONV220))
       Then("We should get a 400")
       And("We should get a message: " + ErrorMessages.UserNotLoggedIn)
       responseGet map { r =>
@@ -50,7 +50,7 @@ class BankTests extends V400ServerSetupAsync with DefaultUsers {
     scenario("We try to consume endpoint createBank without proper role - Authorized access", ApiEndpoint1, VersionOfApi) {
       When("We make the request")
       val requestGet = (v4_0_0_Request / "banks").POST <@ (user1)
-      val responseGet = makePostRequestAsync(requestGet, write(SwaggerDefinitionsJSON.bankJSONV220))
+      val responseGet = makePostRequestAsync(requestGet, write(bankJSONV220))
       Then("We should get a 403")
       And("We should get a message: " + s"$CanCreateBank entitlement required")
       responseGet map { r =>
@@ -64,10 +64,20 @@ class BankTests extends V400ServerSetupAsync with DefaultUsers {
       Entitlement.entitlement.vend.addEntitlement("", resourceUser1.userId, ApiRole.CanCreateBank.toString)
       And("We make the request")
       val requestGet = (v4_0_0_Request / "banks").POST <@ (user1)
-      val responseGet = makePostRequestAsync(requestGet, write(SwaggerDefinitionsJSON.bankJSONV220))
+      val response = for {
+        before <- NewStyle.function.getEntitlementsByUserId(resourceUser1.userId, None) map {
+          _.exists( e => e.roleName == ApiRole.CanCreateEntitlementAtOneBank.toString && e.bankId == bankJSONV220.id)
+        }
+        response: APIResponse <- makePostRequestAsync(requestGet, write(bankJSONV220))
+        after <- NewStyle.function.getEntitlementsByUserId(resourceUser1.userId, None) map {
+          _.exists( e => e.roleName == ApiRole.CanCreateEntitlementAtOneBank.toString && e.bankId == bankJSONV220.id)
+        }
+      } yield (before, after, response)
       Then("We should get a 201")
-      responseGet map { r =>
-          r.code should equal(201)
+      response map { r =>
+          r._1 should equal(false) // Before we create a bank there is no role CanCreateEntitlementAtOneBank
+          r._2 should equal(true) // After we create a bank there is a role CanCreateEntitlementAtOneBank
+          r._3.code should equal(201)
       }
     }
   }
