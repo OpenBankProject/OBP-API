b4e23b44ad46a15372843836010c9ba306daa557
Marko Milić <marko@tesobe.com>
2019-12-03 15:43:59 +0100

    System views - alpha release fixes

--- obp-api/src/main/scala/code/api/util/migration/MigrationOfSystemViewsToCustomViews.scala
+++ obp-api/src/main/scala/code/api/util/migration/MigrationOfSystemViewsToCustomViews.scala
@@ -3,10 +3,8 @@ package code.api.util.migration
 import java.time.format.DateTimeFormatter
 import java.time.{ZoneId, ZonedDateTime}
 
-import code.api.Constant._
 import code.api.util.APIUtil
 import code.api.util.migration.Migration.{DbFunction, saveLog}
-import code.views.Views
 import code.views.system.{AccountAccess, ViewDefinition}
 import net.liftweb.mapper.{By, DB, NotNullRef, NullRef}
 import net.liftweb.util.DefaultConnectionIdentifier
@@ -66,11 +64,6 @@ object UpdateTableViewDefinition {
             accountAccess.view_id(view.viewId.value).save()
           }
         
-        // Create system views
-        val owner = Views.views.vend.getOrCreateSystemView(SYSTEM_OWNER_VIEW_ID).isDefined
-        val auditor = Views.views.vend.getOrCreateSystemView(SYSTEM_AUDITOR_VIEW_ID).isDefined
-        val accountant = Views.views.vend.getOrCreateSystemView(SYSTEM_ACCOUNTANT_VIEW_ID).isDefined
-        
         val isSuccessful = views.forall(_.name.startsWith("_"))
         val endDate = System.currentTimeMillis()
         val comment: String =
@@ -78,9 +71,6 @@ object UpdateTableViewDefinition {
              |Number of instance specific system views: ${instanceSpecificSystemViews.size}
              |Number of bank specific system views: ${bankSpecificSystemViews.size}
              |Number of updated rows at table AccountAccess: ${updatedAccountAccessRows.size}
-             |System view ${SYSTEM_OWNER_VIEW_ID} exist/created at the instance: ${owner}
-             |System view ${SYSTEM_AUDITOR_VIEW_ID} exist/created at the instance: ${auditor}
-             |System view ${SYSTEM_ACCOUNTANT_VIEW_ID} exist/created at the instance: ${accountant}
              |""".stripMargin
         saveLog(name, commitId, isSuccessful, startDate, endDate, comment)
         isSuccessful
