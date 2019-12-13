d1e8de0e9b364ae70f994a908421a768f40d2e1f
shuang <shuang@tesobe.com>
2019-12-13 21:26:05 +0800

    feature/stored_procedure_connector: move db connection settings to props file.

--- obp-api/src/main/scala/code/bankconnectors/storedprocedure/MssqlDBUtils.scala
+++ obp-api/src/main/scala/code/bankconnectors/storedprocedure/MssqlDBUtils.scala
@@ -2,17 +2,42 @@ package code.bankconnectors.storedprocedure
 
 import java.sql.PreparedStatement
 
+import code.api.util.APIUtil
 import com.openbankproject.commons.model.TopicTrait
 import net.liftweb.json
 import net.liftweb.json.JValue
 import net.liftweb.json.Serialization.write
 import scalikejdbc.{DB, _}
-import scalikejdbc.config.DBs
 
 object MssqlDBUtils {
-  DBs.setupAll()
   private implicit val formats = code.api.util.CustomJsonFormats.formats
   private val before: PreparedStatement => Unit = _ => ()
+
+  // lazy initial DB connection
+  {
+    val driver = APIUtil.getPropsValue("db.default.driver").openOrThrowException("mandatory property db.default.driver is missing!")
+    val url = APIUtil.getPropsValue("db.default.url").openOrThrowException("mandatory property db.default.url is missing!")
+    val user = APIUtil.getPropsValue("db.default.user").openOrThrowException("mandatory property db.default.user is missing!")
+    val password = APIUtil.getPropsValue("db.default.password").openOrThrowException("mandatory property db.default.password is missing!")
+
+    val initialSize = APIUtil.getPropsAsIntValue("db.default.poolInitialSize", 5)
+    val maxSize = APIUtil.getPropsAsIntValue("db.default.poolMaxSize", 20)
+    val timeoutMillis = APIUtil.getPropsAsLongValue("db.default.poolConnectionTimeoutMillis", 3000L)
+    val validationQuery = APIUtil.getPropsValue("db.default.poolValidationQuery", "select 1 from dual")
+    val poolFactoryName = APIUtil.getPropsValue("db.default.poolFactoryName", "defaultPoolFactory")
+
+
+    Class.forName(driver)
+    val settings = ConnectionPoolSettings(
+      initialSize = initialSize,
+      maxSize = maxSize,
+      connectionTimeoutMillis = timeoutMillis,
+      validationQuery = validationQuery,
+      connectionPoolFactoryName = poolFactoryName
+    )
+    ConnectionPool.singleton(url, user, password, settings)
+  }
+
 
   def callMsProcedure[T: Manifest](procedureName: String, outBound: TopicTrait): T = {
     val procedureParam: String = write(outBound) // convert OutBound to json string
