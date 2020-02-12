2289e948e50ae0c0d70807090681c09155631ce8
hongwei <hongwei@tesobe.com>
2020-02-12 16:04:48 +0100

    added the customerAttributes tests -- step2

--- obp-api/src/test/scala/code/api/v4_0_0/CustomerAttributesTest.scala
+++ obp-api/src/test/scala/code/api/v4_0_0/CustomerAttributesTest.scala
@@ -2,10 +2,13 @@ package code.api.v4_0_0
 
 import code.api.ResourceDocs1_4_0.SwaggerDefinitionsJSON
 import code.api.util.APIUtil.OAuth._
+import code.api.util.ApiRole.canCreateCustomerAttributeAtOneBank
 import code.api.util.ErrorMessages.{UserHasMissingRoles, UserNotLoggedIn}
+import code.api.v3_1_0.CustomerAttributeResponseJson
 import code.api.v4_0_0.OBPAPI4_0_0.Implementations4_0_0
+import code.entitlement.Entitlement
 import com.github.dwickern.macros.NameOf.nameOf
-import com.openbankproject.commons.model.{ ErrorMessage}
+import com.openbankproject.commons.model.ErrorMessage
 import com.openbankproject.commons.util.ApiVersion
 import net.liftweb.json.Serialization.write
 import org.scalatest.Tag
@@ -27,6 +30,7 @@ class CustomerAttributesTest extends V400ServerSetup {
   
   lazy val bankId = randomBankId
   lazy val postCustomerAttributeJsonV400 = SwaggerDefinitionsJSON.customerAttributeJsonV400
+  lazy val putCustomerAttributeJsonV400 = SwaggerDefinitionsJSON.customerAttributeJsonV400.copy(name="test")
   lazy val customerId = createAndGetCustomerId(bankId, user1)
   
 
@@ -51,49 +55,27 @@ class CustomerAttributesTest extends V400ServerSetup {
       response400.body.extract[ErrorMessage].message.toString contains (UserHasMissingRoles) should be (true)
     }
   }
-//
-//  feature(s"test $ApiEndpoint1 version $VersionOfApi - authorized access- with role- success") {
-//    scenario("We will call the endpoint without user credentials", ApiEndpoint1, VersionOfApi) {
-//      When("We make a request v4.0.0")
-//      val request400 = (v4_0_0_Request / "banks" / bankId / "customers" / customerId / "attribute").POST <@ (user1)
-//      val response400 = makePostRequest(request400, write(postCustomerAttributeJsonV400))
-//      Then("We should get a 403")
-//      response400.code should equal(403)
-//      response400.body.extract[ErrorMessage].message.toString contains (UserHasMissingRoles) should be (true)
-//    }
-//  }
-//  feature(s"test $ApiEndpoint2 version $VersionOfApi - Unauthorized access") {
-//    scenario("We will call the endpoint without user credentials", ApiEndpoint1, VersionOfApi) {
-//      When("We make a request v4.0.0")
-//      val request400 = (v4_0_0_Request / "banks" / bankId / "accounts" / bankAccount.id / "account-access" / "revoke").POST
-//      val response400 = makePostRequest(request400, write(postAccountAccessJson))
-//      Then("We should get a 400")
-//      response400.code should equal(400)
-//      response400.body.extract[ErrorMessage].message should equal(UserNotLoggedIn)
-//    }
-//  }
-//  feature(s"test $ApiEndpoint1 and $ApiEndpoint2 version $VersionOfApi - Authorized access") {
-//    scenario("We will call the endpoint with user credentials", VersionOfApi, ApiEndpoint1, ApiEndpoint2) {
-//      
-//      val account = createAnAccount(bankId, user1)
-//      val view = createViewForAnAccount(bankId, account.account_id)
-//      val postJson = PostAccountAccessJsonV400(resourceUser2.userId, PostViewJsonV400(view.id, view.is_system))
-//      When("We send the request")
-//      val request = (v4_0_0_Request / "banks" / bankId / "accounts" / account.account_id / "account-access" / "grant").POST <@ (user1)
-//      val response = makePostRequest(request, write(postJson))
-//      Then("We should get a 201 and check the response body")
-//      response.code should equal(201)
-//      response.body.extract[ViewJsonV300]
-//      
-//      When("We send the request")
-//      val requestRevoke = (v4_0_0_Request / "banks" / bankId / "accounts" / account.account_id / "account-access" / "revoke").POST <@ (user1)
-//      val responseRevoke = makePostRequest(requestRevoke, write(postJson))
-//      Then("We should get a 201 and check the response body")
-//      responseRevoke.code should equal(201)
-//      responseRevoke.body.extract[RevokedJsonV400]
-//    }
-//  }
-
   
+  feature(s"test $ApiEndpoint1 version $VersionOfApi - authorized access - with role - should be success!") {
+    scenario("We will call the endpoint without user credentials", ApiEndpoint1, VersionOfApi) {
+      When("We make a request v4.0.0")
+      val request400 = (v4_0_0_Request / "banks" / bankId / "customers" / customerId / "attribute").POST <@ (user1)
+      val response400 = makePostRequest(request400, write(putCustomerAttributeJsonV400))
+      Then("We should get a 403")
+      response400.code should equal(403)
+      response400.body.extract[ErrorMessage].message.toString contains (UserHasMissingRoles) should be (true)
+      
+      Then("We should get a 403")
+      Entitlement.entitlement.vend.addEntitlement(bankId, resourceUser1.userId, canCreateCustomerAttributeAtOneBank.toString)
+
+      val responseWithRole = makePostRequest(request400, write(putCustomerAttributeJsonV400))
+      Then("We should get a 201")
+      responseWithRole.code should equal(201)
+      responseWithRole.body.extract[CustomerAttributeResponseJson].name equals(postCustomerAttributeJsonV400.name)
+      responseWithRole.body.extract[CustomerAttributeResponseJson].value equals(postCustomerAttributeJsonV400.value)
+      responseWithRole.body.extract[CustomerAttributeResponseJson].`type` equals(postCustomerAttributeJsonV400.`type`)
+    }
+  }
+ 
   
 }
