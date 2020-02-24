01a49e383293e2493586d12aea2d4e1dc9060574
shuang <shuang@tesobe.com>
2020-02-24 10:01:13 +0800

    feature/inbound_outbound_structure_convert implemented generate Json according schema.

---
+++ obp-api/src/main/scala/code/util/JsonUtils.scala
@@ -0,0 +1,401 @@
+package code.util
+
+import net.liftweb.json
+import net.liftweb.json.{JNothing, JNull}
+import net.liftweb.json.JsonAST.{JArray, JBool, JDouble, JField, JInt, JObject, JString, JValue}
+import org.apache.commons.lang3.StringUtils
+import net.liftweb.json.JsonDSL._
+
+object JsonUtils {
+  /* match string that end with '[number]', e.g: hobby[3] */
+  private val RegexArrayIndex = """(.*?)\[(\d+)\]$""".r
+  /* match string that end with '[]', e.g: hobby[] */
+  private val RegexArray = """(.*?)\[\]$""".r
+
+  /* match string !abc.efg or -abc.efg or abc.efg */
+  private val RegexSingleExp = """([!-])?\s*(.+?)\s*$""".r
+  /* match string -abc.efg * hij.klm or abc.efg ~ hij.klm */
+  private val RegexMultipleExp = """([!-]?\s*.+?)\s*(\+|\-|\*|/|~|&|\|)\s*(.+)""".r
+
+  /* match 'boolean: true' style string, to extract boolean value */
+  private val RegexBoolean = """(?i)\s*'\s*boolean\s*:\s*(.*)'\s*""".r
+  /* match 'double: 123.11' style string, to extract number value */
+  private val RegexDouble = """(?i)\s*'\s*(double|number)\s*:\s*(.*)'\s*""".r
+  /* match 'int: 123.11' style string, to extract number value */
+  private val RegexInt = """(?i)\s*'\s*int\s*:\s*(.*)'\s*""".r
+  /* match 'abc' style string, to extract abc */
+  private val RegexStr = """\s*'(.*)'\s*""".r
+
+  def convertJson(source: JValue, schema: JValue) = {
+    transformField(schema){
+      case (jField, path) if path.contains("$default") =>
+        jField
+
+      case (JField(name, v), _) if name.endsWith("$default") =>
+        val newName = StringUtils.substringBeforeLast(name,"$default")
+        JField(newName, v)
+
+      case (JField(name, JString(s)), _) if name.endsWith("[]") => {
+        val jValue = calculateValue(source, s)
+        val jArray = jValue match {
+          case array: JArray => array
+          case _ @JNothing | _ @JNull => JArray(Nil)
+          case v => JArray(List(v))
+        }
+        val newName = StringUtils.substringBeforeLast(name, "[]")
+        JField(newName, jArray)
+      }
+
+      case (JField(name, jArray: JArray), _) if name.endsWith("[]") =>
+        val newName = StringUtils.substringBeforeLast(name, "[]")
+        JField(newName, jArray)
+
+      case (JField(name, jObj @JObject(jFields)), _) if name.endsWith("[]") =>
+        val newName = StringUtils.substringBeforeLast(name, "[]")
+        if(jFields.isEmpty) {
+          JField(newName, JArray(Nil))
+        } else if(allValeIsSameSizeArray(jFields)) {
+          /*convert the follow structure
+           * {
+           *  "foo[]": {
+           *    "name": ["Ken", "Billy"],
+           *    "address": [{
+           *        "name": "Berlin",
+           *        "PostCode" : 116100
+           *      }, {
+           *        "name": "DaLian",
+           *        "PostCode": 11660
+           *      }]
+           *  }
+           * }
+           * to real list:
+           *{
+           *  "foo": [{
+           *     "name": "Ken",
+           *     "address": {
+           *        "name": "Berlin",
+           *        "PostCode" : 116100
+           *      }
+           *    }, {
+           *    "name": "Billy",
+           *    "address": {
+           *        "name": "DaLian",
+           *        "PostCode": 11660
+           *    }
+           * }]
+           */
+
+          val arraySize = jFields.head.value.asInstanceOf[JArray].arr.size
+          val allFields = for {
+            i <- (0 until arraySize).toList
+            newJFields = jFields.collect {
+              case JField(fieldName, JArray(arr)) => JField(fieldName, arr(i))
+            }
+          } yield {
+            JObject(newJFields)
+          }
+          JField(newName, JArray(allFields))
+        } else {
+          JField(newName, JArray(jObj :: Nil))
+        }
+
+      // to avoid regex do check multiple times, make top case to match regex.
+      case (JField(name, jValue), _) if RegexArrayIndex.findFirstIn(name).isDefined => {
+        val RegexArrayIndex(newName, indexStr) = name
+        val index = indexStr.toInt
+        jValue match {
+          case JString(s) =>
+            val value = getIndexValue(calculateValue(source, s), index)
+            JField(newName, value)
+
+          case jArray: JArray => JField(newName, jArray(index))
+
+          case jObj @JObject(jFields) =>
+            if(jFields.isEmpty) {
+              JField(newName, JNothing)
+            } else if(allValeIsSameSizeArray(jFields)) {
+              /*convert the follow structure
+               * {
+               *  "foo[1]": {
+               *    "name": ["Ken", "Billy"],
+               *    "address": [{
+               *        "name": "Berlin",
+               *        "PostCode" : 116100
+               *      }, {
+               *        "name": "DaLian",
+               *        "PostCode": 11660
+               *      }]
+               *  }
+               * }
+               * to real list:
+               *{
+               *  "foo": [{
+               *     "name": "Ken",
+               *     "address": {
+               *        "name": "Berlin",
+               *        "PostCode" : 116100
+               *      }
+               *    }]
+               */
+              val newFields =  jFields.collect {
+                case JField(fieldName, arr :JArray) => JField(fieldName, getIndexValue(arr, index))
+              }
+              if(newFields.forall(_.value == JNothing)) JField(newName, JNothing) else JField(newName, JObject(newFields))
+            } else {
+              JField(newName, jObj)
+            }
+        }
+
+      }
+
+      case (JField(name, JString(s)), _) =>
+        JField(name, calculateValue(source, s))
+
+    }
+  }
+
+  def getValueByPath(jValue: JValue, p: String): JValue = {
+    p match {
+      case RegexBoolean(b) => JBool(b.toBoolean)
+      case RegexDouble(n) => JDouble(n.toDouble)
+      case RegexInt(n) => JInt(n.toInt)
+      case RegexStr(s) => JString(s.replace("''", "'"))// escape '' to '
+      case  RegexSingleExp(op, s) =>
+        val path = StringUtils.substringBeforeLast(s, "[").split('.').toList
+        def getField(v: JValue, fieldPath: List[String]): JValue = (v, fieldPath) match {
+          case (_, Nil)  => v
+
+          case (JArray(arr), fieldName::tail) =>
+            val values = arr.map(_ \ fieldName)
+            val newArray = JArray(values)
+            getField(newArray, tail)
+
+          case (_, fieldName:: tail) =>
+            getField(v \ fieldName, tail)
+        }
+        val value: JValue = (s, getField(jValue, path)) match {
+          //convert Array result to no JArray type, e.g: "someValue": "data.foo.bar[0]"
+          case (RegexArrayIndex(_, i), v @JArray(arr)) =>
+            assume(arr.forall(it => it == JNothing || it == JNull || it.isInstanceOf[JArray]), s"the path has index: '$p', that means the result should be two-dimension Array, but the value is one-dimension Array: ${json.prettyRender(v)}")
+            val index = i.toInt
+            val jObj = arr.map(getIndexValue(_, index))
+            JArray(jObj)
+          // convert result to JArray type, e.g: "someValue": "data.foo.bar[]"
+          case (RegexArray(_), v) =>
+            assume(v.isInstanceOf[JArray], s"the path marked as Array: '$p', that means the result should be Array, but the value is ${json.prettyRender(v)}")
+            val newArray = v.asInstanceOf[JArray].arr.map(it => JArray(it :: Nil))
+            JArray(newArray)
+          case (_, v) => v
+        }
+
+        op match {
+          case "!" => !value
+          case "-" => -value
+          case _ => value
+        }
+    }
+
+  }
+
+  /**
+   * Get given index value from path value, get direct index value if it JArray, escape IndexOutOfBoundsException
+   * @param jValue
+   * @param index
+   * @return JNothing or array element of given index
+   */
+  private def getIndexValue(jValue: JValue, index: Int): JValue = jValue match {
+    case JArray(arr) => arr.lift(index)
+    case _ => JNothing
+  }
+
+  def allValeIsSameSizeArray(jFields: List[JField]): Boolean = {
+    val firstFieldSize = jFields.headOption.collect {
+      case JField(_, JArray(arr)) => arr.size
+    }
+
+    firstFieldSize.exists( arraySize => jFields.tail.forall {
+      case JField(_, JArray(arr)) => arr.size == arraySize
+      case _ => false
+    })
+  }
+
+  def mapField(jValue: JValue)(f: (JField, String) => JField): JValue = {
+    def buildPath(parentPath: String, currentFieldName: String): String =
+      if(parentPath == "") currentFieldName else s"$parentPath.$currentFieldName"
+
+    def rec(v: JValue, path: String): JValue = v match {
+      case JObject(l) => JObject(l.map { field =>
+        f(field.copy(value = rec(field.value, buildPath(path, field.name))), path)
+      }
+      )
+      case JArray(l) => JArray(l.map(rec(_, path)))
+      case x => x
+    }
+    rec(jValue, "")
+  }
+
+  def transformField(jValue: JValue)(f: PartialFunction[(JField, String), JField]): JValue = mapField(jValue) { (field, path) =>
+    if (f.isDefinedAt(field, path)) f(field, path) else field
+  }
+  implicit class EnhancedJValue(jValue: JValue) {
+    def unary_- : JValue = jValue match{
+      case JDouble(num) => JDouble(-num)
+      case JInt(num) => JInt(-num)
+      case JArray(arr) => JArray(arr.map(-_))
+      case n @(JNothing | JNull) => n
+      case _ => throw new IllegalArgumentException(s"$jValue is not number or Array[number] type, not support - operation")
+    }
+    def unary_! : JValue = jValue match{
+      case JBool(value) => JBool(!value)
+      case JArray(arr) => JArray(arr.map(!_))
+      case n @(JNothing | JNull) => n
+      case _ => throw new IllegalArgumentException(s"$jValue is not boolean or Array[boolean] type, not support ! operation")
+    }
+
+    def &(v: JValue): JValue = (jValue, v) match {
+      case ((JNothing | JNull), JBool(_)) => JBool(false)
+      case (JBool(_), (JNothing | JNull)) => JBool(false)
+      case (JBool(v1), JBool(v2)) => JBool(v1 & v2)
+      case (JArray(v1), JArray(v2)) =>
+        val indexes = (0 until Math.max(v1.size, v2.size)).toList
+        val newArray = indexes map {index => getIndexValue(jValue, index) & getIndexValue(v, index) }
+        JArray(newArray)
+      case (JArray(v1), _: JBool) =>
+        val newArray = v1 map {_ & v }
+        JArray(newArray)
+      case (_: JBool, JArray(v1)) =>
+        val newArray = v1 map {jValue & _}
+        JArray(newArray)
+      case _ => throw new IllegalArgumentException(s"operand of operation & must be two boolean or Array[boolean] value, but current values: $jValue , $v")
+    }
+
+    def |(v: JValue): JValue = (jValue, v) match {
+      case ((JNothing | JNull), b :JBool) => b
+      case (b: JBool, (JNothing | JNull)) => b
+      case (JBool(v1), JBool(v2)) => JBool(v1 || v2)
+      case (JArray(v1), JArray(v2)) =>
+        val indexes = (0 until Math.max(v1.size, v2.size)).toList
+        val newArray = indexes map {index => getIndexValue(jValue, index) | getIndexValue(v, index) }
+        JArray(newArray)
+      case (JArray(v1), _: JBool) =>
+        val newArray = v1 map {_ | v }
+        JArray(newArray)
+      case (_: JBool, JArray(v1)) =>
+        val newArray = v1 map {jValue | _}
+        JArray(newArray)
+      case _ => throw new IllegalArgumentException(s"operand of operation | must be two boolean or Array[boolean] value, but current values: $jValue , $v")
+    }
+
+    def ~(v: JValue): JValue = (jValue, v) match {
+      case ((JNothing | JNull), _: JObject) => v
+      case (_: JObject, (JNothing | JNull)) => jValue
+      case (v1: JObject, v2: JObject) => v1 ~ v2
+      case (JArray(v1), JArray(v2)) =>
+        val indexes = (0 until Math.max(v1.size, v2.size)).toList
+        val newArray = indexes map {index => getIndexValue(jValue, index) ~ getIndexValue(v, index) }
+        JArray(newArray)
+      case (JArray(v1), _: JObject) =>
+        val newArray = v1 map { _ ~ v }
+        JArray(newArray)
+      case (_: JObject, JArray(v1)) =>
+        val newArray = v1 map {jValue ~ _}
+        JArray(newArray)
+      case _ => throw new IllegalArgumentException(s"operand of operation ~ must be two object or Array[object] value, but current values: $jValue , $v")
+    }
+
+    def +(v: JValue): JValue = (jValue, v) match {
+      case ((JNothing | JNull), _) => v
+      case (_, (JNothing | JNull)) => jValue
+      case (JDouble(v1), JDouble(v2)) => JDouble((BigDecimal(v1) + BigDecimal(v2)).toDouble)
+      case (JInt(v1), JInt(v2)) => JInt(v1 + v2)
+      case (JDouble(v1), JInt(v2)) => JDouble((BigDecimal(v1) + BigDecimal(v2)).toDouble)
+      case (JInt(v1), JDouble(v2)) => JDouble((BigDecimal(v1) + BigDecimal(v2)).toDouble)
+      case (JString(v1), JString(v2)) => JString(v1 + v2)
+      case (JArray(v1), JArray(v2)) =>
+        val indexes = (0 until Math.max(v1.size, v2.size)).toList
+        val newArray = indexes map {index => getIndexValue(jValue, index) + getIndexValue(v, index) }
+        JArray(newArray)
+      case (JArray(v1), (_:JInt | _: JDouble | _: JString)) =>
+        val newArray = v1 map {_ + v }
+        JArray(newArray)
+      case ((_:JInt | _: JDouble | _: JString), JArray(v1)) =>
+        val newArray = v1 map {jValue + _}
+        JArray(newArray)
+      case _ => throw new IllegalArgumentException(s"operand of operation + must be String, number, Array[String] or Array[number], but current values: $jValue , $v")
+    }
+
+    def -(v: JValue): JValue = (jValue, v) match {
+      case (JDouble(v1), JDouble(v2)) => JDouble((BigDecimal(v1) - BigDecimal(v2)).toDouble)
+      case (JInt(v1), JInt(v2)) => JInt(v1 - v2)
+      case (JDouble(v1), JInt(v2)) => JDouble((BigDecimal(v1) - BigDecimal(v2)).toDouble)
+      case (JInt(v1), JDouble(v2)) => JDouble((BigDecimal(v1) - BigDecimal(v2)).toDouble)
+      case (JArray(v1), JArray(v2)) =>
+        val indexes = (0 until Math.max(v1.size, v2.size)).toList
+        val newArray = indexes map {index => getIndexValue(jValue, index) - getIndexValue(v, index) }
+        JArray(newArray)
+      case (JArray(v1), (_:JInt| _: JDouble)) =>
+        val newArray = v1 map {_ - v }
+        JArray(newArray)
+      case ((_:JInt| _: JDouble), JArray(v1)) =>
+        val newArray = v1 map {jValue - _}
+        JArray(newArray)
+      case _ => throw new IllegalArgumentException(s"operand of operation - must be number or Array[number], but current values: $jValue , $v")
+    }
+
+    def *(v: JValue): JValue = (jValue, v) match {
+      case (JDouble(v1), JDouble(v2)) => JDouble((BigDecimal(v1) * BigDecimal(v2)).toDouble)
+      case (JInt(v1), JInt(v2)) => JInt(v1 * v2)
+      case (JDouble(v1), JInt(v2)) => JDouble((BigDecimal(v1) * BigDecimal(v2)).toDouble)
+      case (JInt(v1), JDouble(v2)) => JDouble((BigDecimal(v1) * BigDecimal(v2)).toDouble)
+      case (JArray(v1), JArray(v2)) =>
+        val indexes = (0 until Math.max(v1.size, v2.size)).toList
+        val newArray = indexes map {index => getIndexValue(jValue, index) * getIndexValue(v, index) }
+        JArray(newArray)
+      case (JArray(v1), (_:JInt| _: JDouble)) =>
+        val newArray = v1 map {_ * v }
+        JArray(newArray)
+      case ((_:JInt| _: JDouble), JArray(v1)) =>
+        val newArray = v1 map {jValue * _}
+        JArray(newArray)
+      case _ => throw new IllegalArgumentException(s"operand of operation * must be number or Array[number], but current values: $jValue , $v")
+    }
+
+    def /(v: JValue): JValue = (jValue, v) match {
+      case (JDouble(v1), JDouble(v2)) => JDouble((BigDecimal(v1) / BigDecimal(v2)).toDouble)
+      case (JInt(v1), JInt(v2)) => JInt(v1 / v2)
+      case (JDouble(v1), JInt(v2)) => JDouble((BigDecimal(v1) / BigDecimal(v2)).toDouble)
+      case (JInt(v1), JDouble(v2)) => JDouble((BigDecimal(v1) / BigDecimal(v2)).toDouble)
+      case (JArray(v1), JArray(v2)) =>
+        val indexes = (0 until Math.max(v1.size, v2.size)).toList
+        val newArray = indexes map {index => getIndexValue(jValue, index) / getIndexValue(v, index) }
+        JArray(newArray)
+      case (JArray(v1), (_:JInt| _: JDouble)) =>
+        val newArray = v1 map {_ / v }
+        JArray(newArray)
+      case ((_:JInt| _: JDouble), JArray(v1)) =>
+        val newArray = v1 map {jValue / _}
+        JArray(newArray)
+      case _ => throw new IllegalArgumentException(s"operand of operation / must be number or Array[number], but current values: $jValue , $v")
+    }
+  }
+
+  private def calculateValue(jValue: JValue, path: String): JValue = {
+    path match {
+      case RegexMultipleExp(a, b, c) => {
+        val v1 = JsonUtils.getValueByPath(jValue, a)
+        val op: JValue => JValue = b match {
+          case "+" => v1 +
+          case "-" => v1 -
+          case "*" => v1 *
+          case "/" => v1 /
+          case "~" => v1 ~
+          case "&" => v1 &
+          case "|" => v1 |
+        }
+        op(calculateValue(jValue, c))
+      }
+
+      case RegexSingleExp(_, _) => getValueByPath(jValue, path)
+    }
+  }
+}
---
+++ obp-api/src/test/scala/code/util/JsonUtilsTest.scala
@@ -0,0 +1,130 @@
+package code.util
+
+import org.scalatest.{FlatSpec, Matchers, Tag}
+import JsonUtils.convertJson
+import net.liftweb.json
+
+class JsonUtilsTest extends FlatSpec with Matchers {
+  object JsonUtilsTag extends Tag("JsonUtils")
+
+  val zson = json.parse(
+    """
+      |{
+      |  "banks":[{
+      |    "id":"dmo.01.uk.uk",
+      |    "short_name":"uk",
+      |    "full_name":"uk",
+      |    "is_deleted": true,
+      |    "is_new": true,
+      |    "score": 10,
+      |    "logo":"http://www.example.com",
+      |    "website":"http://www.example.com",
+      |    "bank_routings":[{
+      |      "scheme":"OBP",
+      |      "address":"dmo.01.uk.uk"
+      |    }]
+      |  },{
+      |    "id":"dmo.02.uk.uk",
+      |    "short_name":"uk",
+      |    "full_name":"uk",
+      |    "is_deleted": false,
+      |    "is_new": true,
+      |    "score": 5.3,
+      |    "logo":"https://static.openbankproject.com/images/sandbox/bank_y.png",
+      |    "website":"http://www.example.com"
+      |  },{
+      |    "id":"dmo.02.de.de",
+      |    "short_name":"de",
+      |    "full_name":"de",
+      |    "is_deleted": false,
+      |    "is_new": false,
+      |    "score": 2,
+      |    "logo":"https://static.openbankproject.com/images/sandbox/bank_z.png",
+      |    "website":"http://www.example.com",
+      |    "bank_routings":[{
+      |      "scheme":"OBP",
+      |      "address":"dmo.02.de.de"
+      |    }]
+      |  }]
+      |}
+      |""".stripMargin)
+
+  val schema = json.parse(
+    """
+      |{
+      | "code": 200,
+      | "meta$default": {
+      |   "count": 10,
+      |   "classInfo": {
+      |       "someInfo[]": "hello"
+      |   }
+      | }
+      | "result[]": {
+      |   "bkId": "banks.id",
+      |   "bkName": "banks.short_name + ' : ' + banks.full_name",
+      |   "is_exists": "!banks.is_deleted",
+      |   "newBank": "!banks.is_deleted & banks.is_new",
+      |   "routing": "banks.bank_routings[0]"
+      | },
+      | "secondBank[1]": {
+      |   "id": "banks.id",
+      |   "name": "banks.short_name",
+      |   "count": "banks.score * 'int: 2'"
+      | }
+      |}
+      |""".stripMargin)
+
+  val expectedJson = json.parse(
+    """
+      |{
+      |  "code":200,
+      |  "meta":{
+      |    "count":10,
+      |    "classInfo":{
+      |      "someInfo[]":"hello"
+      |    }
+      |  },
+      |  "result":[
+      |    {
+      |      "bkId":"dmo.01.uk.uk",
+      |      "bkName":"uk : uk",
+      |      "is_exists":false,
+      |      "newBank":false,
+      |      "routing":{
+      |        "scheme":"OBP",
+      |        "address":"dmo.01.uk.uk"
+      |      }
+      |    },
+      |    {
+      |      "bkId":"dmo.02.uk.uk",
+      |      "bkName":"uk : uk",
+      |      "is_exists":true,
+      |      "newBank":true
+      |    },
+      |    {
+      |      "bkId":"dmo.02.de.de",
+      |      "bkName":"de : de",
+      |      "is_exists":true,
+      |      "newBank":false,
+      |      "routing":{
+      |        "scheme":"OBP",
+      |        "address":"dmo.02.de.de"
+      |      }
+      |    }
+      |  ],
+      |  "secondBank": {
+      |      "id":"dmo.02.uk.uk",
+      |      "name":"uk",
+      |      "count":10.6
+      |    }
+      |}
+      |""".stripMargin)
+  "transformField" should "generate JValue according schema" taggedAs JsonUtilsTag in {
+    val resultJson = convertJson(zson, schema)
+
+    val str1 = json.prettyRender(resultJson)
+    val str2 = json.prettyRender(expectedJson)
+    str1 shouldEqual str2
+    println(str1)
+  }
+}
