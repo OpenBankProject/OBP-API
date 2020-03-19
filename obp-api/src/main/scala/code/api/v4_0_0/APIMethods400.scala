bd6d6fd06719a34e485e33a969c043937181c634
tawoe <tobias.woelk@tesobe.com>
2020-03-19 12:40:49 +0100

    fix typo in resource doc for updating Customer Attribute

--- obp-api/src/main/scala/code/api/v4_0_0/APIMethods400.scala
+++ obp-api/src/main/scala/code/api/v4_0_0/APIMethods400.scala
@@ -2140,7 +2140,7 @@ trait APIMethods400 {
       implementedInApiVersion,
       nameOf(updateCustomerAttribute),
       "PUT",
-      "/banks/BANK_ID/customers/CUSTOMER_ID/attributes/ACCOUNT_ATTRIBUTE_ID",
+      "/banks/BANK_ID/customers/CUSTOMER_ID/attributes/CUSTOMER_ATTRIBUTE_ID",
       "Update Customer Attribute",
       s""" Update Customer Attribute
          |
