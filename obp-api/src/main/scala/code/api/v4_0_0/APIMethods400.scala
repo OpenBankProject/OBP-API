d63bb6ac324034c49e599d30b527ef2db9bad9e9
hongwei <hongwei@tesobe.com>
2020-03-30 13:45:51 +0200

    added space for createDynamicEndpoint

--- obp-api/src/main/scala/code/api/v4_0_0/APIMethods400.scala
+++ obp-api/src/main/scala/code/api/v4_0_0/APIMethods400.scala
@@ -2778,8 +2778,8 @@ trait APIMethods400 {
       nameOf(createDynamicEndpoint),
       "POST",
       "/management/dynamic-endpoints",
-      " Create DynamicEndpoint",
-      s"""Create a DynamicEndpoint.
+      " Create Dynamic Endpoint",
+      s"""Create a Dynamic Endpoint.
          |
          |Create one DynamicEndpoint,
          |
@@ -2828,8 +2828,8 @@ trait APIMethods400 {
       nameOf(getDynamicEndpoint),
       "GET",
       "/management/dynamic-endpoints/DYNAMIC_ENDPOINT_ID",
-      " Get DynamicEndpoint",
-      s"""Get a DynamicEndpoint.
+      " Get Dynamic Endpoint",
+      s"""Get a Dynamic Endpoint.
          |
          |
          |Get one DynamicEndpoint,
@@ -2867,10 +2867,10 @@ trait APIMethods400 {
       nameOf(getDynamicEndpoints),
       "GET",
       "/management/dynamic-endpoints",
-      " Get DynamicEndpoints",
+      " Get Dynamic Endpoints",
       s"""
          |
-         |Get DynamicEndpoints.
+         |Get Dynamic Endpoints.
          |
          |""",
       emptyObjectJson,
