8435a8634844f359fcea2bf030b4c15421dd7a43
hongwei <hongwei@tesobe.com>
2020-03-30 07:09:44 +0200

    removed personal / company details in the example swagger

--- obp-api/src/main/scala/code/api/util/ExampleValue.scala
+++ obp-api/src/main/scala/code/api/util/ExampleValue.scala
@@ -367,11 +367,11 @@ object ExampleValue {
       |  "swagger": "2.0",
       |  "info": {
       |    "version": "0.0.1",
-      |    "title": "Portus EVS sandbox demo API",
-      |    "description": "Portus EVS sandbox demo API",
+      |    "title": "Example Title",
+      |    "description": "Example Description",
       |    "contact": {
-      |      "name": "Digital & FinTech, Grant Thornton",
-      |      "email": "peng.xu@ie.gt.com",
+      |      "name": "Example Company",
+      |      "email": " simon@example.com",
       |      "url": "https://www.tesobe.com/"
       |    }
       |  },
