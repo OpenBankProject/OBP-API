f22174f7b3be50cf2bf67405b412ff629bc8e4c4
Marko Milić <marko@tesobe.com>
2020-05-04 12:11:38 +0200

    bugfix/OpenID Connect - use aud instead of azp as Client ID

--- obp-api/src/main/scala/code/api/util/JwtUtil.scala
+++ obp-api/src/main/scala/code/api/util/JwtUtil.scala
@@ -184,10 +184,9 @@ object JwtUtil extends MdcLoggable {
 
     // The required parameters
     val iss: Issuer = new Issuer(getIssuer(idToken).getOrElse(""))
-    val azp = getClaim("azp", idToken)
-    val clientID: ClientID = new ClientID(azp)
+    val aud = getAudience(idToken).headOption.getOrElse("")
+    val clientID: ClientID = new ClientID(aud)
     val jwsAlg: JWSAlgorithm = JWSAlgorithm.RS256
-    //val jwkSetURL: URL = new URL("https://www.googleapis.com/oauth2/v3/certs")
     val jwkSetURL: URL = new URL(remoteJWKSetUrl)
 
     // Create validator for signed ID tokens
