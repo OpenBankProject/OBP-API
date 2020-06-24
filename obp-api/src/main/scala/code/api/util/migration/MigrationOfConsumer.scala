13284d2991e15f93aead454b1b5bced1228598fe
Marko Milić <marko@tesobe.com>
2020-06-24 12:51:56 +0200

    feature/Add migartion script populateAzpAndSub

--- obp-api/src/main/scala/code/api/util/migration/MigrationOfConsumer.scala
+++ obp-api/src/main/scala/code/api/util/migration/MigrationOfConsumer.scala
@@ -60,5 +60,51 @@ object MigrationOfConsumer {
         saveLog(name, commitId, isSuccessful, startDate, endDate, comment)
         isSuccessful
     }
+  }  
+  def populateAzpAndSub(name: String): Boolean = {
+    DbFunction.tableExists(Consumer, (DB.use(DefaultConnectionIdentifier){ conn => conn})) match {
+      case true =>
+        val startDate = System.currentTimeMillis()
+        val commitId: String = APIUtil.gitCommit
+        var isSuccessful = false
+
+        val emptyNameConsumers = 
+          for {
+            consumer <- Consumer.findAll() if consumer.azp.equals(null)
+          } yield {
+            consumer
+              .azp(APIUtil.generateUUID())
+              .saveMe()
+          }
+
+        val emptyAppTypeConsumers =
+          for {
+            consumer <- Consumer.findAll() if consumer.sub.equals(null)
+          } yield {
+            consumer
+              .sub(APIUtil.generateUUID())
+              .saveMe()
+          }
+        
+        val consumersAll = (emptyNameConsumers++emptyAppTypeConsumers).distinct
+        val endDate = System.currentTimeMillis()
+        val comment: String =
+          s"""Updated number of rows: 
+             |${consumersAll.size}
+             |""".stripMargin
+        isSuccessful = true
+        saveLog(name, commitId, isSuccessful, startDate, endDate, comment)
+        isSuccessful
+
+      case false =>
+        val startDate = System.currentTimeMillis()
+        val commitId: String = APIUtil.gitCommit
+        val isSuccessful = false
+        val endDate = System.currentTimeMillis()
+        val comment: String =
+          s"""${Consumer._dbTableNameLC} table does not exist""".stripMargin
+        saveLog(name, commitId, isSuccessful, startDate, endDate, comment)
+        isSuccessful
+    }
   }
 }
