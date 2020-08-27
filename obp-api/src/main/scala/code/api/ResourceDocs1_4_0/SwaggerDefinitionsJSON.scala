be3654a0fdc37d51ce44c98f352f55fbe2a83cd7
shuang <shuang@tesobe.com>
2020-08-27 15:32:07 +0800

    feature/remove_account_attributes_from_Get_Account_by_Id_(Core): remove account_attributes field from endpoint 'Get Account by Id (Core)', the version is v3.0.0

--- obp-api/src/main/scala/code/api/ResourceDocs1_4_0/SwaggerDefinitionsJSON.scala
+++ obp-api/src/main/scala/code/api/ResourceDocs1_4_0/SwaggerDefinitionsJSON.scala
@@ -3486,8 +3486,7 @@ object SwaggerDefinitionsJSON {
     `type`= accountTypeExample.value,
     balance = amountOfMoneyJsonV121,
     account_routings = List(accountRoutingJsonV121),
-    views_basic = List(viewBasicV300),
-    account_attributes = List(accountAttributeResponseJson)
+    views_basic = List(viewBasicV300)
   )  
   val moderatedCoreAccountJsonV400 = ModeratedCoreAccountJsonV400(
     id = accountIdExample.value,
