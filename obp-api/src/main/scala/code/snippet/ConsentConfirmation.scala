92313ce687f8da1b22cebdbd96b6cfbb8893336c
shuang <shuang@tesobe.com>
2020-09-03 13:03:40 +0800

    feature/login_with_hydra: change confirm consent page and logic, not call save consent function yet.

--- obp-api/src/main/scala/code/snippet/ConsentConfirmation.scala
+++ obp-api/src/main/scala/code/snippet/ConsentConfirmation.scala
@@ -26,16 +26,22 @@
  */
 package code.snippet
 
+import code.api.util.NewStyle
 import code.model.dataAccess.AuthUser
 import code.util.Helper.MdcLoggable
+import code.views.Views
 import code.webuiprops.MappedWebUiPropsProvider.getWebUiPropsValue
 import net.liftweb.http.{RequestVar, S, SHtml}
 import net.liftweb.util.CssSel
 import net.liftweb.util.Helpers._
 import sh.ory.hydra.model.{AcceptConsentRequest, ConsentRequestSession, RejectRequest}
 
-import scala.jdk.CollectionConverters.{asScalaBufferConverter, seqAsJavaListConverter}
+import scala.jdk.CollectionConverters.{asScalaBufferConverter, mapAsJavaMapConverter, seqAsJavaListConverter}
+import com.openbankproject.commons.ExecutionContext.Implicits.global
+import com.openbankproject.commons.util.Functions.Implicits._
 
+import scala.concurrent.{Await, Future}
+import scala.concurrent.duration.{Duration, SECONDS}
 
 class ConsentConfirmation extends MdcLoggable {
 
@@ -47,7 +53,7 @@ class ConsentConfirmation extends MdcLoggable {
   val confirmConsentButtonValue = getWebUiPropsValue("webui_post_confirm_consent_submit_button_value", "Yes, I confirm")
   val rejectConsentButtonValue = getWebUiPropsValue("webui_post_reject_consent_submit_button_value", "Cancel")
 
-  def registerForm: CssSel = {
+  def confirmConsentsForm: CssSel = {
 
     def submitButtonDefense: Unit = {
       submitButtonDefenseFlag("true")
@@ -91,7 +97,24 @@ class ConsentConfirmation extends MdcLoggable {
       consentRequest.setGrantAccessTokenAudience(consentResponse.getRequestedAccessTokenAudience)
       consentRequest.setRemember(false)
       consentRequest.setRememberFor(3600) // TODO set in props
-      consentRequest.setSession(new ConsentRequestSession())
+      val bankId = S.param("bank_id").orNull
+      val accountIds: List[String] = S.params("account_id")
+      val fromDate = S.param("from_date").orNull
+      val toDate = S.param("to_date").orNull
+      val expirationDate = S.param("expiration_date").orNull
+      // TODO create consent
+      val currentUser = AuthUser.getCurrentUser.openOrThrowException("User is not login, do confirm consent must be authenticated user.")
+
+      val session = new ConsentRequestSession()
+      val userName = currentUser.name
+      val idTokenValues = Map("given_name" -> userName,
+        "family_name" -> userName,
+        "name" -> userName,
+        "email" -> currentUser.emailAddress,
+        "email_verified" -> true).asJava
+
+      session.setIdToken(idTokenValues)
+      consentRequest.setSession(session)
 
       val acceptConsentResponse = AuthUser.hydraAdmin.acceptConsentRequest(consentChallenge, consentRequest)
       S.redirectTo(acceptConsentResponse.getRedirectTo)
@@ -105,12 +128,47 @@ class ConsentConfirmation extends MdcLoggable {
         val skipResponse = AuthUser.hydraAdmin.acceptConsentRequest(consentChallenge, requestBody)
         S.redirectTo(skipResponse.getRedirectTo)
       } else {
+        val currentUser = AuthUser.getCurrentUser.openOrThrowException("User is not login, do confirm consent must be authenticated user.")
+
+        val bankAndAccountFuture: Future[List[(String, String, String, String)]] = for {
+          availablePrivateAccounts <- Views.views.vend.getPrivateBankAccountsFuture(currentUser)
+          (accounts, _) <- NewStyle.function.getCoreBankAccountsFuture(availablePrivateAccounts, None)
+          (banks, _) <- NewStyle.function.getBanks(None)
+        } yield {
+          for {
+            bank <- banks
+            account <- accounts
+            if account.bankId == bank.bankId.value
+          } yield (bank.bankId.value, bank.shortName, account.id, account.label)
+        }
+        //(bankId, bankName, accountId, accountLabel)
+        val bankAndAccount: List[(String, String, String, String)] = Await.result(bankAndAccountFuture, Duration(30, SECONDS))
+
+        val banks = bankAndAccount.map(it => it._1 -> it._2).distinctBy(_._1)
+
         formElement {
           "#confirm-errors" #> "" &
             "#consent_challenge [value]" #> consentChallenge &
-            "#scope_group" #> consentResponse.getRequestedScope.asScala.map { scope =>
+            ".bank" #> {
+              banks.map { it =>
+                  ".bank [value]" #> it._1 &
+                  ".bank *" #> it._2
+                }
+            } &
+            "#account_group" #> {
+              bankAndAccount.map { account =>
+                val (bankId, _, accountId, label) = account
+                "@account_id [value]" #> accountId &
+                  "@account_id [id]" #> s"account_$accountId" &
+                  "@account_id [bank_id]" #> bankId &
+                  "@account_id_label [for]" #> s"account_$accountId" &
+                  "@account_id_label *" #> label
+              }
+            } &
+            "#scope_group" #> consentResponse.getRequestedScope.asScala.filter(it => it != "openid" && it != "offline").map { scope =>
               "@consent_scope [value]" #> scope &
-                "@consent_scope_label [for]" #> scope &
+              "@consent_scope [id]" #> s"consent_$scope" &
+                "@consent_scope_label [for]" #> s"consent_$scope" &
                 "@consent_scope_label *" #> scope
             }
         }
