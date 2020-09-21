037716e893d37faa94fedf0643ec7170e280a9d5
hongwei <hongwei@tesobe.com>
2020-09-21 12:48:17 +0200

    feature/added the guard for the introduction page

--- obp-api/src/main/scala/code/api/util/APIUtil.scala
+++ obp-api/src/main/scala/code/api/util/APIUtil.scala
@@ -3434,4 +3434,6 @@ object APIUtil extends MdcLoggable with CustomJsonFormats{
       connectorToEndpoint.put(method, newInfos)
     })
   }
+
+  val glossaryDocsRequireRole = APIUtil.getPropsAsBoolValue("glossary_requires_role", false)
 }
