9e5974d32eb08a842c2733a45224c904703704f9
Marko Milić <marko@tesobe.com>
2020-11-12 16:16:42 +0100

    feature/Tweak Payment Cancellation Request of Berlin Group

--- obp-api/src/main/scala/code/api/berlin/group/v1_3/PaymentInitiationServicePISApi.scala
+++ obp-api/src/main/scala/code/api/berlin/group/v1_3/PaymentInitiationServicePISApi.scala
@@ -104,7 +104,8 @@ or * access method is generally applicable, but further authorisation processes 
              fromAccountIban = transactionRequestBody.debtorAccount.iban
              toAccountIban = transactionRequestBody.creditorAccount.iban
              (fromAccount, callContext) <- NewStyle.function.getBankAccountByIban(fromAccountIban, callContext)
-             (toAccount, callContext) <- NewStyle.function.getBankAccountByIban(toAccountIban, callContext)
+             (_, callContext) <- NewStyle.function.validateAndCheckIbanNumber(toAccountIban, callContext)
+             (toAccount, callContext) <- NewStyle.function.getToBankAccountByIban(toAccountIban, callContext)
              negativeAmount = - transactionRequest.body.value.amount.toDouble
              currency = transactionRequest.body.value.currency
              (createdTransactionRequest,callContext) <- transactionRequestTypes match {
