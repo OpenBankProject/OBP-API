913e976e495a92180d1a627d3d36f0205b63f48e
hongwei <hongwei@tesobe.com>
2021-02-03 13:19:43 +0100

    feature/OBP V400 added the DynamicMessageDoc model

---
+++ obp-api/src/main/scala/code/dynamicMessageDoc/DynamicMessageDoc.scala
@@ -0,0 +1,40 @@
+package code.dynamicMessageDoc
+
+import code.util.UUIDString
+import net.liftweb.mapper._
+import scala.collection.immutable.List
+
+class DynamicMessageDoc extends LongKeyedMapper[DynamicMessageDoc] with IdPK {
+
+  override def getSingleton = DynamicMessageDoc
+
+  object DynamicMessageDocId extends UUIDString(this)     
+  object Process extends MappedString(this, 255)   
+  object MessageFormat extends MappedString(this, 255)   
+  object Description extends MappedString(this, 255)   
+  object OutboundTopic extends MappedString(this, 255)   
+  object InboundTopic extends MappedString(this, 255)   
+  object ExampleOutboundMessage extends MappedString(this, 255)   
+  object ExampleInboundMessage extends MappedString(this, 255)   
+  object OutboundAvroSchema extends MappedString(this, 255)   
+  object InboundAvroSchema extends MappedString(this, 255)   
+  object AdapterImplementation  extends MappedString(this, 255)   
+}
+
+
+object DynamicMessageDoc extends DynamicMessageDoc with LongKeyedMetaMapper[DynamicMessageDoc] {
+  override def dbIndexes: List[BaseIndex[DynamicMessageDoc]] = UniqueIndex(DynamicMessageDocId) :: super.dbIndexes
+  def getJsonDynamicMessageDoc(dynamicMessageDoc: DynamicMessageDoc) = JsonDynamicMessageDoc(
+    dynamicMessageDocId = Some(dynamicMessageDoc.DynamicMessageDocId.get),
+    process = dynamicMessageDoc.Process.get,
+    messageFormat = dynamicMessageDoc.MessageFormat.get,
+    description = dynamicMessageDoc.Description.get,
+    outboundTopic = dynamicMessageDoc.OutboundTopic.get,
+    inboundTopic = dynamicMessageDoc.InboundTopic.get,
+    exampleOutboundMessage = dynamicMessageDoc.ExampleOutboundMessage.get,
+    exampleInboundMessage = dynamicMessageDoc.ExampleInboundMessage.get,
+    outboundAvroSchema = dynamicMessageDoc.OutboundAvroSchema.get,
+    inboundAvroSchema = dynamicMessageDoc.InboundAvroSchema.get,
+    adapterImplementation = dynamicMessageDoc.AdapterImplementation.get
+  )
+}
---
+++ obp-api/src/main/scala/code/dynamicMessageDoc/DynamicMessageDocProvider.scala
@@ -0,0 +1,39 @@
+package code.dynamicMessageDoc
+
+import com.openbankproject.commons.model.JsonFieldReName
+import net.liftweb.common.Box
+import net.liftweb.util.SimpleInjector
+
+import scala.collection.immutable.List
+
+object DynamicMessageDocProvider extends SimpleInjector {
+
+  val provider = new Inject(buildOne _) {}
+
+  def buildOne: MappedDynamicMessageDocProvider.type = MappedDynamicMessageDocProvider
+}
+
+case class JsonDynamicMessageDoc(
+  dynamicMessageDocId: Option[String],
+  process: String,
+  messageFormat: String, 
+  description: String, 
+  outboundTopic: String, 
+  inboundTopic: String, 
+  exampleOutboundMessage: String, 
+  exampleInboundMessage: String, 
+  outboundAvroSchema: String, 
+  inboundAvroSchema: String,
+  adapterImplementation: String
+) extends JsonFieldReName
+
+trait DynamicMessageDocProvider {
+
+  def getById(dynamicMessageDocId: String): Box[JsonDynamicMessageDoc]
+  def getAll(): List[JsonDynamicMessageDoc]
+
+  def create(entity: JsonDynamicMessageDoc): Box[JsonDynamicMessageDoc]
+  def update(entity: JsonDynamicMessageDoc): Box[JsonDynamicMessageDoc]
+  def deleteById(dynamicMessageDocId: String): Box[Boolean]
+
+}
