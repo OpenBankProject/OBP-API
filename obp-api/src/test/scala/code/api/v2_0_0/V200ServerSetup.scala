51df6826e6eef73255aa26595fad339c713e5d93
Marko Milić <marko@tesobe.com>
2021-05-20 16:22:35 +0200

    feature/Rewrite publicAccountsAllBanks v2.0.0 as New Style endpoint 2

--- obp-api/src/test/scala/code/api/v2_0_0/V200ServerSetup.scala
+++ obp-api/src/test/scala/code/api/v2_0_0/V200ServerSetup.scala
@@ -1,10 +1,15 @@
 package code.api.v2_0_0
 
-import code.setup.ServerSetupWithTestData
+import code.setup.{APIResponse, ServerSetupWithTestData}
 
 trait V200ServerSetup extends ServerSetupWithTestData {
 
   def v1_4Request = baseRequest / "obp" / "v1.4.0"
   def v2_0Request = baseRequest / "obp" / "v2.0.0"
+
+  def getPublicAccountsForAllBanks() : APIResponse= {
+    val request = v2_0Request / "accounts" / "public"
+    makeGetRequest(request)
+  }
 
 }
