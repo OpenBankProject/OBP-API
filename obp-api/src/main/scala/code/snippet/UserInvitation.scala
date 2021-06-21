0954aaa3db32ade7957d4a44b38ff5a182d75881
Marko Milić <marko@tesobe.com>
2021-06-21 17:25:34 +0200

    feature/Add terms and conditions for user invitation flow

--- obp-api/src/main/scala/code/snippet/UserInvitation.scala
+++ obp-api/src/main/scala/code/snippet/UserInvitation.scala
@@ -47,8 +47,13 @@ class UserInvitation extends MdcLoggable {
   private object countryVar extends RequestVar("None")
   private object devEmailVar extends RequestVar("")
   private object usernameVar extends RequestVar("")
+  private object termsCheckboxVar extends RequestVar(false)
+  private object privacyCheckboxVar extends RequestVar(false)
   
   val registrationConsumerButtonValue: String = getWebUiPropsValue("webui_post_user_invitation_submit_button_value", "Register as a Developer")
+  val privacyConditionsValue: String = getWebUiPropsValue("webui_post_user_invitation_privacy_conditions_value", "Privacy conditions")
+  val termsAndConditionsValue: String = getWebUiPropsValue("webui_post_user_invitation_terms_and_conditions_value", "Terms and Conditions")
+  val termsAndConditionsCheckboxValue: String = getWebUiPropsValue("webui_post_user_invitation_terms_and_conditions_checkbox_value", "I agree to the above Developer Terms and Conditions")
   
   def registerForm: CssSel = {
 
@@ -65,6 +70,8 @@ class UserInvitation extends MdcLoggable {
     def submitButtonDefense(): Unit = {
       if(Users.users.vend.getUserByUserName(usernameVar.is).isDefined) showErrorsForUsername()
       else if(userInvitation.map(_.status != "CREATED").getOrElse(false)) showErrorsForStatus()
+      else if(privacyCheckboxVar.is == false) showErrorsForPrivacyConditions()
+      else if(termsCheckboxVar.is == false) showErrorsForTermsAndConditions()
       else {
         createResourceUser(
           provider = "OBP-User-Invitation",
@@ -93,9 +100,14 @@ class UserInvitation extends MdcLoggable {
     def showErrorsForUsername() = {
       showError(Helper.i18n("unique.username"))
     }
-
     def showErrorsForStatus() = {
       showError(Helper.i18n("user.invitation.is.already.finished"))
+    }
+    def showErrorsForTermsAndConditions() = {
+      showError(Helper.i18n("terms.and.conditions.are.not.selected"))
+    }
+    def showErrorsForPrivacyConditions() = {
+      showError(Helper.i18n("privacy.conditions.are.not.selected"))
     }
 
     def register = {
@@ -106,6 +118,10 @@ class UserInvitation extends MdcLoggable {
           "#companyName" #> SHtml.text(companyVar.is, companyVar(_)) &
           "#devEmail" #> SHtml.text(devEmailVar, devEmailVar(_)) &
           "#username" #> SHtml.text(usernameVar, usernameVar(_)) &
+          "#privacy" #> SHtml.textarea(privacyConditionsValue, privacyConditionsValue => privacyConditionsValue) &
+          "#privacy_checkbox" #> SHtml.checkbox(privacyCheckboxVar, privacyCheckboxVar(_)) &
+          "#terms" #> SHtml.textarea(termsAndConditionsValue, termsAndConditionsValue => termsAndConditionsValue) &
+          "#terms_checkbox" #> SHtml.checkbox(termsCheckboxVar, termsCheckboxVar(_)) &
           "type=submit" #> SHtml.submit(s"$registrationConsumerButtonValue", () => submitButtonDefense)
       } &
       "#register-consumer-success" #> ""
