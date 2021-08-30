d750bd895bea602e4db5e04b7e9e2aabb103dee9
Marko Milić <marko@tesobe.com>
2021-08-30 13:27:45 +0200

    feature/Stop OBP startup if database is not available in development mode

--- obp-api/src/main/scala/bootstrap/liftweb/Boot.scala
+++ obp-api/src/main/scala/bootstrap/liftweb/Boot.scala
@@ -270,14 +270,17 @@ class Boot extends MdcLoggable {
     }
     implicit val formats = CustomJsonFormats.formats 
     LiftRules.statelessDispatch.prepend {
-      case _ if tryo(DB.use(DefaultConnectionIdentifier){ conn => conn}.isClosed).isEmpty => 
-        () =>
-          Full(
-            JsonResponse(
-              Extraction.decompose(ErrorMessage(code = 500, message = s"${ErrorMessages.DatabaseConnectionClosedError}")),
-              500
-            )
-          )
+      case _ if tryo(DB.use(DefaultConnectionIdentifier){ conn => conn}.isClosed).isEmpty =>
+        Props.mode match {
+          case Props.RunModes.Development =>
+            () =>
+              Full(
+                JsonResponse(
+                  Extraction.decompose(ErrorMessage(code = 500, message = s"${ErrorMessages.DatabaseConnectionClosedError}")),
+                  500
+                )
+              )
+        }
     }
     
     logger.info("Mapper database info: " + Migration.DbFunction.mapperDatabaseInfo())
