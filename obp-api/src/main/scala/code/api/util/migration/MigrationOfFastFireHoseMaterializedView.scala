f270ff56997e7d14c25c6203bc21eee50de93a8d
hongwei <hongwei@tesobe.com>
2021-10-20 10:00:17 +0200

    refactor/removed the duplicate migration code

--- obp-api/src/main/scala/code/api/util/migration/MigrationOfFastFireHoseMaterializedView.scala
+++ obp-api/src/main/scala/code/api/util/migration/MigrationOfFastFireHoseMaterializedView.scala
@@ -23,117 +23,64 @@ object MigrationOfFastFireHoseMaterializedView {
         val commitId: String = APIUtil.gitCommit
         var isSuccessful = false
 
+        def migrationSql(isMaterializedView:Boolean) =s"""
+             |CREATE ${if(isMaterializedView) "MATERIALIZED" else ""} VIEW mv_fast_firehose_accounts AS select
+             |    mappedbankaccount.theaccountid as account_id,
+             |    mappedbankaccount.bank as bank_id,
+             |    mappedbankaccount.accountlabel as account_label,
+             |    mappedbankaccount.accountnumber as account_number,
+             |    (select
+             |        string_agg(
+             |            'user_id:'
+             |            || resourceuser.userid_
+             |            ||',provider:'
+             |            ||resourceuser.provider_
+             |            ||',user_name:'
+             |            ||resourceuser.name_,
+             |         ',') as owners
+             |     from resourceuser
+             |     where
+             |        resourceuser.id = mapperaccountholders.user_c
+             |    ),
+             |    mappedbankaccount.kind as kind,
+             |    mappedbankaccount.accountcurrency as account_currency ,
+             |    mappedbankaccount.accountbalance as account_balance,
+             |    (select 
+             |        string_agg(
+             |            'bank_id:'
+             |            ||bankaccountrouting.bankid 
+             |            ||',account_id:' 
+             |            ||bankaccountrouting.accountid,
+             |            ','
+             |            ) as account_routings
+             |        from bankaccountrouting
+             |        where 
+             |              bankaccountrouting.accountid = mappedbankaccount.theaccountid
+             |     ),                                                          
+             |    (select 
+             |        string_agg(
+             |                'type:'
+             |                || mappedaccountattribute.mtype
+             |                ||',code:'
+             |                ||mappedaccountattribute.mcode
+             |                ||',value:'
+             |                ||mappedaccountattribute.mvalue,
+             |            ',') as account_attributes
+             |    from mappedaccountattribute
+             |    where
+             |         mappedaccountattribute.maccountid = mappedbankaccount.theaccountid
+             |     )
+             |from mappedbankaccount
+             |         LEFT JOIN mapperaccountholders
+             |                   ON (mappedbankaccount.bank = mapperaccountholders.accountbankpermalink and mappedbankaccount.theaccountid = mapperaccountholders.accountpermalink);
+             |""".stripMargin
         val executedSql =
           DbFunction.maybeWrite(true, Schemifier.infoF _, DB.use(DefaultConnectionIdentifier){ conn => conn}) {
             APIUtil.getPropsValue("db.driver") openOr("org.h2.Driver") match {
               case value if value.contains("org.h2.Driver") =>
-                () => //Note: H2 database, do not support the MATERIALIZED view
-                  """
-                    |CREATE VIEW mv_fast_firehose_accounts AS select
-                    |    mappedbankaccount.theaccountid as account_id,
-                    |    mappedbankaccount.bank as bank_id,
-                    |    mappedbankaccount.accountlabel as account_label,
-                    |    mappedbankaccount.accountnumber as account_number,
-                    |    (select
-                    |        string_agg(
-                    |            'user_id:'
-                    |            || resourceuser.userid_
-                    |            ||',provider:'
-                    |            ||resourceuser.provider_
-                    |            ||',user_name:'
-                    |            ||resourceuser.name_,
-                    |         ',') as owners
-                    |     from resourceuser
-                    |     where
-                    |        resourceuser.id = mapperaccountholders.user_c
-                    |    ),
-                    |    mappedbankaccount.kind as kind,
-                    |    mappedbankaccount.accountcurrency as account_currency ,
-                    |    mappedbankaccount.accountbalance as account_balance,
-                    |    (select 
-                    |        string_agg(
-                    |            'bank_id:'
-                    |            ||bankaccountrouting.bankid 
-                    |            ||',account_id:' 
-                    |            ||bankaccountrouting.accountid,
-                    |            ','
-                    |            ) as account_routings
-                    |        from bankaccountrouting
-                    |        where 
-                    |              bankaccountrouting.accountid = mappedbankaccount.theaccountid
-                    |     ),                                                          
-                    |    (select 
-                    |        string_agg(
-                    |                'type:'
-                    |                || mappedaccountattribute.mtype
-                    |                ||',code:'
-                    |                ||mappedaccountattribute.mcode
-                    |                ||',value:'
-                    |                ||mappedaccountattribute.mvalue,
-                    |            ',') as account_attributes
-                    |    from mappedaccountattribute
-                    |    where
-                    |         mappedaccountattribute.maccountid = mappedbankaccount.theaccountid
-                    |     )
-                    |from mappedbankaccount
-                    |         LEFT JOIN mapperaccountholders
-                    |                   ON (mappedbankaccount.bank = mapperaccountholders.accountbankpermalink and mappedbankaccount.theaccountid = mapperaccountholders.accountpermalink);
-                    |""".stripMargin
+                () => migrationSql(false)//Note: H2 database, do not support the MATERIALIZED view
               case _ =>
-                () =>
-                  """
-                    |CREATE MATERIALIZED VIEW mv_fast_firehose_accounts AS select
-                    |    mappedbankaccount.theaccountid as account_id,
-                    |    mappedbankaccount.bank as bank_id,
-                    |    mappedbankaccount.accountlabel as account_label,
-                    |    mappedbankaccount.accountnumber as account_number,
-                    |    (select
-                    |        string_agg(
-                    |            'user_id:'
-                    |            || resourceuser.userid_
-                    |            ||',provider:'
-                    |            ||resourceuser.provider_
-                    |            ||',user_name:'
-                    |            ||resourceuser.name_,
-                    |         ',') as owners
-                    |     from resourceuser
-                    |     where
-                    |        resourceuser.id = mapperaccountholders.user_c
-                    |    ),
-                    |    mappedbankaccount.kind as kind,
-                    |    mappedbankaccount.accountcurrency as account_currency ,
-                    |    mappedbankaccount.accountbalance as account_balance,
-                    |    (select 
-                    |        string_agg(
-                    |            'bank_id:'
-                    |            ||bankaccountrouting.bankid 
-                    |            ||',account_id:' 
-                    |            ||bankaccountrouting.accountid,
-                    |            ','
-                    |            ) as account_routings
-                    |        from bankaccountrouting
-                    |        where 
-                    |              bankaccountrouting.accountid = mappedbankaccount.theaccountid
-                    |     ),                                                          
-                    |    (select 
-                    |        string_agg(
-                    |                'type:'
-                    |                || mappedaccountattribute.mtype
-                    |                ||',code:'
-                    |                ||mappedaccountattribute.mcode
-                    |                ||',value:'
-                    |                ||mappedaccountattribute.mvalue,
-                    |            ',') as account_attributes
-                    |    from mappedaccountattribute
-                    |    where
-                    |         mappedaccountattribute.maccountid = mappedbankaccount.theaccountid
-                    |     )
-                    |from mappedbankaccount
-                    |         LEFT JOIN mapperaccountholders
-                    |                   ON (mappedbankaccount.bank = mapperaccountholders.accountbankpermalink and mappedbankaccount.theaccountid = mapperaccountholders.accountpermalink);
-                    |CREATE INDEX account_id ON mv_fast_firehose_accounts(account_id);
-                    |CREATE INDEX bank_id ON mv_fast_firehose_accounts(bank_id);
-                    |""".stripMargin
+                () => migrationSql(true)
             }
           }
 
