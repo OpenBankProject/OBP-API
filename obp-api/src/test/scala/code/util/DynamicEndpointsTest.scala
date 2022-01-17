dd6b671ca5c4e33eaecb0ccdad51b0a51e53b831
hongwei <hongwei@tesobe.com>
2022-01-17 14:07:55 +0100

    test/added the test for DynamicEndpoints.CompiledObjects.validateDependency

---
+++ obp-api/src/test/scala/code/util/DynamicEndpointsTest.scala
@@ -0,0 +1,51 @@
+/**
+Open Bank Project - API
+Copyright (C) 2011-2022, TESOBE GmbH.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Email: contact@tesobe.com
+TESOBE GmbH.
+Osloer Strasse 16/17
+Berlin 13359, Germany
+
+This product includes software developed at
+TESOBE (http://www.tesobe.com/)
+
+  */
+
+package code.util
+
+import code.api.ResourceDocs1_4_0.SwaggerDefinitionsJSON
+import code.api.v4_0_0.V400ServerSetup
+import code.api.v4_0_0.dynamic.CompiledObjects
+import code.setup.PropsReset
+import org.scalatest.Tag
+
+
+
+class DynamicEndpointsTest extends V400ServerSetup  with PropsReset {
+  object DynamicUtilsTag extends Tag("DynamicEndpoints")
+
+  feature("test DynamicEndpoints.CompiledObjects.validateDependency method") {
+    //This mean, we are only disabled the v4.0.0, all other versions should be enabled
+    setPropsValues(
+      "dynamic_code_compile_validate_enable" -> "true"
+    )
+    
+    val jsonDynamicResourceDoc = SwaggerDefinitionsJSON.jsonDynamicResourceDoc
+    val compiledObject = CompiledObjects(jsonDynamicResourceDoc.exampleRequestBody, jsonDynamicResourceDoc.successResponseBody, jsonDynamicResourceDoc.methodBody)
+      .validateDependency()
+  }
+}
