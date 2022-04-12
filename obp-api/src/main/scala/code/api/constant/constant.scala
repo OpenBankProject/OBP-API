5a52de86b2bf9d77850c3a9a7a8ee21181a4d81b
hongwei <hongwei@tesobe.com>
2022-04-12 13:41:54 +0200

    feature/added the new system View `SmallPaymentVerified`

--- obp-api/src/main/scala/code/api/constant/constant.scala
+++ obp-api/src/main/scala/code/api/constant/constant.scala
@@ -26,6 +26,7 @@ object Constant extends MdcLoggable {
   final val SYSTEM_AUDITOR_VIEW_ID = "auditor"
   final val SYSTEM_ACCOUNTANT_VIEW_ID = "accountant"
   final val SYSTEM_FIREHOSE_VIEW_ID = "firehose"
+  final val SYSTEM_SMALL_PAYMENT_VERIFIED_VIEW_ID = "SmallPaymentVerified"
   final val SYSTEM_READ_ACCOUNTS_BASIC_VIEW_ID = "ReadAccountsBasic"
   final val SYSTEM_READ_ACCOUNTS_DETAIL_VIEW_ID = "ReadAccountsDetail"
   final val SYSTEM_READ_BALANCES_VIEW_ID = "ReadBalances"
