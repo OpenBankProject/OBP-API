ccd286f4d9d8ad65fa3bf1bf950e91cb6846ba60
hongwei <hongwei@tesobe.com>
2022-05-04 15:27:14 +0200

    docfix/added the comments for override val attemptCounter

--- obp-commons/src/main/scala/com/openbankproject/commons/model/CommonModel.scala
+++ obp-commons/src/main/scala/com/openbankproject/commons/model/CommonModel.scala
@@ -532,7 +532,7 @@ case class ChallengeCommons(
   override val scaMethod: Option[SCA],
   override val scaStatus: Option[SCAStatus],
   override val authenticationMethodId: Option[String] ,
-  override val attemptCounter: Int  = 0
+  override val attemptCounter: Int  = 0 //NOTE: set the default value here, so do not break current connectors
 ) extends ChallengeTrait
 object ChallengeCommons extends Converter[ChallengeTrait, ChallengeCommons]
 
