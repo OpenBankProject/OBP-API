33b5c918d88ec7cc0ce1223b668a88d9559777a6
Marko Milić <marko@tesobe.com>
2023-03-08 19:47:56 +0100

    feature/Tweaks to Portal API selections

--- obp-api/src/main/scala/code/snippet/WebUI.scala
+++ obp-api/src/main/scala/code/snippet/WebUI.scala
@@ -231,6 +231,18 @@ class WebUI extends MdcLoggable{
     }
     ".api-explorer-link a [href]" #> scala.xml.Unparsed(baseUrlWithQ)
   }
+  def dynamicEntityApiExplorerLink: CssSel = {
+    val tags = S.attr("tags") openOr ""
+    val locale = S.locale.toString
+    // Note the Props value might contain a query parameter e.g. ?psd2=true
+    val baseUrl = getWebUiPropsValue("webui_api_explorer_url", "")
+    // hack (we should use url operators instead) so we can add further query parameters if one is already included in the the baseUrl
+    val baseUrlWithQ =  baseUrl.contains("?") match {
+      case true => baseUrl +  s"&tags=$tags${brandString}&locale=${locale}#group-Dynamic-Entity-Manage" // ? found so add & instead
+      case false => baseUrl + s"?tags=$tags${brandString}&locale=${locale}#group-Dynamic-Entity-Manage" // ? not found so add it.
+    }
+    ".api-explorer-link-dynamic-entity a [href]" #> scala.xml.Unparsed(baseUrlWithQ)
+  }
   
   def wrapPropsUrlLocaleParameter (propsKey: String) = {
     val localeString = S.locale.toString
