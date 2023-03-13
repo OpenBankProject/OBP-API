41b1a00819e37e3681a622bb5fc84cc373c127fe
Marko Milić <marko@tesobe.com>
2023-03-13 15:09:57 +0100

    test/Add tests for endpoint accountAccessUniqueIndexCheck v5.1.0

--- obp-api/src/test/scala/code/api/v5_1_0/SystemIntegrityTest.scala
+++ obp-api/src/test/scala/code/api/v5_1_0/SystemIntegrityTest.scala
@@ -21,6 +21,7 @@ class SystemIntegrityTest extends V510ServerSetup {
   object VersionOfApi extends Tag(ApiVersion.v5_1_0.toString)
   object ApiEndpoint1 extends Tag(nameOf(Implementations5_1_0.customViewNamesCheck))
   object ApiEndpoint2 extends Tag(nameOf(Implementations5_1_0.systemViewNamesCheck))
+  object ApiEndpoint3 extends Tag(nameOf(Implementations5_1_0.accountAccessUniqueIndexCheck))
   
   feature(s"test $ApiEndpoint1 version $VersionOfApi - Unauthorized access") {
     scenario("We will call the endpoint without user credentials", ApiEndpoint1, VersionOfApi) {
@@ -85,6 +86,42 @@ class SystemIntegrityTest extends V510ServerSetup {
       Entitlement.entitlement.vend.addEntitlement("", resourceUser1.userId, CanGetSystemIntegrity.toString)
       When("We make a request v5.1.0")
       val request510 = (v5_1_0_Request / "management" / "system" / "integrity" / "system-view-names-check").GET <@(user1)
+      val response510 = makeGetRequest(request510)
+      Then("We get successful response")
+      response510.code should equal(200)
+      response510.body.extract[CheckSystemIntegrityJsonV510]
+    }
+  }
+
+
+
+  feature(s"test $ApiEndpoint3 version $VersionOfApi - Unauthorized access") {
+    scenario("We will call the endpoint without user credentials", ApiEndpoint1, VersionOfApi) {
+      When("We make a request v5.1.0")
+      val request510 = (v5_1_0_Request / "management" / "system" / "integrity" / "account-access-unique-index-1-check").GET
+      val response510 = makeGetRequest(request510)
+      Then("We should get a 401")
+      response510.code should equal(401)
+      response510.body.extract[ErrorMessage].message should equal(UserNotLoggedIn)
+    }
+  }
+
+  feature(s"test $ApiEndpoint3 version $VersionOfApi - Authorized access") {
+    scenario("We will call the endpoint with user credentials but without a proper entitlement", ApiEndpoint1, VersionOfApi) {
+      When("We make a request v5.1.0")
+      val request510 = (v5_1_0_Request / "management" / "system" / "integrity" / "account-access-unique-index-1-check").GET <@(user1)
+      val response510 = makeGetRequest(request510)
+      Then("error should be " + UserHasMissingRoles + CanGetSystemIntegrity)
+      response510.code should equal(403)
+      response510.body.extract[ErrorMessage].message should be (UserHasMissingRoles + CanGetSystemIntegrity)
+    }
+  }
+
+  feature(s"test $ApiEndpoint3 version $VersionOfApi - Authorized access") {
+    scenario("We will call the endpoint with user credentials and a proper entitlement", ApiEndpoint1, VersionOfApi) {
+      Entitlement.entitlement.vend.addEntitlement("", resourceUser1.userId, CanGetSystemIntegrity.toString)
+      When("We make a request v5.1.0")
+      val request510 = (v5_1_0_Request / "management" / "system" / "integrity" / "account-access-unique-index-1-check").GET <@(user1)
       val response510 = makeGetRequest(request510)
       Then("We get successful response")
       response510.code should equal(200)
