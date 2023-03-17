38b06dd19b37e2bc4a76b8112022ff16fc766a3c
Marko Milić <marko@tesobe.com>
2023-03-17 11:55:59 +0100

    feature/Tweak the function getDbConnectionParameters

--- obp-api/src/main/scala/code/api/util/DBUtil.scala
+++ obp-api/src/main/scala/code/api/util/DBUtil.scala
@@ -5,10 +5,11 @@ import code.api.Constant
 object DBUtil {
   def getDbConnectionParameters: (String, String, String) = {
     val dbUrl = APIUtil.getPropsValue("db.url") openOr Constant.h2DatabaseDefaultUrlValue
-    val username = dbUrl.split(";").filter(_.contains("user")).toList.headOption.map(_.split("=")(1))
-    val password = dbUrl.split(";").filter(_.contains("password")).toList.headOption.map(_.split("=")(1))
-    val dbUser = APIUtil.getPropsValue("db.user").orElse(username)
-    val dbPassword = APIUtil.getPropsValue("db.password").orElse(password)
-    (dbUrl, dbUser.getOrElse(""), dbPassword.getOrElse(""))
+    val usernameAndPassword = dbUrl.split("\\?").filter(_.contains("user")).mkString
+    val username = usernameAndPassword.split("&").filter(_.contains("user")).mkString.split("=")(1)
+    val password = usernameAndPassword.split("&").filter(_.contains("password")).mkString.split("=")(1)
+    val dbUser = APIUtil.getPropsValue("db.user").getOrElse(username)
+    val dbPassword = APIUtil.getPropsValue("db.password").getOrElse(password)
+    (dbUrl, dbUser, dbPassword)
   }
 }
