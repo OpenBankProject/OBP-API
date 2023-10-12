c9a3a87107a93742683fcfbca836beb2c6024beb
hongwei <hongwei@tesobe.com>
2023-10-12 12:18:17 +0200

    refactor/set the "api_instance_id" to Constant-tweaked error

--- obp-api/src/main/scala/code/api/constant/constant.scala
+++ obp-api/src/main/scala/code/api/constant/constant.scala
@@ -23,7 +23,7 @@ object Constant extends MdcLoggable {
   final val HostName = APIUtil.getPropsValue("hostname").openOrThrowException(ErrorMessages.HostnameNotSpecified)
   
   final val ApiInstanceId = {
-    val apiInstanceIdFromProps = APIUtil.getPropsValue("api_instance_id").openOrThrowException(ErrorMessages.HostnameNotSpecified)
+    val apiInstanceIdFromProps = APIUtil.getPropsValue("api_instance_id").openOrThrowException(ErrorMessages.ApiInstanceIdNotSpecified)
     if(apiInstanceIdFromProps.endsWith("final")){
       apiInstanceIdFromProps
     }else{
