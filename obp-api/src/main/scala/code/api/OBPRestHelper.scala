cc29b66cef9b591fc4ee700c018f32a3658c497a
Marko Milić <marko@tesobe.com>
2023-11-13 14:07:23 +0100

    feature/Add operationId to the Call Context

--- obp-api/src/main/scala/code/api/OBPRestHelper.scala
+++ obp-api/src/main/scala/code/api/OBPRestHelper.scala
@@ -355,7 +355,8 @@ trait OBPRestHelper extends RestHelper with MdcLoggable {
       correlationId = correlationId,
       url = url,
       ipAddress = remoteIpAddress,
-      requestHeaders = reqHeaders
+      requestHeaders = reqHeaders,
+      operationId = rd.map(_.operationId)
     )
 
     // before authentication interceptor build response
