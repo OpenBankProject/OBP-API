eafb7bd05109435a911bfb95a767f52b7c520172
Marko Milić <marko@tesobe.com>
2024-02-28 11:35:48 +0100

    feature/Ensure that the WHERE clause is not missing in UPDATE query.

--- obp-api/src/main/scripts/migrate/migrate_00000013.sql
+++ obp-api/src/main/scripts/migrate/migrate_00000013.sql
@@ -1,6 +1,14 @@
-UPDATE consumer SET
-  perhourcalllimit = -1 ,
-  perdaycalllimit = -1 ,
-  perweekcalllimit = -1 ,
-  permonthcalllimit = -1 ,
-  perminutecalllimit = -1;
+UPDATE
+  consumer
+SET
+  perhourcalllimit = -1,
+  perdaycalllimit = -1,
+  perweekcalllimit = -1,
+  permonthcalllimit = -1,
+  perminutecalllimit = -1
+WHERE
+  perhourcalllimit <> -1
+  OR perdaycalllimit <> -1
+  OR perweekcalllimit <> -1
+  OR permonthcalllimit <> -1
+  OR perminutecalllimit <> -1;
