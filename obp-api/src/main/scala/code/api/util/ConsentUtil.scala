53274430868093b7a524b9913d1811d9df4d951a
hongwei <hongwei@tesobe.com>
2024-07-31 15:37:52 +0200

    bugfix/added callContext to applyConsentRules method

--- obp-api/src/main/scala/code/api/util/ConsentUtil.scala
+++ obp-api/src/main/scala/code/api/util/ConsentUtil.scala
@@ -451,7 +451,7 @@ object Consent extends MdcLoggable {
   private def applyBerlinGroupConsentRulesCommon(consentId: String, callContext: CallContext): Future[(Box[User], Option[CallContext])] = {
     implicit val dateFormats = CustomJsonFormats.formats
 
-    def applyConsentRules(consent: ConsentJWT): Future[(Box[User], Option[CallContext])] = {
+    def applyConsentRules(consent: ConsentJWT, callContext: CallContext): Future[(Box[User], Option[CallContext])] = {
       val cc = callContext
       // 1. Get or Create a User
       getOrCreateUser(consent.sub, consent.iss, Some(consent.toConsent().consentId), None, None) map {
@@ -526,7 +526,7 @@ object Consent extends MdcLoggable {
                     // Update MappedConsent.usesSoFarTodayCounter field
                     val consentUpdatedBox = Consents.consentProvider.vend.updateBerlinGroupConsent(consentId, currentCounterState + 1)
                     logger.debug(s"applyBerlinGroupConsentRulesCommon.consentUpdatedBox: $consentUpdatedBox")
-                    applyConsentRules(consent)
+                    applyConsentRules(consent, updatedCallContext)
                   case failure@Failure(_, _, _) => // Handled errors
                     Future(failure, Some(updatedCallContext))
                   case _ => // Unexpected errors
