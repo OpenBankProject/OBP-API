4e68e8968d84c4d4103e2a380de7f76a4cd7c604
hongwei <hongwei@tesobe.com>
2024-10-08 09:20:01 +0200

    feature/added RabbitMQ added  OBP database to server -step8

--- obp-api/src/main/scala/code/bankconnectors/rabbitmq/Adapter/RPCServer.scala
+++ obp-api/src/main/scala/code/bankconnectors/rabbitmq/Adapter/RPCServer.scala
@@ -1,47 +1,61 @@
 package code.bankconnectors.rabbitmq.Adapter
 
-import com.openbankproject.commons.dto.{InBoundGetBanks, OutBoundGetBanks}
-import com.openbankproject.commons.model.{BankCommons, BankId, InboundAdapterCallContext, Status}
+import bootstrap.liftweb.ToSchemify
+import code.api.util.APIUtil
+import com.openbankproject.commons.dto._
+import com.openbankproject.commons.model._
 import com.rabbitmq.client.AMQP.BasicProperties
 import com.rabbitmq.client._
+import net.liftweb.db.DB
 import net.liftweb.json
 import net.liftweb.json.Serialization.write
-
+import net.liftweb.mapper.{MapperRules, Schemifier}
+import scala.concurrent.duration._
 import java.util.concurrent.CountDownLatch
+import scala.concurrent.Await
 
 class ServerCallback(val ch: Channel, val latch: CountDownLatch) extends DeliverCallback {
   
   private implicit val formats = code.api.util.CustomJsonFormats.nullTolerateFormats
-  
+  val TIMEOUT = (3 seconds)
   override def handle(consumerTag: String, delivery: Delivery): Unit = {
     var response: String = null
+    val obpMessageId = delivery.getProperties.getMessageId
     val replyProps = new BasicProperties.Builder()
       .correlationId(delivery.getProperties.getCorrelationId)
+      .messageId(obpMessageId)
       .build
-
+    val message = new String(delivery.getBody, "UTF-8")
+    println(s"Request: OutBound message from OBP: process($obpMessageId) : message is $message " )
+    
     try {
-      val message = new String(delivery.getBody, "UTF-8")
-      println(s"Request: OutBound message from OBP: $message " )
-      val outBoundGetBanks = json.parse(message).extract[OutBoundGetBanks]
-      val inBoundGetBanks = InBoundGetBanks(
-        inboundAdapterCallContext = InboundAdapterCallContext(
-           correlationId = outBoundGetBanks.outboundAdapterCallContext.correlationId
-        ),
-        status = Status("",Nil),
-        data = List(BankCommons(
-          bankId = BankId("bankIdExample.value"),
-          shortName = "bankShortNameExample.value",
-          fullName = "bankFullNameExample.value",
-          logoUrl = "bankLogoUrlExample.value",
-          websiteUrl = "bankWebsiteUrlExample.value",
-          bankRoutingScheme = "bankRoutingSchemeExample.value",
-          bankRoutingAddress = "bankRoutingAddressExample.value",
-          swiftBic = "bankSwiftBicExample.value",
-          nationalIdentifier = "bankNationalIdentifierExample.valu"
-        )))
-      val responseToOBP = write(inBoundGetBanks)
-      println(s"Response: inBound message to OBP: $responseToOBP " )
-      response = "" + responseToOBP // convert OutBound to json string
+      
+      val responseToOBP = if(obpMessageId.contains("obp_get_banks")){
+        val outBound = json.parse(message).extract[OutBoundGetBanks]
+        
+        InBoundGetBanks(
+          inboundAdapterCallContext = InboundAdapterCallContext(
+             correlationId = outBound.outboundAdapterCallContext.correlationId
+          ),
+          status = Status("",Nil),
+          data = Await.result(code.bankconnectors.LocalMappedConnector.getBanks(None), TIMEOUT).map(_._1).head
+        )
+      } else if(obpMessageId.contains("obp_get_bank")){
+        val outBound = json.parse(message).extract[OutBoundGetBank]
+        InBoundGetBank(
+          inboundAdapterCallContext = InboundAdapterCallContext(
+            correlationId = outBound.outboundAdapterCallContext.correlationId
+          ),
+          status = Status("",Nil),
+          data = Await.result(code.bankconnectors.LocalMappedConnector.getBank(outBound.bankId,None), TIMEOUT).map(_._1).head
+        )
+      } else {
+        
+      }
+
+      val responseToOBPString = write(responseToOBP)
+      println(s"Response: inBound message to OBP: process($obpMessageId) : message is $responseToOBPString " )
+      response = "" + responseToOBPString 
       
     } catch {
       case e: Exception => {
@@ -62,6 +76,10 @@ object RPCServer {
   private val RPC_QUEUE_NAME = "obp_rpc_queue"
 
   def main(argv: Array[String]) {
+
+    DB.defineConnectionManager(net.liftweb.util.DefaultConnectionIdentifier, APIUtil.vendor)
+    Schemifier.schemify(true, Schemifier.infoF _, ToSchemify.models: _*)
+    
     var connection: Connection = null
     var channel: Channel = null
     try {
