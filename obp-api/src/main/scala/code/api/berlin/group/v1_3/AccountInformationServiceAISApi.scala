3ea348e66b7ce9c075f01162b7d6217b151a09e3
Alexandr I. Simacenco <alexandr.simacenco@maib.md>
2026-01-16 10:00:01 +0200

    Added certificate verification as a first priority

--- obp-api/src/main/scala/code/api/berlin/group/v1_3/AccountInformationServiceAISApi.scala
+++ obp-api/src/main/scala/code/api/berlin/group/v1_3/AccountInformationServiceAISApi.scala
@@ -343,6 +343,7 @@ of the PSU at this ASPSP.
        case "accounts" :: Nil JsonGet _ => {
          cc =>
            for {
+             _ <- passesPsd2Aisp(Some(cc))
             (Full(u), callContext) <- authenticatedAccess(cc)
             withBalanceParam <- NewStyle.function.tryons(s"$InvalidUrlParameters withBalance parameter can only take two values: TRUE or FALSE!", 400, callContext) {
 
