8ef0e7c5cb15bc1c5073f0ce8ff2e67eb7dfc0c5
hongwei <hongwei@tesobe.com>
2026-02-02 11:48:24 +0100

    Refactor/ API version handling in ApiCollector and related files
    Updated the ApiCollector implementations across multiple API versions to replace the deprecated ScannedApiVersion with the new ApiVersion utility. This change enhances consistency and maintainability in the API versioning approach. The affected files include ApiCollector.scala for AU, Bahrain, Polish, STET, and UK Open Banking APIs, as well as the ApiVersion.scala file where new constants were defined for these versions.

--- obp-api/src/main/scala/code/api/AUOpenBanking/v1_0_0/ApiCollector.scala
+++ obp-api/src/main/scala/code/api/AUOpenBanking/v1_0_0/ApiCollector.scala
@@ -35,7 +35,7 @@ import code.api.OBPRestHelper
 import code.api.util.APIUtil.{OBPEndpoint, ResourceDoc, getAllowedEndpoints}
 import code.api.util.ScannedApis
 import code.util.Helper.MdcLoggable
-import com.openbankproject.commons.util.{ApiVersionStatus, ScannedApiVersion}
+import com.openbankproject.commons.util.{ApiVersion, ApiVersionStatus}
 
 import scala.collection.mutable.ArrayBuffer
 
@@ -47,7 +47,7 @@ This file defines which endpoints from all the versions are available in v1
  */
 object ApiCollector extends OBPRestHelper with MdcLoggable with ScannedApis {
   //please modify these three parameter if it is not correct.
-  override val apiVersion = ScannedApiVersion("cds-au", "AU", "v1.0.0")
+  override val apiVersion = ApiVersion.cdsAuV100
   val versionStatus = ApiVersionStatus.DRAFT.toString
 
   private[this] val endpoints =
