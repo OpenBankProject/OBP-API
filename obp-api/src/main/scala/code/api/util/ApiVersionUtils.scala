c2665c6d25b63e989db2495ed58eb14cbe4c0410
hongwei <hongwei@tesobe.com>
2026-03-13 16:36:32 +0100

    bugfix/add BGv1.3 explicit case in ApiVersionUtils to resolve resource docs access

--- obp-api/src/main/scala/code/api/util/ApiVersionUtils.scala
+++ obp-api/src/main/scala/code/api/util/ApiVersionUtils.scala
@@ -7,7 +7,7 @@ import code.api.berlin.group.ConstantsBG
 object ApiVersionUtils {
 
   val scannedApis = ScannedApis.versionMapScannedApis.keysIterator.toList
-  val versions =
+  val versions = (
       v1_2_1 ::
       v1_3_0 ::
       v1_4_0 ::
@@ -23,8 +23,10 @@ object ApiVersionUtils {
       v7_0_0 ::
       `dynamic-endpoint` ::
       `dynamic-entity` ::
+      ConstantsBG.berlinGroupVersion1 ::
       ConstantsBG.berlinGroupVersion2 ::
       scannedApis
+    ).distinct
 
   def valueOf(value: String): ScannedApiVersion = {
 
@@ -47,6 +49,7 @@ object ApiVersionUtils {
       case v7_0_0.fullyQualifiedVersion | v7_0_0.apiShortVersion => v7_0_0
       case `dynamic-endpoint`.fullyQualifiedVersion | `dynamic-endpoint`.apiShortVersion => `dynamic-endpoint`
       case `dynamic-entity`.fullyQualifiedVersion | `dynamic-entity`.apiShortVersion => `dynamic-entity`
+      case version if version == ConstantsBG.berlinGroupVersion1.fullyQualifiedVersion || version == ConstantsBG.berlinGroupVersion1.apiShortVersion => ConstantsBG.berlinGroupVersion1
       case version if version == ConstantsBG.berlinGroupVersion2.fullyQualifiedVersion || version == ConstantsBG.berlinGroupVersion2.apiShortVersion => ConstantsBG.berlinGroupVersion2
       case version if(scannedApis.map(_.fullyQualifiedVersion).contains(version))
         =>scannedApis.filter(_.fullyQualifiedVersion==version).head
