4f7638d3daf336feebb81fde9f15fed2a7120cce
Hongwei <hongwei@tesobe.com>
2026-05-21 01:33:59 +0200

    fix(v2.1.0): restore v210SupportedTransactionRequestTypes val needed by answerChallenge guard

--- obp-api/src/main/scala/code/api/v2_1_0/Http4s210.scala
+++ obp-api/src/main/scala/code/api/v2_1_0/Http4s210.scala
@@ -164,6 +164,9 @@ object Http4s210 {
 
     // The 4 transaction request types this version knows how to handle. v4.0.0 adds more
     // (ACCOUNT, ACCOUNT_OTP, REFUND, SIMPLE, AGENT_CASH_WITHDRAWAL, CARD); the route guard
+    private val v210SupportedTransactionRequestTypes: Set[String] =
+      Set("SANDBOX_TAN", "COUNTERPARTY", "SEPA", "FREE_FORM")
+
     val createTransactionRequest: HttpRoutes[IO] = HttpRoutes.of[IO] {
       case req @ POST -> `prefixPath` / "banks" / _ / "accounts" / _ / viewIdStr / "transaction-request-types" / transactionRequestTypeStr / "transaction-requests" =>
         implicit val cc: CallContext = req.callContext
