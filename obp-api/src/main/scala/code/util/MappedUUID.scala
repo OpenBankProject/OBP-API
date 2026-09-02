8bf919dd51cd99d776238a7fa125114f46a3c0b4
Hongwei <hongwei@tesobe.com>
2026-09-02 14:35:58 +0200

    refactor: delete two DoobieQueries methods with zero callers
    getDistinctParentIds and getParentIdWithAttributes were written for
    AttributeQueryTrait.getParentIdByParams and NewAttributeQueryTrait.getParentIdByParams.
    Both traits are dead code with zero mixers anywhere in the tree, removed in the next
    commit as part of the net.liftweb.mapper cleanup - and neither ever called into these
    two methods either, so their removal leaves this file's only remaining function,
    getDistinctProviders, unaffected.
    
    Found while auditing the mapper cleanup's blast radius: the doc comments on both
    methods asserted a caller that had not existed since the traits were deleted, which
    is worse than no comment at all.

--- obp-api/src/main/scala/code/util/MappedUUID.scala
+++
@@ -1,8 +0,0 @@
-package code.util
-
-import code.api.util.APIUtil.generateUUID
-import net.liftweb.mapper.{MappedString, Mapper}
-
-class MappedUUID[T <: Mapper[T]] (override val fieldOwner : T) extends MappedString(fieldOwner, 36) {
-  override def defaultValue = generateUUID()
-}
